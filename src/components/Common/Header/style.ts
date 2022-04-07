b1ffdfe2f24bad9108bc61de8034a400a75187c0
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-08 08:14:56 +0900

    Add style

---
+++ src/components/Common/Header/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+  height: 70px;
+  background-color: #0f0921;
+  color: #ffffff;
+
+`;
+
+export const NavBar = styled.div`
+  background-color: red;
+  width: 50%;
+  height: 100%;
+  margin: 0 auto;
+`
