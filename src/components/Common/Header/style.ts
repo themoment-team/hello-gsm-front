bb406858f3c1d25579ad4346e5e3b02b98792391
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-09 22:01:48 +0900

    Add NavBar style

--- src/components/Common/Header/style.ts
+++ src/components/Common/Header/style.ts
@@ -4,7 +4,8 @@ export const Header = styled.div`
   height: 70px;
   background-color: #0f0921;
   color: #ffffff;
-
+  display: flex;
+  justify-content: space-around;
 `;
 
 export const NavBar = styled.div`
@@ -12,4 +13,17 @@ export const NavBar = styled.div`
   width: 50%;
   height: 100%;
   margin: 0 auto;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavContent = styled.p`
+  text-decoration: none;
+  font-weight: 700;
+  font-size: 18px;
+  color: #bdbdbd;
+  &:hover {
+    cursor: pointer;
+  }
+`;
