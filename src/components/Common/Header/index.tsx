431ccab989b51a0067aba6684d51c79f179e99fc
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-09 22:00:44 +0900

    Add NavBar

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,11 +1,25 @@
-import * as S from "./style"
+import Link from 'next/link';
+import * as S from './style';
 
 const Header: React.FC = () => {
   return (
     <S.Header>
-      <S.NavBar />
+      <S.NavBar>
+        <Link href="/asdf">
+          <S.NavContent>학교소개</S.NavContent>
+        </Link>
+        <Link href="/asdf">
+          <S.NavContent>팀소개</S.NavContent>
+        </Link>
+        <Link href="/asdf">
+          <S.NavContent>합격 여부</S.NavContent>
+        </Link>
+        <Link href="/asdf">
+          <S.NavContent>자주 묻는 질문</S.NavContent>
+        </Link>
+      </S.NavBar>
     </S.Header>
-  )
+  );
 }
 
 export default Header;
