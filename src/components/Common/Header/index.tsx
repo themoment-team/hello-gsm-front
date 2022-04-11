5218ce9a9e2a4eda230a9753c8422cd19a34cc35
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-11 23:09:29 +0900

    Add select style & passHref

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,33 +1,39 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import * as S from './style';
 
 const Header: React.FC = () => {
+  const pathname: string = useRouter().pathname;
+
+  const select = (navPath: string) =>
+    navPath === pathname ? { color: '#ffffff' } : undefined;
+
   return (
     <S.Header>
       <S.Logo>
-        <Link href="/">
+        <Link href="/" passHref>
           <S.LogoContent>Hello, GSM</S.LogoContent>
         </Link>
       </S.Logo>
       <S.NavBar>
-        <Link href="/">
-          <S.NavContent>홈으로</S.NavContent>
+        <Link href="/" passHref>
+          <S.NavContent style={select('/')}>홈으로</S.NavContent>
         </Link>
-        <Link href="/school">
-          <S.NavContent>학교소개</S.NavContent>
+        <Link href="/school" passHref>
+          <S.NavContent style={select('/school')}>학교소개</S.NavContent>
         </Link>
-        <Link href="/team">
-          <S.NavContent>팀소개</S.NavContent>
+        <Link href="/team" passHref>
+          <S.NavContent style={select('/about')}>팀소개</S.NavContent>
         </Link>
-        <Link href="/faq">
-          <S.NavContent>자주 묻는 질문</S.NavContent>
+        <Link href="/faq" passHref>
+          <S.NavContent style={select('/faq')}>자주 묻는 질문</S.NavContent>
         </Link>
       </S.NavBar>
       <S.MemberBox>
-        <Link href="/signin">
+        <Link href="/signin" passHref>
           <S.MemberContent>로그인</S.MemberContent>
         </Link>
-        <Link href="/signup">
+        <Link href="/signup" passHref>
           <S.MemberContent>회원가입</S.MemberContent>
         </Link>
       </S.MemberBox>
