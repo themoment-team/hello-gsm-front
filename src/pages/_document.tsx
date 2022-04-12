49178f25eea2d47bb3a746e23daa38a60b725b82
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-12 17:05:43 +0900

    Update get fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
