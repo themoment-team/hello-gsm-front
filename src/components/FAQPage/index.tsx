990b2966f8bf367671bd04dd438edc5aad913020
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-14 08:36:15 +0900

    Add get faqData props

--- src/components/FAQPage/index.tsx
+++ src/components/FAQPage/index.tsx
@@ -3,10 +3,20 @@ import type { NextPage } from 'next';
 import Header from 'components/Common/Header';
 // import * as S from './style';
 
-const FAQPage: NextPage = () => {
+interface FAQType {
+  faqData: {
+    title: string;
+    content: string;
+  }[];
+}
+
+const FAQPage: NextPage<FAQType> = ({ faqData }) => {
+  console.log(faqData);
+
   return (
     <>
       <Header />
+      <div>{faqData[0].content}</div>
     </>
   );
 };
