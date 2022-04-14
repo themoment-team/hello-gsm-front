6e78b915954f9d0383015c3e71ca8544150659e2
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-14 12:06:40 +0900

    Add axios get FAQ

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 import FAQPage from 'components/FAQPage';
+import axios from 'axios';
 
 interface FaqType {
-  faqData: {
+  data: {
     title: string;
     content: string;
   }[];
 }
 
-const Faq: NextPage<FaqType> = ({ faqData }) => {
+const Faq: NextPage<FaqType> = ({ data }) => {
   return (
     <>
-      <FAQPage faqData={faqData} />
+      <FAQPage faqData={data} />
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
   try {
-    const faqData = await (
-      await fetch('http://localhost:3000/data/faq.json')
-    ).json();
+    const { data } = await axios.get('http://localhost:3000/data/faq.json');
+    console.log(data);
     return {
       props: {
-        faqData,
+        data,
       },
     };
   } catch (e) {
