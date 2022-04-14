b700f06ca76c178e66ada9387fa25f36ba58508b
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-14 16:11:07 +0900

    Add Header React.memo

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import React from 'react';
 import * as S from './style';
 
 const Header: React.FC = () => {
@@ -41,4 +42,4 @@ const Header: React.FC = () => {
   );
 };
 
-export default Header;
+export default React.memo(Header);
