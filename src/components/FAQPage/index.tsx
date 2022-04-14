129a84f6bb191d3fccb55ad3a84ef6c0a1f261c8
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-14 12:07:15 +0900

    Add Search

--- src/components/FAQPage/index.tsx
+++ src/components/FAQPage/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Header from 'components/Common/Header';
-// import * as S from './style';
+import { useForm } from 'react-hook-form';
+import * as I from '../../Assets/svg';
+import * as S from './style';
 
 interface FAQType {
   faqData: {
@@ -13,10 +15,27 @@ interface FAQType {
 const FAQPage: NextPage<FAQType> = ({ faqData }) => {
   console.log(faqData);
 
+  const { register, watch } = useForm();
+  console.log(watch());
+
   return (
-    <>
+    <S.FAQPage>
       <Header />
-    </>
+      <S.FAQWrapper>
+        <S.Title>자주 묻는 질문</S.Title>
+        <S.SearchWrapper>
+          <I.SearchIcon />
+          <S.Search
+            type="text"
+            placeholder="질문 제목을 입력해주세요"
+            {...register('keywords')}
+          />
+        </S.SearchWrapper>
+        <S.FAQList>
+          
+        </S.FAQList>
+      </S.FAQWrapper>
+    </S.FAQPage>
   );
 };
 
