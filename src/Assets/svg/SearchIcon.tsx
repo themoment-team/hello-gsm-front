6d59ca857cf7b8aa28c1679faf4b5198acbd1963
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-14 12:06:11 +0900

    Add SerachIcon

---
+++ src/Assets/svg/SearchIcon.tsx
@@ -0,0 +1,27 @@
+const SearchIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M27 27L21 21"
+        stroke="#F0F0F0"
+        strokeWidth="4"
+        strokeLinecap="square"
+      />
+      <circle
+        cx="12.5"
+        cy="12.5"
+        r="10.75"
+        stroke="#F0F0F0"
+        strokeWidth="3.5"
+      />
+    </svg>
+  );
+};
+
+export default SearchIcon;
