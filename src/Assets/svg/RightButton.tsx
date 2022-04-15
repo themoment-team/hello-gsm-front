0b6d749138870cc0029abc706c2cd66389064768
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-15 11:52:23 +0900

    Add buttos

---
+++ src/Assets/svg/RightButton.tsx
@@ -0,0 +1,15 @@
+const RightBUtton = () => {
+  return (
+    <svg
+      width="11"
+      height="18"
+      viewBox="0 0 11 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 1L9 9L1 17" stroke="white" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default RightBUtton;
--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -1,1 +1,5 @@
 export { default as SearchIcon } from './SearchIcon';
+export { default as LeftAllButton } from './LeftAllButton';
+export { default as LeftButton } from './LeftButton';
+export { default as RightAllButton } from './RightAllButton';
+export { default as RightButton } from './RightButton';
