fb40afa8c76890bfd4725e92a9015df0f3dafef8
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-16 17:00:06 +0900

    Add svgs

--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -3,3 +3,5 @@ export { default as LeftAllButton } from './LeftAllButton';
 export { default as LeftButton } from './LeftButton';
 export { default as RightAllButton } from './RightAllButton';
 export { default as RightButton } from './RightButton';
+export { default as CloseButton } from './CloseButton';
+export { default as Questioner } from './Questioner';
