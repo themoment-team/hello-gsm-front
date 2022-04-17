7e66d7c835df117c10887479d6245cb34e64e6f4
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-17 23:06:38 +0900

    Add FAQPageBlue svg

---
+++ src/Assets/svg/FAQPageBlue.tsx
@@ -0,0 +1,29 @@
+const FAQPageBlue = () => {
+  return (
+    <svg
+      width="131"
+      height="131"
+      viewBox="0 0 131 131"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="65.5" cy="65.5" r="65.5" fill="url(#paint0_linear_518_145)" />
+      <defs>
+        <linearGradient
+          id="paint0_linear_518_145"
+          x1="96.3919"
+          y1="9.75532"
+          x2="33.2145"
+          y2="131"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5DC4FF" />
+          <stop offset="0.3" stopColor="#2978D6" />
+          <stop offset="1" stopColor="#0A244A" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default FAQPageBlue;
