22e76b03ee2de74b705ac6635a9ace664822b48d
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-18 20:41:56 +0900

    Add Footer

---
+++ src/components/Common/Footer/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './style';
+
+const Footer: React.FC = () => {
+  return (
+    <S.Footer>
+      <S.FooterContent>
+        Copyright 2022. the moment All rights reserved.
+      </S.FooterContent>
+    </S.Footer>
+  );
+};
+
+export default Footer;
