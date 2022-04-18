5d3e72166f14824ebb165f7fc6f72707bc568141
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-18 20:42:32 +0900

    Add for Test

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Header from 'components/Common/Header';
+import Footer from 'components/Common/Footer';
 // import * as S from './style';
 
 const MainPage: NextPage = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
