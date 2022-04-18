b7302574c5335f4af28210a23d98c7aaead8a3ff
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-18 15:05:14 +0900

    Remove balls

--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -3,5 +3,3 @@ export { default as LeftButton } from './LeftButton';
 export { default as RightButton } from './RightButton';
 export { default as CloseButton } from './CloseButton';
 export { default as Questioner } from './Questioner';
-export { default as FAQPageBlue } from './FAQPageBlue';
-export { default as FAQPageSkyBlue } from './FAQPageSkyBlue';
