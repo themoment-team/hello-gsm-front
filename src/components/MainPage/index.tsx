e17336bea278ca7f194732f670917c8a6dc7dfbc
Hyeongrok <khr5k0228@gmail.com>
2022-04-18 20:48:17 +0900

    Update index.tsx

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Header from 'components/Common/Header';
-import Footer from 'components/Common/Footer';
 // import * as S from './style';
 
 const MainPage: NextPage = () => {
