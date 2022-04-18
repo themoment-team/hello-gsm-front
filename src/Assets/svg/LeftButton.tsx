218a1c8a5010e030ef85fd7b0ed996b7121528b5
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-18 15:25:25 +0900

    Update return method

--- src/Assets/svg/LeftButton.tsx
+++ src/Assets/svg/LeftButton.tsx
@@ -1,19 +1,13 @@
-const LeftButton = () => {
-  return (
-    <svg
-      width="11"
-      height="17"
-      viewBox="0 0 11 17"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M10 1.09888L2 8.59888L10 16.0989"
-        stroke="white"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-};
+const LeftButton = () => (
+  <svg
+    width="11"
+    height="17"
+    viewBox="0 0 11 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 1.09888L2 8.59888L10 16.0989" stroke="white" strokeWidth="2" />
+  </svg>
+);
 
 export default LeftButton;
--- src/Assets/svg/RightButton.tsx
+++ src/Assets/svg/RightButton.tsx
@@ -1,15 +1,13 @@
-const RightBUtton = () => {
-  return (
-    <svg
-      width="11"
-      height="18"
-      viewBox="0 0 11 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M1 1L9 9L1 17" stroke="white" strokeWidth="2" />
-    </svg>
-  );
-};
+const RightBUtton = () => (
+  <svg
+    width="11"
+    height="18"
+    viewBox="0 0 11 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1 1L9 9L1 17" stroke="white" strokeWidth="2" />
+  </svg>
+);
 
 export default RightBUtton;
