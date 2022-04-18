2a768c65cd56d538148056710a5fef931e4c70c7
Hyeongrok <khr5k0228@gmail.com>
2022-04-18 15:07:14 +0900

    Update src/Assets/svg/CloseButton.tsx
    Co-authored-by: 유시온 <80191860+yoosion030@users.noreply.github.com>

--- src/Assets/svg/CloseButton.tsx
+++ src/Assets/svg/CloseButton.tsx
@@ -1,19 +1,18 @@
-const CloseButton = () => {
-  return (
-    <svg
-      width="16"
-      height="17"
-      viewBox="0 0 16 17"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M15 1L8 8.5M1 16L8 8.5M8 8.5L1 1L15 16"
-        stroke="black"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-};
+const CloseButton = () => (
+  <svg
+    width="16"
+    height="17"
+    viewBox="0 0 16 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15 1L8 8.5M1 16L8 8.5M8 8.5L1 1L15 16"
+      stroke="black"
+      strokeWidth="2"
+    />
+  </svg>
+);
 
 export default CloseButton;
+
