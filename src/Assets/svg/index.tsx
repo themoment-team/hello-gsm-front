e9db227b940f2471b9be2ae0160f48044b3cd7c7
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-18 12:59:00 +0900

    Delete Left,RightAllButton

--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -1,7 +1,5 @@
 export { default as SearchIcon } from './SearchIcon';
-export { default as LeftAllButton } from './LeftAllButton';
 export { default as LeftButton } from './LeftButton';
-export { default as RightAllButton } from './RightAllButton';
 export { default as RightButton } from './RightButton';
 export { default as CloseButton } from './CloseButton';
 export { default as Questioner } from './Questioner';
