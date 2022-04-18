dd1d6fc49d1c65a87fde4d18d4e6df84d185fe5b
yoosion030 <rsy5487@naver.com>
2022-04-18 20:51:29 +0900

    Add NotFound logo

--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -3,3 +3,4 @@ export { default as LeftButton } from './LeftButton';
 export { default as RightButton } from './RightButton';
 export { default as CloseButton } from './CloseButton';
 export { default as Questioner } from './Questioner';
+export { default as NotFound } from './NotFound';
