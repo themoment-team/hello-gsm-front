4e7b9f1e21bdb25e08f267dd6a618252810d1a06
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-19 18:35:46 +0900

    Add Section1 style

---
+++ src/components/SchoolPage/style.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+
+export const SchoolPage = styled.div`
+  margin-top: 70px;
+`;
+
+export const Section1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 1000px;
+`;
+
+export const SchoolName = styled.p`
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 40px;
+`;
+
+export const VideoBox = styled.div`
+  width: 69%;
+  height: 35vw;
+`;
+
+export const ToHomepage = styled.div`
+  width: 281px;
+  height: 63px;
+  border: 1px solid #ffffff;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const LinkText = styled.a`
+  font-weight: 700;
+  font-size: 22px;
+`;
