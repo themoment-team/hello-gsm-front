1918841f120c275c5e50d9b42fbee20a31985d40
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-19 18:47:41 +0900

    Add Balls style

--- src/components/SchoolPage/style.tsx
+++ src/components/SchoolPage/style.tsx
@@ -21,6 +21,8 @@ export const SchoolName = styled.p`
 export const VideoBox = styled.div`
   width: 69%;
   height: 35vw;
+  background: rgba(255, 255, 255, 0.16);
+  backdrop-filter: blur(48px);
 `;
 
 export const ToHomepage = styled.div`
@@ -37,4 +39,63 @@ export const ToHomepage = styled.div`
 export const LinkText = styled.a`
   font-weight: 700;
   font-size: 22px;
+`;
+
+const Ball = styled.div`
+  border-radius: 100%;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const GreenBall = styled(Ball)`
+  width: 176px;
+  height: 177px;
+  background: linear-gradient(
+    220deg,
+    #faffa4 10.2%,
+    #e6ef54 32.84%,
+    #2a3409 88.36%
+  );
+  top: 400px;
+  left: 59%;
+`;
+
+export const BlueBall = styled(Ball)`
+  width: 110px;
+  height: 110px;
+  background: linear-gradient(
+    220deg,
+    #7be8ff 10.2%,
+    #3b8ef0 32.84%,
+    rgba(0, 51, 77, 0.51) 88.36%
+  );
+  top: 900px;
+  left: 82%;
+`;
+
+export const SkyBlueBall = styled(Ball)`
+  width: 283px;
+  height: 283px;
+  background: linear-gradient(
+    220deg,
+    #9cddff 10.2%,
+    #48afe4 32.84%,
+    rgba(0, 51, 77, 0.51) 88.36%
+  );
+  top: 800px;
+  left: 6%;
+`;
+
+export const SmallBlueBall = styled(Ball)`
+  width: 99px;
+  height: 99px;
+  background: linear-gradient(
+    220deg,
+    #7be8ff 10.2%,
+    #339aff 24.7%,
+    #004891 88.36%
+  );
+  box-shadow: -2px 2px 10px rgba(0, 0, 0, 0.25);
+  top: 2000px;
+  left: 8%;
 `;
