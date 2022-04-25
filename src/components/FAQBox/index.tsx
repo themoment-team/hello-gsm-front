22033f8c02ab89413cd0653b41d9ea955689a41d
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-25 16:01:08 +0900

    Add search highlight

--- src/components/FAQBox/index.tsx
+++ src/components/FAQBox/index.tsx
@@ -1,9 +1,18 @@
 import * as S from './style';
 import { FAQType } from 'components/FAQPage';
 import useStore from 'Stores/StoreContainer';
+import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
 
-const FAQBox: React.FC<FAQType> = ({ title, content }) => {
-  const { setShowModal, setModalTitle, setModalContent } = useStore();
+type FAQBoxType = FAQType & {
+  keyword: string;
+};
+
+const FAQBox: React.FC<FAQBoxType> = ({ title, content, keyword }) => {
+  const { isSearching, setShowModal, setModalTitle, setModalContent } =
+    useStore();
+
+  const [FAQtitle, setFAQtitle] = useState(title);
 
   const showAnswer = () => {
     setShowModal();
@@ -11,9 +20,28 @@ const FAQBox: React.FC<FAQType> = ({ title, content }) => {
     setModalContent(content);
   };
 
+  useEffect(
+    () =>
+      isSearching
+        ? setFAQtitle(
+            title
+              .split(keyword)
+              .join(
+                `<span style="color: #000000; font-weight: 900; font-size: 22px;"}>${keyword}</span>`,
+              ),
+          )
+        : setFAQtitle(title),
+    [isSearching, keyword, title],
+  );
+
   return (
     <S.FAQBox onClick={showAnswer}>
-      <S.Title>{title}</S.Title>
+      <S.Title
+        dangerouslySetInnerHTML={{ __html: FAQtitle }}
+        css={css`
+          color: ${isSearching && '#827F8C'};
+        `}
+      />
     </S.FAQBox>
   );
 };
