cb2b5f0acb13959cce95be08a4c563650374791f
yoosion030 <rsy5487@naver.com>
2022-04-25 23:14:30 +0900

    Add Page Route

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import MyPage from 'components/MyPage';
+
+const Mypage: NextPage = () => {
+  return (
+    <>
+      <MyPage />
+    </>
+  );
+};
+
+export default Mypage;
