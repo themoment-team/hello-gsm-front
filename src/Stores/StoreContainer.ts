1bef7db957558b5f5cbabde267dcf6d9582546ef
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-26 21:07:17 +0900

    Add mypage modal store

--- src/Stores/StoreContainer.ts
+++ src/Stores/StoreContainer.ts
@@ -3,19 +3,28 @@ import create from 'zustand';
 interface storeType {
   showFAQModal: boolean;
   setShowFAQModal: () => void;
-  modalTitle: string;
-  setModalTitle: (title: string) => void;
-  modalContent: string;
-  setModalContent: (content: string) => void;
+  FAQModalTitle: string;
+  setFAQModalTitle: (title: string) => void;
+  FAQModalContent: string;
+  setFAQModalContent: (content: string) => void;
+  showMypageModal: boolean;
+  setShowMypageModal: () => void;
+  mypageModalContent: string;
+  setMypageModalContent: (content: string) => void;
 }
 
 const useStore = create<storeType>(set => ({
   showFAQModal: false,
   setShowFAQModal: () => set(state => ({ showFAQModal: !state.showFAQModal })),
-  modalTitle: '',
-  setModalTitle: title => set({ modalTitle: title }),
-  modalContent: '',
-  setModalContent: content => set({ modalContent: content }),
+  FAQModalTitle: '',
+  setFAQModalTitle: title => set({ FAQModalTitle: title }),
+  FAQModalContent: '',
+  setFAQModalContent: content => set({ FAQModalContent: content }),
+  showMypageModal: false,
+  setShowMypageModal: () =>
+    set(state => ({ showMypageModal: !state.showMypageModal })),
+  mypageModalContent: '',
+  setMypageModalContent: content => set({ mypageModalContent: content }),
 }));
 
 export default useStore;
