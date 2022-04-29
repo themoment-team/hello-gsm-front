b3a0112e97c7390192d5b69c2da9986285e7aef5
hyeongrok7874 <khr5k0228@gmail.com>
2022-04-29 09:11:37 +0900

    Add ApplyPage style

---
+++ src/components/ApplyPage/style.ts
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const ApplyPage = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ApplyPageContent = styled.div`
+  width: 900px;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 24px;
+  color: #f8f8f8;
+  text-align: center;
+`;
+
+export const ImgInputBox = styled.label`
+  width: 210px;
+  height: 280px;
+  background-color: #484453;
+  margin-top: 50px;
+  cursor: pointer;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ImgInput = styled.input`
+  display: none;
+`;
+
+export const Description = styled.p`
+  color: #96939c;
+  font-weight: 400;
+  font-size: 14px;
+`;
