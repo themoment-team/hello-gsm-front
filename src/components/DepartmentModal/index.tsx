1c6746f1d78f6754ee23e16e0be2a286fede70c1
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-09 20:13:59 +0900

    Add choiceDepartment

--- src/components/DepartmentModal/index.tsx
+++ src/components/DepartmentModal/index.tsx
@@ -4,7 +4,47 @@ import useStore from 'Stores/StoreContainer';
 import * as S from './style';
 
 const DepartmentModal: React.FC = () => {
-  const { showDepartmentModal, setShowDepartmentModal } = useStore();
+  const {
+    showDepartmentModal,
+    setShowDepartmentModal,
+    selectedChoice,
+    choice1,
+    choice2,
+    choice3,
+    setChoice1,
+    setChoice2,
+    setChoice3,
+  } = useStore();
+
+  const choiceDepartment = (choiceNumber: number) => {
+    setShowDepartmentModal();
+    switch (selectedChoice) {
+      case 1:
+        if (choice2 === choiceNumber) {
+          setChoice2(0);
+        } else if (choice3 === choiceNumber) {
+          setChoice3(0);
+        }
+        setChoice1(choiceNumber);
+        break;
+      case 2:
+        if (choice1 === choiceNumber) {
+          setChoice1(0);
+        } else if (choice3 === choiceNumber) {
+          setChoice3(0);
+        }
+        setChoice2(choiceNumber);
+        break;
+      case 3:
+        if (choice1 === choiceNumber) {
+          setChoice1(0);
+        } else if (choice2 === choiceNumber) {
+          setChoice2(0);
+        }
+        setChoice3(choiceNumber);
+        break;
+    }
+  };
 
   return (
     <S.DepartmentModal>
@@ -18,13 +58,13 @@ const DepartmentModal: React.FC = () => {
       <S.FAQModalBox>
         <S.Title>원하시는 과를 골라주세요</S.Title>
         <S.SelectBox>
-          <S.SelectContent onClick={setShowDepartmentModal}>
+          <S.SelectContent onClick={() => choiceDepartment(1)}>
             인공지능 개발과
           </S.SelectContent>
-          <S.SelectContent onClick={setShowDepartmentModal}>
+          <S.SelectContent onClick={() => choiceDepartment(2)}>
             소프트웨어 개발과
           </S.SelectContent>
-          <S.SelectContent onClick={setShowDepartmentModal}>
+          <S.SelectContent onClick={() => choiceDepartment(3)}>
             스마트 iot과
           </S.SelectContent>
         </S.SelectBox>
