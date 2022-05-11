4095a7d86260a46e489c7987b07b0b7ff16700fb
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-12 02:37:47 +0900

    Update FindSchoolModal style

--- src/components/FindSchoolModal/style.ts
+++ src/components/FindSchoolModal/style.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const FindSchoolModal = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.51);
+  background: rgba(0, 0, 0, 0.51);
   display: flex;
   justify-content: center;
   align-items: center;
