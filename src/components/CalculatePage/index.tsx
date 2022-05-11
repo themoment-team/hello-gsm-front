2bc066405d4e42020d43801cbe5c02279ea845ed
yoosion030 <rsy5487@naver.com>
2022-05-11 16:14:06 +0900

    Add Line

--- src/components/CalculatePage/index.tsx
+++ src/components/CalculatePage/index.tsx
@@ -5,11 +5,17 @@ import * as S from './style';
 import * as I from '../../Assets/svg';
 
 const CalculatePage: NextPage = () => {
+  const Lines = ['일반교과', '체육•예술 교과', '비교과'];
   return (
     <>
       <Header />
       <S.CalculatePage>
         <S.Title>성적입력</S.Title>
+        <S.LineList>
+          {Lines.map((line, i) => (
+            <S.Line key={i}>{line}</S.Line>
+          ))}
+        </S.LineList>
       </S.CalculatePage>
     </>
   );
