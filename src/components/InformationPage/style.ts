957db238a079593e4498f55f91fc893a9c08a528
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-12 17:45:09 +0900

    Add Balls style

--- src/components/InformationPage/style.ts
+++ src/components/InformationPage/style.ts
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled';
 
 export const InformationPage = styled.div`
+  position: relative;
+  overflow-x: hidden;
+`;
+
+export const InformationContent = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -72,4 +77,56 @@ export const Confirm = styled.a`
   padding: 18px 87px;
   margin: 200px 0;
   cursor: pointer;
+`;
+
+const Ball = styled.div`
+  border-radius: 100%;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const GreenBall = styled(Ball)`
+  width: 154px;
+  height: 154px;
+  background: linear-gradient(
+    207.52deg,
+    rgba(252, 255, 173, 0.9) 13.95%,
+    rgba(222, 228, 73, 0.9) 27.08%,
+    rgba(48, 95, 2, 0.513) 91.31%
+  );
+`;
+
+export const BigBlueBall = styled(Ball)`
+  width: 459px;
+  height: 459px;
+  background: linear-gradient(
+    207.52deg,
+    rgba(93, 196, 255, 0.9) 13.95%,
+    rgba(41, 120, 214, 0.9) 37.16%,
+    rgba(10, 36, 74, 0.9) 91.31%
+  );
+`;
+
+export const SmallBlueBall = styled(Ball)`
+  width: 75px;
+  height: 75px;
+  background: linear-gradient(
+    207.52deg,
+    rgba(93, 196, 255, 0.9) 13.95%,
+    rgba(41, 120, 214, 0.9) 37.16%,
+    rgba(10, 36, 74, 0.9) 91.31%
+  );
+`;
+
+export const LightBlueBall = styled(Ball)`
+  width: 230px;
+  height: 230px;
+  top: 2075px;
+  left: 80%;
+  background: linear-gradient(
+    220deg,
+    #9cddff 10.2%,
+    #48afe4 32.84%,
+    rgba(0, 51, 77, 0.51) 88.36%
+  );
 `;
