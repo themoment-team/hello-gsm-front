76924edc0040493933c1742f7974e0885c700b56
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-15 01:43:15 +0900

    Add Login button

--- src/components/MainPageDescription/style.ts
+++ src/components/MainPageDescription/style.ts
@@ -1,20 +1,35 @@
 import styled from '@emotion/styled';
 
 export const Description = styled.div`
-  font-weight: 700;
-  font-size: 36px;
-  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const DescriptionLine = styled.p`
-  text-align: center;
+  font-weight: 700;
+  font-size: 36px;
   margin-top: 15px;
+  color: #ffffff;
 `;
 
 export const PostScript = styled.p`
-  text-align: center;
   font-weight: 400;
   font-size: 20px;
   color: rgba(255, 255, 255, 0.7);
   margin-top: 15px;
+`;
+
+export const Login = styled.a`
+  width: 155px;
+  height: 45px;
+  background: #42bafe;
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 21px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
