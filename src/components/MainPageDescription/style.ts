7eda89c01c3557c6d08b5f95e8d775dd06cd1f1b
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-14 14:29:30 +0900

    Add MainPageDescription style

---
+++ src/components/MainPageDescription/style.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Description = styled.div`
+  font-weight: 700;
+  font-size: 36px;
+  color: #ffffff;
+`;
+
+export const DescriptionLine = styled.p`
+  text-align: center;
+  margin-top: 15px;
+`;
+
+export const PostScript = styled.p`
+  text-align: center;
+  font-weight: 400;
+  font-size: 20px;
+  color: rgba(255, 255, 255, 0.7);
+  margin-top: 15px;
+`;
