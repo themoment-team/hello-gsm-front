06bf7e050d34c2907ca31e7f28e021c2ef5789d7
yoosion030 <rsy5487@naver.com>
2022-05-15 21:16:31 +0900

    Update ProfileType

--- src/type/profile.ts
+++ src/type/profile.ts
@@ -1,66 +1,51 @@
 export interface ProfileType {
-  applicant: ApplicantType;
-  guardian: GuardianType;
-  teacher: TeacherType;
-  school: SchoolType;
-  screening: ScreeningType;
-  curriculum: CurriculumType;
-  nonCurriculum: NonCurriculumType;
-  scoreTotal: number;
-  major: MajorType;
+  user: UserType;
+  application: ApplicantType;
+  applicationDetails: ApplicationDetailsType;
+}
+
+interface UserType {
+  name: string;
+  birth: string;
+  gender: '남자' | '여자';
+  cellphoneNumber: string;
 }
 
 interface ApplicantType {
-  registration_number: number;
-  name: string;
-  gender: '남자' | '여자';
-  birth: string;
-  address: string;
-  contact: {
-    telephone: string;
-    cellphone: string;
-  };
-}
-
-interface GuardianType {
-  name: string;
-  relation: string;
-  cellphone: string;
-}
-
-interface TeacherType {
-  name: string;
-  cellphone: string;
-}
-
-interface SchoolType {
-  name: string;
-  educationStatus: string;
-  area: string;
-}
-
-interface ScreeningType {
+  registrationNumber: number;
+  isDocumentReception: boolean;
+  isFinalSubmission: boolean;
+  isFirstResult: boolean;
+  isFinalResult: boolean;
+  idPhotoUrl: string;
+  guardianName: string;
+  guardianCellphoneNumber: string;
+  teacherName: string;
+  teacherCellphoneNumber: string;
+  schoolLocation: string;
+  schoolName: string;
   screening: '일반전형' | '사회특별전형' | '특별전형';
-  socialScreening: '기회균등전형' | '사회다양성전형'; 
+  socialScreening: '기회균등전형' | '사회다양성전형';
 }
 
-interface CurriculumType {
+interface ApplicationDetailsType {
+  address: string;
+  telephoneNumber?: string;
+  guardianRelation: string;
+  educationStatus: string;
+  schoolTelephoneNumber: string;
   score1_1: number;
   score1_2: number;
   score2_1: number;
   score2_2: number;
   score3_1: number;
+  generalCurriculumScoreSubtotal: number;
   artSportsScore: number;
-  curriculumScore: number;
-}
-
-interface NonCurriculumType {
+  curriculumScoreSubtotal: number;
   attendanceScore: number;
   volunteerScore: number;
-  nonCurriculumScore: number;
-}
-
-interface MajorType {
+  nonCurriculumScoreSubtotal: number;
+  scoreTotal: number;
   firstWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
   secondWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
   thirdWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
