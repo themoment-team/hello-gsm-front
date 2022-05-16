d3c3d0f56d3cde4711b4167937400f4d545ac0c2
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-16 20:34:03 +0900

    Add favicon in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />
