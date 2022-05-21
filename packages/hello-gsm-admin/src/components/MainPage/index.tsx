2768f2308462f74efe6de9a4403412325ea48ada
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-21 18:54:31 +0900

    Add MainPage from legacy

--- packages/hello-gsm-admin/src/components/MainPage/index.tsx
+++ packages/hello-gsm-admin/src/components/MainPage/index.tsx
@@ -1,8 +1,31 @@
+import PassModal from 'components/PassModal';
 import type { NextPage } from 'next';
 import * as S from './style';
 
 const MainPage: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      {/* <PassModal /> */}
+      <S.MainPageContent>
+        <S.Searchbox>
+          <S.SearchInput placeholder="검색어를 입력하세요" />
+          <S.SearchButton>검색</S.SearchButton>
+        </S.Searchbox>
+        <S.ContentHeader>
+          <S.HeaderElement>지원자 번호</S.HeaderElement>
+          <S.HeaderElement>성명</S.HeaderElement>
+          <S.HeaderElement>전형</S.HeaderElement>
+          <S.HeaderElement>출신중</S.HeaderElement>
+          <S.HeaderElement>서류제출</S.HeaderElement>
+          <S.HeaderElement>지원자연락처</S.HeaderElement>
+          <S.HeaderElement>부모님연락처</S.HeaderElement>
+          <S.HeaderElement>담임선생님연락처</S.HeaderElement>
+          <S.HeaderElement>1차</S.HeaderElement>
+          <S.HeaderElement>2차</S.HeaderElement>
+        </S.ContentHeader>
+      </S.MainPageContent>
+    </>
+  );
 };
 
 export default MainPage;
