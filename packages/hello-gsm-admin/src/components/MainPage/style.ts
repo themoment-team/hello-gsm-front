1d1e55cf78e4e01768c77fefd6497bc53583910c
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-23 09:17:03 +0900

    Add Content style

--- packages/hello-gsm-admin/src/components/MainPage/style.ts
+++ packages/hello-gsm-admin/src/components/MainPage/style.ts
@@ -1,5 +1,13 @@
 import styled from '@emotion/styled';
 
+export const MainPage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const MainPageContent = styled.div`
   width: 1530px;
   margin: 0 auto;
@@ -58,10 +66,13 @@ export const HeaderElement = styled.div`
   font-weight: 700;
   font-size: 20px;
   color: #f8f8f8;
+  text-align: center;
 `;
 
 export const ContentList = styled.div`
   width: 100%;
+  height: 600px;
+  overflow-y: scroll;
 `;
 
 export const ContentBox = styled.div`
@@ -77,6 +88,72 @@ export const Content = styled.div`
   height: 100%;
   border-radius: 10px;
   background: #ffffff;
+  display: flex;
+  align-items: center;
+`;
+
+export const RegistrationNumber = styled.p`
+  width: 140px;
+  font-weight: 700;
+  font-size: 18px;
+  color: #0f0921;
+  text-align: center;
+`;
+
+export const Name = styled.p`
+  width: 130px;
+  font-weight: 500;
+  font-size: 18px;
+  color: #0f0921;
+  text-align: center;
+`;
+
+export const Screening = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  color: rgba(38, 38, 38, 0.54);
+  text-align: center;
+`;
+
+export const SchoolName = styled.p`
+  width: 210px;
+  font-weight: 500;
+  font-size: 18px;
+  color: rgba(38, 38, 38, 0.54);
+  text-align: center;
+`;
+
+export const isDocumentReception = styled.div`
+  width: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Checkbox = styled.span`
+  width: 25px;
+  height: 25px;
+  background: #bdbdbd;
+  border-radius: 5px;
+`;
+
+const Number = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  color: rgba(38, 38, 38, 0.54);
+  text-align: center;
+`;
+
+export const PhoneNumber = styled(Number)`
+  width: 170px;
+`;
+
+export const GuardianNumber = styled(Number)`
+  width: 200px;
+`;
+
+export const TeacherNumber = styled(Number)`
+  width: 210px;
 `;
 
 export const Pass = styled.div`
