15cccceb32faf08a8d2669c4e46cb04428921f80
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-23 20:17:05 +0900

    Add MockData

--- packages/hello-gsm-admin/src/components/MainPage/index.tsx
+++ packages/hello-gsm-admin/src/components/MainPage/index.tsx
@@ -3,65 +3,18 @@ import type { NextPage } from 'next';
 import * as S from './style';
 import useStore from 'Stores/StoreContainer';
 import { css } from '@emotion/react';
+import { useRef, useState } from 'react';
+import { applicantsType } from 'pages';
 
-interface applicantsType {
-  registrationNumber: number;
-  name: string;
-  screening: string;
-  schoolName: string;
-  isDocumentReception: boolean;
-  phoneNumber: string;
-  guardianNumber: string;
-  teacherNumber: string;
-}
-
-const MainPage: NextPage = () => {
+const MainPage: NextPage<applicantsType> = ({ data }) => {
   const { showPassModal, setShowPassModal } = useStore();
-  const applicants: applicantsType[] = [
-    {
-      registrationNumber: 2001,
-      name: '김형록',
-      screening: '사회통합전형',
-      schoolName: '성덕중학교',
-      isDocumentReception: true,
-      phoneNumber: '010-1234-5678',
-      guardianNumber: '010-1234-5678',
-      teacherNumber: '010-1234-5678',
-    },
-    {
-      registrationNumber: 2002,
-      name: '김형록',
-      screening: '사회통합전형',
-      schoolName: '성덕중학교',
-      isDocumentReception: false,
-      phoneNumber: '010-1234-5678',
-      guardianNumber: '010-1234-5678',
-      teacherNumber: '010-1234-5678',
-    },
-    {
-      registrationNumber: 2003,
-      name: '김형록',
-      screening: '사회통합전형',
-      schoolName: '성덕중학교',
-      isDocumentReception: true,
-      phoneNumber: '010-1234-5678',
-      guardianNumber: '010-1234-5678',
-      teacherNumber: '010-1234-5678',
-    },
-    {
-      registrationNumber: 2003,
-      name: '김형록',
-      screening: '사회통합전형',
-      schoolName: '성덕중학교',
-      isDocumentReception: false,
-      phoneNumber: '010-1234-5678',
-      guardianNumber: '010-1234-5678',
-      teacherNumber: '010-1234-5678',
-    },
-  ];
+  const searchRef = useRef<HTMLInputElement>(null);
+  const [keyword, setKeyword] = useState<string>('');
 
   const search = () => {
-    console.log('search');
+    if (searchRef.current) {
+      setKeyword(searchRef.current.value);
+    }
   };
 
   return (
@@ -69,7 +22,7 @@ const MainPage: NextPage = () => {
       {showPassModal && <PassModal />}
       <S.MainPageContent>
         <S.Searchbox>
-          <S.SearchInput placeholder="검색어를 입력하세요" />
+          <S.SearchInput placeholder="검색어를 입력하세요" ref={searchRef} />
           <S.SearchButton onClick={search}>검색</S.SearchButton>
         </S.Searchbox>
         <S.Header>
@@ -103,44 +56,50 @@ const MainPage: NextPage = () => {
           </S.HeaderElement>
         </S.Header>
         <S.ContentList>
-          {applicants.map(
-            (
-              {
-                registrationNumber,
-                name,
-                screening,
-                schoolName,
-                isDocumentReception,
-                phoneNumber,
-                guardianNumber,
-                teacherNumber,
-              }: applicantsType,
-              index: number,
-            ) => (
-              <S.ContentBox key={index}>
-                <S.Content>
-                  <S.RegistrationNumber>
-                    {registrationNumber}
-                  </S.RegistrationNumber>
-                  <S.Name>{name}</S.Name>
-                  <S.Screening>{screening}</S.Screening>
-                  <S.SchoolName>{schoolName}</S.SchoolName>
-                  <S.isDocumentReception>
-                    <S.Checkbox
-                      css={css`
-                        background: ${isDocumentReception && '#19BAFF'};
-                      `}
-                    />
-                  </S.isDocumentReception>
-                  <S.PhoneNumber>{phoneNumber}</S.PhoneNumber>
-                  <S.GuardianNumber>{guardianNumber}</S.GuardianNumber>
-                  <S.TeacherNumber>{teacherNumber}</S.TeacherNumber>
-                </S.Content>
-                <S.Pass onClick={setShowPassModal}>선택</S.Pass>
-                <S.Pass onClick={setShowPassModal}>선택</S.Pass>
-              </S.ContentBox>
-            ),
-          )}
+          {data
+            .filter(
+              ({ name, registrationNumber }) =>
+                name.includes(keyword) ||
+                registrationNumber === parseInt(keyword),
+            )
+            .map(
+              (
+                {
+                  registrationNumber,
+                  name,
+                  screening,
+                  schoolName,
+                  isDocumentReception,
+                  phoneNumber,
+                  guardianNumber,
+                  teacherNumber,
+                },
+                index: number,
+              ) => (
+                <S.ContentBox key={index}>
+                  <S.Content>
+                    <S.RegistrationNumber>
+                      {registrationNumber}
+                    </S.RegistrationNumber>
+                    <S.Name>{name}</S.Name>
+                    <S.Screening>{screening}</S.Screening>
+                    <S.SchoolName>{schoolName}</S.SchoolName>
+                    <S.isDocumentReception>
+                      <S.Checkbox
+                        css={css`
+                          background: ${isDocumentReception && '#19BAFF'};
+                        `}
+                      />
+                    </S.isDocumentReception>
+                    <S.PhoneNumber>{phoneNumber}</S.PhoneNumber>
+                    <S.GuardianNumber>{guardianNumber}</S.GuardianNumber>
+                    <S.TeacherNumber>{teacherNumber}</S.TeacherNumber>
+                  </S.Content>
+                  <S.Pass onClick={setShowPassModal}>선택</S.Pass>
+                  <S.Pass onClick={setShowPassModal}>선택</S.Pass>
+                </S.ContentBox>
+              ),
+            )}
         </S.ContentList>
       </S.MainPageContent>
     </S.MainPage>
