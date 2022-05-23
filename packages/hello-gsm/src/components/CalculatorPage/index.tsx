145d5eff4c51d8a89f968aa25f4bfa5d7fcb4923
yoosion030 <rsy5487@naver.com>
2022-05-23 16:14:30 +0900

    Add Rounds function

--- packages/hello-gsm/src/components/CalculatorPage/index.tsx
+++ packages/hello-gsm/src/components/CalculatorPage/index.tsx
@@ -3,58 +3,61 @@ import Header from 'components/Common/Header';
 import * as S from './style';
 import * as I from '../../Assets/svg';
 import { useForm, useFieldArray } from 'react-hook-form';
+import { useState } from 'react';
 
 const CalculatorPage: NextPage = () => {
   const { register, handleSubmit, watch, control } = useForm();
 
-  const calculate = (array: any, grade: number) => {
-    let result = 0;
-    let sum = 0;
-    array.map((test, i) => {
-      array[i] = Number(test);
-    });
-    for (let i = 0; i < array.length; i++) {
-      sum += array[i];
-    }
-    sum = array.reduce((accumulator, curr) => accumulator + curr);
-    console.log(sum);
+  const Rounds = (num: number) => {
+    return Number(num.toFixed(4)); // 소수점 넷째 자리에서 반올림 후 형변환
+  };
+  // 성적 계산해주는 함수
+  const Calculate = (array: Array<number>, grade: number) => {
+    array.map((arr, i) => {
+      array[i] = Number(arr);
+    }); // 배열을 number형변환
 
+    const sum = array.reduce((accumulator, curr) => accumulator + curr); // 배열 총합계 저장
+    let result = 0; // 결과
     switch (grade) {
       case 2:
-        result = 54 * (sum / (array.length * 5));
+        result = 54 * (sum / (array.length * 5)); // 2학년 공식 적용
         break;
       case 3:
-        result = 72 * (sum / (array.length * 5));
+        result = 72 * (sum / (array.length * 5)); // 3학년 공식 적용
         break;
       case 4:
-        result = 60 * (sum / (array.length * 5));
+        result = 60 * (sum / (array.length * 5)); // 예체능 공식 적용
         break;
     }
-
-    return result;
+    return Rounds(result);
   };
 
   const onValid = validForm => {
     console.log(validForm);
-    const score2_1 = calculate(validForm.score2_1, 2);
-    const score2_2 = calculate(validForm.score2_2, 2);
-    const score3_1 = calculate(validForm.score3_1, 3);
+    const score2_1 = Calculate(validForm.score2_1, 2);
+    const score2_2 = Calculate(validForm.score2_2, 2);
+    const score3_1 = Calculate(validForm.score3_1, 3);
 
-    const generalCurriculumScoreSubtotal = score2_1 + score2_2 + score3_1;
-    const artSportsScore = calculate(validForm.artSportsScore, 4);
-    const curriculumScoreSubtotal =
-      generalCurriculumScoreSubtotal + artSportsScore;
+    const generalCurriculumScoreSubtotal = Rounds(
+      score2_1 + score2_2 + score3_1,
+    );
+    const artSportsScore = Calculate(validForm.artSportsScore, 4);
+    const curriculumScoreSubtotal = Rounds(
+      generalCurriculumScoreSubtotal + artSportsScore,
+    );
     console.log(
       score2_1,
       score2_2,
       score3_1,
       generalCurriculumScoreSubtotal,
       artSportsScore,
+      curriculumScoreSubtotal,
     );
   };
 
   const Lines = ['일반교과', '체육•예술 교과', '비교과'];
-  const subjects = [
+  const [subjects, setSubjects] = useState([
     '국어',
     '도덕',
     '사회',
@@ -63,8 +66,8 @@ const CalculatorPage: NextPage = () => {
     '과학',
     '기술가정',
     '영어',
-  ];
-  const nonSubjects = ['체육', '미술', '음악'];
+  ]);
+  const [nonSubjects, setNonSubjects] = useState(['체육', '미술', '음악']);
 
   return (
     <>
@@ -130,6 +133,7 @@ const CalculatorPage: NextPage = () => {
               ))}
             </S.SemesterSection>
           </S.Section>
+
           <S.Section style={{ height: 200 }}>
             <S.SemesterSection>
               <div>
@@ -275,6 +279,6 @@ const CalculatorPage: NextPage = () => {
 };
 
 export default CalculatorPage;
-function calculate(score2_1: any, arg1: number) {
+function Calculate(score2_1: any, arg1: number) {
   throw new Error('Function not implemented.');
 }
