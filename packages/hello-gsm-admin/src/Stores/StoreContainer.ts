7152f99b7517815a28ff4549ad817cef04c7711d
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-24 18:10:10 +0900

    Add Logged store

--- packages/hello-gsm-admin/src/Stores/StoreContainer.ts
+++ packages/hello-gsm-admin/src/Stores/StoreContainer.ts
@@ -2,13 +2,19 @@ import create from 'zustand';
 
 interface storeType {
   showPassModal: boolean;
+  logged: boolean;
+
   setShowPassModal: () => void;
+  setLogged: () => void;
 }
 
 const useStore = create<storeType>(set => ({
   showPassModal: false,
+  logged: false,
+
   setShowPassModal: () =>
     set(state => ({ showPassModal: !state.showPassModal })),
+  setLogged: () => set(state => ({ logged: !state.logged })),
 }));
 
 export default useStore;
