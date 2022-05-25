83ecfe5092af4d76797b4d5c622686f0ab04cf17
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-25 20:59:16 +0900

    Remove BarElement positioning in style.ts

--- packages/hello-gsm/src/components/ApplyPage/style.ts
+++ packages/hello-gsm/src/components/ApplyPage/style.ts
@@ -55,6 +55,54 @@ export const BarElement = styled.p`
     left: 6.5px;
     margin-left: 15px;
   }
+  :nth-of-type(1) {
+    top: 100px;
+  }
+  :nth-of-type(2) {
+    top: 410px;
+  }
+  :nth-of-type(3) {
+    top: 500px;
+  }
+  :nth-of-type(4) {
+    top: 580px;
+  }
+  :nth-of-type(5) {
+    top: 665px;
+  }
+  :nth-of-type(6) {
+    top: 865px;
+  }
+  :nth-of-type(7) {
+    top: 920px;
+  }
+  :nth-of-type(8) {
+    top: 1110px;
+  }
+  :nth-of-type(9) {
+    top: 1190px;
+  }
+  :nth-of-type(10) {
+    top: 1250px;
+  }
+  :nth-of-type(11) {
+    top: 1330px;
+  }
+  :nth-of-type(12) {
+    top: 1540px;
+  }
+  :nth-of-type(13) {
+    top: 1625px;
+  }
+  :nth-of-type(14) {
+    top: 1710px;
+  }
+  :nth-of-type(15) {
+    top: 1900px;
+  }
+  :nth-of-type(16) {
+    top: 1980px;
+  }
 `;
 
 export const ApplyPageContent = styled.div`
