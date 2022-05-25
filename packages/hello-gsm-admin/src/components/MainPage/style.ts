403afee667b1f504e7e3c7c4def1c5ca20d8d6c4
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-25 15:09:12 +0900

    Add Print style

--- packages/hello-gsm-admin/src/components/MainPage/style.ts
+++ packages/hello-gsm-admin/src/components/MainPage/style.ts
@@ -74,6 +74,20 @@ export const SearchButton = styled.div`
   cursor: pointer;
 `;
 
+export const Print = styled.div`
+  width: 140px;
+  height: 100%;
+  background: #19baff;
+  border-radius: 10px;
+  color: rgba(0, 0, 0, 0.75);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
 export const Header = styled.div`
   width: 100%;
   height: 60px;
