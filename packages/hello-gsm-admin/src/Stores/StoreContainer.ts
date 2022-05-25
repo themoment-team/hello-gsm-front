7a945aecf844677ccdb0bfba411bdc75b0d180cd
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-25 09:21:09 +0900

    Add modal store

--- packages/hello-gsm-admin/src/Stores/StoreContainer.ts
+++ packages/hello-gsm-admin/src/Stores/StoreContainer.ts
@@ -2,18 +2,27 @@ import create from 'zustand';
 
 interface storeType {
   showPassModal: boolean;
+  passModalRegistrationNumber: number;
+  passModalName: string;
   logged: boolean;
 
   setShowPassModal: () => void;
+  setPassModalRegistrationNumber: (registrationNumber: number) => void;
+  setPassModalName: (name: string) => void;
   setLogged: () => void;
 }
 
 const useStore = create<storeType>(set => ({
   showPassModal: false,
+  passModalRegistrationNumber: 0,
+  passModalName: '',
   logged: false,
 
   setShowPassModal: () =>
     set(state => ({ showPassModal: !state.showPassModal })),
+  setPassModalRegistrationNumber: registrationNumber =>
+    set({ passModalRegistrationNumber: registrationNumber }),
+  setPassModalName: name => set({ passModalName: name }),
   setLogged: () => set(state => ({ logged: !state.logged })),
 }));
 
