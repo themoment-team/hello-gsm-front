e795822736c4753bea0b059b24e19c7e98aeac43
yoosion030 <rsy5487@naver.com>
2022-05-26 12:28:02 +0900

    Add string to number

--- packages/hello-gsm/src/components/CalculatorPage/function/Volunteer.ts
+++ packages/hello-gsm/src/components/CalculatorPage/function/Volunteer.ts
@@ -2,6 +2,9 @@
 const Volunteer = (array: Array<number>) => {
   let result = 0;
   array.map((arr, i) => {
+    array[i] = Number(arr);
+  }); // 배열을 number형변환
+  array.map((arr, i) => {
     const score = array[i];
     if (score >= 7) result += 10;
     else if (score >= 6) result += 8;
