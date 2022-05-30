4c022941f01144f132e0b1dc79524b3c8aff24cb
hyeongrok7874 <khr5k0228@gmail.com>
2022-05-30 15:34:24 +0900

    Add config.ts

---
+++ packages/hello-gsm/src/shared/config.ts
@@ -0,0 +1,21 @@
+interface sizeType {
+  readonly mobile: string;
+  readonly tablet: string;
+  readonly laptop: string;
+}
+
+const size: sizeType = {
+  mobile: '640px',
+  tablet: '960px',
+  laptop: '1080px',
+};
+
+const device: sizeType = {
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+};
+
+export const baseUrl = 'https://server.hellogsm.kr';
+
+export default device;
