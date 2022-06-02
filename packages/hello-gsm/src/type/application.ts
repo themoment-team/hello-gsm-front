136c6a0378415a789598eae9aef5d480f0abc97c
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-02 13:40:15 +0900

    Add application type

---
+++ packages/hello-gsm/src/type/application.ts
@@ -0,0 +1,19 @@
+export interface applicationType {
+  address: string;
+  addressDetails: string;
+  telephoneNumber: string;
+  screening: '일반전형' | '사회통합전형' | '특별전형';
+  schoolName: string;
+  schoolLocation: string;
+  graduationYear: string;
+  graduationMonth: string;
+  educationStatus: '졸업예정' | '졸업' | '검정고시';
+  firstWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  secondWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  thirdWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  guardianName: string;
+  guardianRelation: string;
+  guardianCellphoneNumber: string;
+  teacherName: string;
+  TeacherNumber: string;
+}
