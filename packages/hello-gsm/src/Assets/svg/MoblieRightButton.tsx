42b2a364dc110b42441558998c1bf14ad6b155e9
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-02 10:09:31 +0900

    Add MobileRightButton

---
+++ packages/hello-gsm/src/Assets/svg/MoblieRightButton.tsx
@@ -0,0 +1,13 @@
+const MobileRightButton = () => (
+  <svg
+    width="7"
+    height="10"
+    viewBox="0 0 7 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1 1L5 5L1 9" stroke="white" strokeWidth="1.5" />
+  </svg>
+);
+
+export default MobileRightButton;
