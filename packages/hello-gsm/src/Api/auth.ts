4f56d857b2dff29e11d08ac8f3408bb5c6ba4ddc
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-02 10:36:25 +0900

    Add signup

--- packages/hello-gsm/src/Api/auth.ts
+++ packages/hello-gsm/src/Api/auth.ts
@@ -1,13 +1,29 @@
+import { BASE_URL } from 'shared/config';
 import RequestApi from 'Utils/Libs/requestApi';
 import { AuthController } from 'Utils/Libs/requestUrls';
 
 class Auth {
   signin() {
+    return BASE_URL + AuthController.signin();
+  }
+
+  signup(name: string, cellphoneNumber: string, birth: Date, gender: string) {
     try {
+      const data = {
+        name,
+        cellphoneNumber,
+        birth,
+        gender,
+      };
       return RequestApi({
-        method: "get",
-        url: AuthController.signin(),
+        method: 'POST',
+        url: AuthController.signup(),
+        data: data,
       });
+    } catch (error) {
+      console.log(error);
     }
   }
 }
+
+export default new Auth();
