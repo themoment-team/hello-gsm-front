6ca3b42286164ccd7b53f4e27d2b533e30fc8f96
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-02 14:08:23 +0900

    Add postFirstSubmission

--- packages/hello-gsm/src/Api/application.ts
+++ packages/hello-gsm/src/Api/application.ts
@@ -1,4 +1,4 @@
-import { applicationType } from 'type/application';
+import { applicationType, scoreType } from 'type/application';
 import RequestApi from 'Utils/Libs/requestApi';
 import { ApplicationController } from 'Utils/Libs/requestUrls';
 
@@ -19,6 +19,18 @@ class Application {
       return RequestApi({
         method: 'DELETE',
         url: ApplicationController.information(),
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  postFirstSubmission(data: applicationType) {
+    try {
+      return RequestApi({
+        method: 'POST',
+        url: ApplicationController.firstSubmission(),
+        data: data,
       });
     } catch (error) {
       return error;
