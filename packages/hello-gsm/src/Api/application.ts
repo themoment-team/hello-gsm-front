ddc19c59f5b2277d42b2756f766d5e872fc434a4
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-02 13:47:50 +0900

    Add deleteInformation

--- packages/hello-gsm/src/Api/application.ts
+++ packages/hello-gsm/src/Api/application.ts
@@ -1,3 +1,4 @@
+import { applicationType } from 'type/application';
 import RequestApi from 'Utils/Libs/requestApi';
 import { ApplicationController } from 'Utils/Libs/requestUrls';
 
@@ -13,11 +14,12 @@ class Application {
     }
   }
 
-  deleteInformation() {
+  deleteInformation(data: applicationType) {
     try {
       return RequestApi({
         method: 'DELETE',
         url: ApplicationController.information(),
+        data: data,
       });
     } catch (error) {
       return error;
