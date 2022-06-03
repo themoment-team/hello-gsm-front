d2b6b080f56605cde432553425fb43018f4beb79
yoosion030 <rsy5487@naver.com>
2022-06-03 11:09:01 +0900

    Add Meta tag

---
+++ packages/hello-gsm/src/components/Common/SEOHelmet/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+
+interface SEOHelmetProps {
+  seoTitle: string;
+}
+
+const SEOHelmet = ({ seoTitle }: SEOHelmetProps) => {
+  return (
+    <Head>
+      <title>{seoTitle} | HelloGSM</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="광주소프트웨어마이스터고등학교 신입생 입학 지원 서비스입니다."
+      />
+      <meta name="og:title" content="Hello, GSM" />
+      <meta name="og:site_name" content="Hello, GSM" />
+      <meta
+        name="og:description"
+        content="광주소프트웨어마이스터고등학교 신입생 입학 지원 서비스입니다."
+      />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content="https://hellogsm.kr/" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+  );
+};
+
+export default SEOHelmet;
