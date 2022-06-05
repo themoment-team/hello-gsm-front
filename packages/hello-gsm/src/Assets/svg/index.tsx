c14078a08d6f0c3bbebf22ac971f06b432c56bfe
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-05 21:27:27 +0900

    Add Success svg

--- packages/hello-gsm/src/Assets/svg/index.tsx
+++ packages/hello-gsm/src/Assets/svg/index.tsx
@@ -14,3 +14,4 @@ export { default as MobileRightButton } from './MoblieRightButton';
 export { default as Celebration } from './Celebration';
 export { default as KakaoLogo } from './KakaoLogo';
 export { default as SmallCelebration } from './SmallCelebration';
+export { default as Success } from './Success';
