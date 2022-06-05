75cf8b44e58fd1bc53d63c0470af82e8f802da95
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-06 03:01:28 +0900

    Add SuccessModal

---
+++ packages/hello-gsm/src/components/SuccessModal/index.tsx
@@ -0,0 +1,29 @@
+import * as S from './style';
+import * as I from 'Assets/svg';
+import React, { useEffect } from 'react';
+import useStore from 'Stores/StoreContainer';
+
+const SuccessModal: React.FC = () => {
+  const { setShowSuccessModal } = useStore();
+
+  useEffect(() => {
+    setTimeout(() => setShowSuccessModal(), 3000);
+  }, []);
+
+  return (
+    <S.SuccessModal>
+      <S.Content>
+        <I.Success />
+        <S.DescriptionWrap>
+          <S.Description>원서 제출에 성공했습니다.</S.Description>
+          <S.PostScript>
+            ( 원서와 수기로 작성해야되는 파일들을 출력해서 저희 학교 행정실에
+            제출해야됩니다. )
+          </S.PostScript>
+        </S.DescriptionWrap>
+      </S.Content>
+    </S.SuccessModal>
+  );
+};
+
+export default SuccessModal;
