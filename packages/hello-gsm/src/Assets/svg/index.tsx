4b56f5ad4f1963c574f2de3721dec66d71ddafd9
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-07 11:30:40 +0900

    Delete MobileQuestioner

--- packages/hello-gsm/src/Assets/svg/index.tsx
+++ packages/hello-gsm/src/Assets/svg/index.tsx
@@ -8,7 +8,6 @@ export { default as TMLogo } from './TMLogo';
 export { default as Woman } from './Woman';
 export { default as Man } from './Man';
 export { default as MobileLeftButton } from './MobileLeftButton';
-export { default as MobileQuestioner } from './MobileQuestioner';
 export { default as MobileSearchIcon } from './MobileSearchIcon';
 export { default as MobileRightButton } from './MoblieRightButton';
 export { default as Celebration } from './Celebration';
