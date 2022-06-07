4ee0243a6b48caf247c651ad90683da5827f7f7d
yoosion030 <rsy5487@naver.com>
2022-06-07 12:23:07 +0900

    Add btns

--- packages/hello-gsm/src/Assets/svg/index.tsx
+++ packages/hello-gsm/src/Assets/svg/index.tsx
@@ -9,3 +9,5 @@ export { default as Woman } from './Woman';
 export { default as Man } from './Man';
 export { default as Celebration } from './Celebration';
 export { default as KakaoLogo } from './KakaoLogo';
+export { default as HamburgerButton } from './HamburgerButton';
+export { default as SideBarButton } from './SideBarButton';
