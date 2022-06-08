4c5f4ed6e448c5999047516ad0f86050fadabe06
yoosion030 <rsy5487@naver.com>
2022-06-08 17:52:26 +0900

    Update SEOHelmet content

--- packages/hello-gsm/src/components/Common/SEOHelmet/index.tsx
+++ packages/hello-gsm/src/components/Common/SEOHelmet/index.tsx
@@ -2,14 +2,16 @@ import Head from 'next/head';
 
 interface SEOHelmetProps {
   seoTitle: string;
+  desc: string;
 }
 
 /**
  *
- * @param seoTitle - head title 값
+ * @param seoTitle - page 제목
+ * @param desc - page 설명
  * @returns 검색엔진 최적화를 위한 HEAD태그
  */
-const SEOHelmet = ({ seoTitle }: SEOHelmetProps) => {
+const SEOHelmet = ({ seoTitle, desc }: SEOHelmetProps) => {
   return (
     <Head>
       <title>{seoTitle} | HelloGSM</title>
@@ -17,18 +19,24 @@ const SEOHelmet = ({ seoTitle }: SEOHelmetProps) => {
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
-        name="description"
-        content="광주소프트웨어마이스터고등학교 신입생 입학 지원 서비스입니다."
+        name="keywords"
+        content="광주소프트웨어마이스터고등학교,신입생입학지원"
       />
+      <meta name="author" content="themoment-team" />
+      <meta name="description" content={desc} />
+
       <meta name="og:title" content="Hello, GSM" />
       <meta name="og:site_name" content="Hello, GSM" />
-      <meta
-        name="og:description"
-        content="광주소프트웨어마이스터고등학교 신입생 입학 지원 서비스입니다."
-      />
+      <meta name="og:description" content={desc} />
       <meta name="og:type" content="website" />
-      <meta name="og:url" content="https://hellogsm.kr/" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="og:url" content="https://hellogsm.kr" />
+      <meta property="og:image" content="https://hellogsm.kr/thumbnail.png" />
+
+      <meta name="twitter:title" content="Hello, GSM" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="https://hellogsm.kr" />
+      <meta name="twitter:creator" content="themoment-team" />
+      <meta name="twitter:image" content="https://hellogsm.kr/thumbnail.png" />
     </Head>
   );
 };
