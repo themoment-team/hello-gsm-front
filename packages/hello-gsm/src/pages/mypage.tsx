7c9e060421cbb2c06b8729c4bfa90295f644e7e8
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-09 09:44:23 +0900

    Add getServerSideProps

--- packages/hello-gsm/src/pages/mypage.tsx
+++ packages/hello-gsm/src/pages/mypage.tsx
@@ -1,8 +1,29 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { MypagePage } from 'components';
+import user from 'Api/user';
+import { StatusType } from 'type/user';
 
-const MyPage: NextPage = () => {
-  return <MypagePage />;
+interface DataType {
+  data: StatusType;
+}
+
+const MyPage: NextPage<DataType> = ({ data }) => {
+  return <MypagePage status={data} />;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const { data } = await user.status();
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (e) {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default MyPage;
