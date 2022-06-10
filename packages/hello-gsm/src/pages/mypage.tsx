5b53dd833710ca27d9f0cd1bc6ca125f7c873325
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-10 16:08:08 +0900

    Add res value log

--- packages/hello-gsm/src/pages/mypage.tsx
+++ packages/hello-gsm/src/pages/mypage.tsx
@@ -10,10 +10,11 @@ interface DataType {
 const MyPage: NextPage<DataType> = ({ res }) => {
   const seoTitle = '내 정보';
   const desc = '원서 삭제, 원서 수정, 최종 제출 등을 할 수 있습니다. ';
+  console.log(res);
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
-      <MypagePage status={res.data} />
+      <MypagePage />
     </>
   );
 };
