4cef690693ad37aaf427cc7464cb857b2b3914b8
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-10 15:05:56 +0900

    Add token handling

--- packages/hello-gsm/src/Utils/Libs/requestApi.ts
+++ packages/hello-gsm/src/Utils/Libs/requestApi.ts
@@ -1,18 +1,45 @@
 import { BASE_URL } from 'shared/config';
 import axios, { AxiosRequestConfig } from 'axios';
+import { getAccessToken, getRefreshToken } from 'Utils/cookies';
+import { AuthController } from './requestUrls';
 
 const RequestApi = (p: AxiosRequestConfig) => {
-  try {
-    const res = axios({
-      method: p.method,
-      baseURL: BASE_URL,
-      url: p.url,
-      data: p.data,
-      withCredentials: true,
-    });
-    return res;
-  } catch (error) {
-    return error;
+  const accessToken = getAccessToken();
+  const refreshToken = getRefreshToken();
+
+  const refresh = async () => {
+    try {
+      await axios.post(AuthController.refresh(), {
+        headers: {
+          Cookie: refreshToken,
+        },
+      });
+      RequestApi(p);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  if (refreshToken) {
+    if (accessToken) {
+      try {
+        const res = axios({
+          method: p.method,
+          baseURL: BASE_URL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+          headers: {
+            Cookie: accessToken,
+          },
+        });
+        return res;
+      } catch (error) {
+        return error;
+      }
+    } else {
+      refresh();
+    }
   }
 };
 
