dd19d3219ba8d9c777b33739b72dd358d9af1556
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-11 16:02:57 +0900

    Add CookiesProvder

--- packages/hello-gsm/src/pages/_app.tsx
+++ packages/hello-gsm/src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-
 import { GlobalStyle } from 'shared/Styles/GlobalStyle';
+import { CookiesProvider } from 'react-cookie';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <CookiesProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </CookiesProvider>
     </>
   );
 }
