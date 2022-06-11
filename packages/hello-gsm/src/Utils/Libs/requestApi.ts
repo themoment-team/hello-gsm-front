917e88be339fc590e217aea3a6812f04972d7be8
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-11 17:03:58 +0900

    Add requestApi param

--- packages/hello-gsm/src/Utils/Libs/requestApi.ts
+++ packages/hello-gsm/src/Utils/Libs/requestApi.ts
@@ -1,55 +1,64 @@
 import { BASE_URL } from 'shared/config';
 import axios, { AxiosRequestConfig } from 'axios';
-import { getAccessToken, getRefreshToken } from 'Utils/cookies';
-import { AuthController } from './requestUrls';
 
-const RequestApi = (p: AxiosRequestConfig) => {
-  const accessToken = getAccessToken();
-  const refreshToken = getRefreshToken();
+const RequestApi = (p: AxiosRequestConfig, getToken?: string) => {
+  const token: string = getToken ? getToken : '';
+  // const refresh = async () => {
+  //   try {
+  //     // url 추가
+  //     await axios.post(BASE_URL + AuthController.refresh(), {
+  //       headers: {
+  //         Cookie: refreshToken,
+  //       },
+  //     });
+  //     console.log('get refresh');
+  //     RequestApi(p);
+  //   } catch (error) {
+  //     console.log(error);
+  //     return error;
+  //   }
+  // };
 
-  console.log('accessToken : ' + accessToken);
-  console.log('refreshToken : ' + refreshToken);
+  // if (refreshToken) {
+  //   if (accessToken) {
+  //     try {
+  //       console.log('accesstoken exist');
+  //       const res = axios({
+  //         method: p.method,
+  //         baseURL: BASE_URL,
+  //         url: p.url,
+  //         data: p.data,
+  //         withCredentials: true,
+  //         headers: {
+  //           Cookie: accessToken,
+  //         },
+  //       });
+  //       console.log('accesstoken exist and get success');
+  //       return res;
+  //     } catch (error) {
+  //       console.log('accesstoken exist error');
+  //       return error;
+  //     }
+  //   } else {
+  //     console.log('no accesstoken');
+  //     refresh();
+  //   }
+  // }
 
-  const refresh = async () => {
-    try {
-      // url 추가
-      await axios.post(BASE_URL + AuthController.refresh(), {
-        headers: {
-          Cookie: refreshToken,
-        },
-      });
-      console.log('get refresh');
-      RequestApi(p);
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  };
-
-  if (refreshToken) {
-    if (accessToken) {
-      try {
-        console.log('accesstoken exist');
-        const res = axios({
-          method: p.method,
-          baseURL: BASE_URL,
-          url: p.url,
-          data: p.data,
-          withCredentials: true,
-          headers: {
-            Cookie: accessToken,
-          },
-        });
-        console.log('accesstoken exist and get success');
-        return res;
-      } catch (error) {
-        console.log('accesstoken exist error');
-        return error;
-      }
-    } else {
-      console.log('no accesstoken');
-      refresh();
-    }
+  try {
+    const res = axios({
+      method: p.method,
+      baseURL: BASE_URL,
+      url: p.url,
+      data: p.data,
+      withCredentials: true,
+      headers: {
+        Cookies: token,
+      },
+    });
+    return res;
+  } catch (error) {
+    return error;
   }
 };
 
