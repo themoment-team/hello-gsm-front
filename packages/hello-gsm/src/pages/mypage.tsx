386e97e544051d58cf7e204c2bec3def8575b232
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-11 19:36:57 +0900

    Remove console.log

--- packages/hello-gsm/src/pages/mypage.tsx
+++ packages/hello-gsm/src/pages/mypage.tsx
@@ -12,7 +12,6 @@ interface DataType {
 const MyPage: NextPage<DataType> = ({ res }) => {
   const seoTitle = '내 정보';
   const desc = '원서 삭제, 원서 수정, 최종 제출 등을 할 수 있습니다. ';
-  console.log(res);
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
@@ -64,7 +63,6 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
     const res = await axios.get('https://server.hellogsm.kr/user', {
       headers: { cookie: accessToken },
     });
-    console.log(res);
     return {
       props: {
         res,
