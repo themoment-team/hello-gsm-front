badba9dc825a09e0febce7381ea2ea8022da2473
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-11 18:38:19 +0900

    Remove legacy codes

--- packages/hello-gsm/src/Utils/Libs/requestApi.ts
+++ packages/hello-gsm/src/Utils/Libs/requestApi.ts
@@ -3,47 +3,6 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 const RequestApi = (p: AxiosRequestConfig, getToken?: string) => {
   const token: string = getToken ? getToken : '';
-  // const refresh = async () => {
-  //   try {
-  //     // url 추가
-  //     await axios.post(BASE_URL + AuthController.refresh(), {
-  //       headers: {
-  //         Cookie: refreshToken,
-  //       },
-  //     });
-  //     console.log('get refresh');
-  //     RequestApi(p);
-  //   } catch (error) {
-  //     console.log(error);
-  //     return error;
-  //   }
-  // };
-
-  // if (refreshToken) {
-  //   if (accessToken) {
-  //     try {
-  //       console.log('accesstoken exist');
-  //       const res = axios({
-  //         method: p.method,
-  //         baseURL: BASE_URL,
-  //         url: p.url,
-  //         data: p.data,
-  //         withCredentials: true,
-  //         headers: {
-  //           Cookie: accessToken,
-  //         },
-  //       });
-  //       console.log('accesstoken exist and get success');
-  //       return res;
-  //     } catch (error) {
-  //       console.log('accesstoken exist error');
-  //       return error;
-  //     }
-  //   } else {
-  //     console.log('no accesstoken');
-  //     refresh();
-  //   }
-  // }
 
   try {
     const res = axios({
