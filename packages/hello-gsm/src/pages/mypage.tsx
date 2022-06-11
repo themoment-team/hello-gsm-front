84330e74894c0109ee8724c496f2569834e29ee3
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-11 19:48:09 +0900

    Add logs

--- packages/hello-gsm/src/pages/mypage.tsx
+++ packages/hello-gsm/src/pages/mypage.tsx
@@ -12,12 +12,16 @@ interface DataType {
 const MyPage: NextPage<DataType> = ({ res }) => {
   const seoTitle = '내 정보';
   const desc = '원서 삭제, 원서 수정, 최종 제출 등을 할 수 있습니다. ';
-  return (
-    <>
-      <SEOHelmet seoTitle={seoTitle} desc={desc} />
-      <MypagePage status={res.data} />
-    </>
-  );
+
+  console.log(res);
+
+  return <div>test</div>;
+  // return (
+  //   <>
+  //     <SEOHelmet seoTitle={seoTitle} desc={desc} />
+  //     <MypagePage status={res.data} />
+  //   </>
+  // );
 };
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
