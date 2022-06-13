4b097405c9e1f05c251312eb8f967b3cccfb4c0c
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-13 15:30:49 +0900

    Update split type

--- packages/hello-gsm/src/type/user.ts
+++ packages/hello-gsm/src/type/user.ts
@@ -1,12 +1,19 @@
-export interface StatusType {
-  data: {
-    name: string;
-    userImg: string;
-    application: null | {
-      firstResultScreening: boolean;
-      finalResultScreening: boolean;
-      isFinalSubmission: boolean;
-      isDocumentReception: boolean;
-    };
+interface StatusDataType {
+  name: string;
+  userImg: string;
+  application: null | {
+    firstResultScreening: boolean;
+    finalResultScreening: boolean;
+    isFinalSubmission: boolean;
+    isDocumentReception: boolean;
   };
+}
+
+export interface StatusType {
+  data: StatusDataType;
+}
+
+export interface MainDescStatusType {
+  selectedIndex: number;
+  data: StatusDataType;
 }
