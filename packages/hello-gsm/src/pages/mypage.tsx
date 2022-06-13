673765568f26286d6079af99cd4979eeba5ae461
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-13 17:16:50 +0900

    Add tsdoc

--- packages/hello-gsm/src/pages/mypage.tsx
+++ packages/hello-gsm/src/pages/mypage.tsx
@@ -17,6 +17,12 @@ const MyPage: NextPage<StatusType> = ({ data }) => {
   );
 };
 
+/**
+ * 유저 상태를 요청하는 함수
+ *
+ * @param accessToken - accessToken을 param으로 넘겨준다
+ * @returns - 유저 상태
+ */
 const getStatus = async (accessToken: string) => {
   try {
     const { data }: StatusType = await user.status(accessToken);
@@ -33,6 +39,11 @@ const getStatus = async (accessToken: string) => {
   }
 };
 
+/**
+ *
+ * @returns - refreshToken 비존재 시 signin redirection,
+ * refreshToken 존재 시, accessToken 존재 시 요청, 비존재 시 accessToken 요청 후 status 요청
+ */
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
   const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
@@ -42,9 +53,13 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
       return getStatus(accessToken);
     } else {
       try {
+        // 요청 헤더를 가저온다
         const { headers }: HeaderType = await auth.refresh(refreshToken);
+        // headers의 set-cookie의 첫번째 요소 (accessToken)을 가져와 저장한다.
         const accessToken = headers['set-cookie'][0].split(';')[0];
+        // 브라우저에 쿠키들을 저장한다
         ctx.res.setHeader('set-cookie', headers['set-cookie']);
+        // headers에서 가져온 accessToken을 담아 요청을 보낸다
         return getStatus(accessToken);
       } catch (error) {
         console.log(error);
