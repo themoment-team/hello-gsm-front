053126d6bfa8c5e560b822b632508e0416b8a6a4
yoosion030 <rsy5487@naver.com>
2022-06-14 09:25:19 +0900

    Update type

--- packages/hello-gsm/src/type/profile.ts
+++ packages/hello-gsm/src/type/profile.ts
@@ -1,40 +1,44 @@
 export interface ProfileType {
-  user: UserType;
-  application: ApplicantType;
-  applicationDetails: ApplicationDetailsType;
-}
-
-interface UserType {
-  name: string;
-  birth: string;
-  gender: '남자' | '여자';
-  cellphoneNumber: string;
+  data: {
+    name: string;
+    birth: Date;
+    cellphoneNumber: string;
+    gender: string;
+    application: ApplicantType;
+    applicationScore: ApplicantScoreType;
+  };
 }
 
 interface ApplicantType {
-  registrationNumber: number;
-  isDocumentReception: boolean;
-  isFinalSubmission: boolean;
-  isFirstResult: boolean;
-  isFinalResult: boolean;
-  idPhotoUrl: string;
-  guardianName: string;
-  guardianCellphoneNumber: string;
-  teacherName: string;
-  teacherCellphoneNumber: string;
-  schoolLocation: string;
   schoolName: string;
   screening: '일반전형' | '사회통합전형' | '특별전형';
+  teacherCellphoneNumber: string;
+  guardianCellphoneNumber: string;
+  applicationDetails: ApplicantDetailsType;
 }
 
-interface ApplicationDetailsType {
+interface ApplicantDetailsType {
+  applicantIdx: number;
+  idPhotoUrl: string;
   address: string;
+  addressDetails: string;
   telephoneNumber?: string;
+  gurardinanName: string;
   guardianRelation: string;
+  teacherName: string;
+  schoolLocation: string;
   educationStatus: '졸업예정' | '졸업' | '검정고시';
-  graduation_year: string;
-  graduation_month: string;
+  graduationYear: string;
+  graduationMonth: string;
   schoolTelephoneNumber: string;
+  firstWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  secondWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  thirdWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
+  majorResult?: string;
+}
+
+interface ApplicantScoreType {
+  applicationIdx: number;
   score2_1: number;
   score2_2: number;
   score3_1: number;
@@ -45,7 +49,4 @@ interface ApplicationDetailsType {
   volunteerScore: number;
   nonCurriculumScoreSubtotal: number;
   scoreTotal: number;
-  firstWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
-  secondWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
-  thirdWantedMajor: '소프트웨어 개발과' | '스마트 IOT과' | '인공지능과';
 }
