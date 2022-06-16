947b57a7b2dbbf3effd76c6155dc8155e8696925
yoosion030 <rsy5487@naver.com>
2022-06-16 10:30:39 +0900

    Add check ssr

--- packages/hello-gsm/src/pages/calculator.tsx
+++ packages/hello-gsm/src/pages/calculator.tsx
@@ -1,5 +1,7 @@
+import auth from 'Api/auth';
 import { SEOHelmet } from 'components';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import { HeaderType } from 'type/header';
 import CalculatorPage from '../components/CalculatorPage';
 
 const Calculator: NextPage = () => {
@@ -11,6 +13,41 @@ const Calculator: NextPage = () => {
       <CalculatorPage />
     </>
   );
+};
+
+/**
+ *
+ * @returns - 로그인 여부 확인 후 요청 성공 시 페이지 접근 가능
+ * accessToken이 없어 에러가 난다면 refresh 요청 후 접근 가능
+ * refreshToken도 없을 경우 로그인페이지로 이동
+ */
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
+  const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
+
+  try {
+    const { data } = await auth.check(accessToken);
+    return {
+      props: {},
+    };
+  } catch (err) {
+    if (refreshToken) {
+      // 요청 헤더를 가저온다
+      const { headers }: HeaderType = await auth.refresh(refreshToken);
+      // 브라우저에 쿠키들을 저장한다
+      ctx.res.setHeader('set-cookie', headers['set-cookie']);
+      return {
+        props: {},
+      };
+    } else {
+      return {
+        props: {},
+        redirect: {
+          destination: '/auth/signin',
+        },
+      };
+    }
+  }
 };
 
 export default Calculator;
