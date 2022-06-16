f794fc76fc4e883c25684eeab672aa25d4d137cf
yoosion030 <rsy5487@naver.com>
2022-06-16 10:14:08 +0900

    Add calculate helmet

--- packages/hello-gsm/src/pages/calculator.tsx
+++ packages/hello-gsm/src/pages/calculator.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { SEOHelmet } from 'components';
 import type { NextPage } from 'next';
 import CalculatorPage from '../components/CalculatorPage';
 
 const Calculator: NextPage = () => {
-  return <CalculatorPage />;
+  const seoTitle = '성적 입력';
+  const desc = '지원자의 성적을 기재합니다.';
+  return (
+    <>
+      <SEOHelmet seoTitle={seoTitle} desc={desc} />
+      <CalculatorPage />
+    </>
+  );
 };
 
 export default Calculator;
