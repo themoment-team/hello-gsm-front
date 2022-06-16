ead6503c85470164012d59b25d92d1c84c2fe6d5
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-16 10:08:35 +0900

    Add reload

--- packages/hello-gsm/src/components/SideBar/index.tsx
+++ packages/hello-gsm/src/components/SideBar/index.tsx
@@ -26,6 +26,7 @@ const SideBar: NextPage = () => {
       await auth.logout();
       setLogged(false);
       replace('/');
+      location.reload();
     } catch (error: any) {
       // accessToken 없을 시에 accessToken 발급 후 logout 요청
       if (error.response.status === 401) {
