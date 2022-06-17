8beddb2e8b570de197b51925985309f00f18b195
yoosion030 <rsy5487@naver.com>
2022-06-17 11:26:37 +0900

    Update setLogged boolean

--- packages/hello-gsm/src/pages/about.tsx
+++ packages/hello-gsm/src/pages/about.tsx
@@ -3,10 +3,15 @@ import { AboutPage, SEOHelmet } from 'components';
 import auth from 'Api/auth';
 import useStore from 'Stores/StoreContainer';
 
+interface CheckType {
+  data: boolean;
+}
+
 const About: NextPage<CheckType> = ({ data }) => {
   const seoTitle = '팀 소개';
   const desc = 'Hello, GSM 서비스를 제작한 themoment-team을 소개합니다.';
-  console.log(data);
+  const { setLogged } = useStore();
+  setLogged(data);
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
@@ -15,20 +20,20 @@ const About: NextPage<CheckType> = ({ data }) => {
   );
 };
 
-interface CheckType {
-  data: string;
-}
-
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
   try {
-    const { data }: CheckType = await auth.check(accessToken);
+    await auth.check(accessToken);
     return {
-      props: { data },
+      props: {
+        data: true,
+      },
     };
   } catch (error: any) {
     return {
-      props: {},
+      props: {
+        data: false,
+      },
     };
   }
 };
