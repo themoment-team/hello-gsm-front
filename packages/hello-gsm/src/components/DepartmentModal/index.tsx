2e66cb37d2ac924080437ec40d07029c039f1f79
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-17 14:43:20 +0900

    Update department type

--- packages/hello-gsm/src/components/DepartmentModal/index.tsx
+++ packages/hello-gsm/src/components/DepartmentModal/index.tsx
@@ -16,32 +16,32 @@ const DepartmentModal: React.FC = () => {
     setChoice3,
   } = useStore();
 
-  const choiceDepartment = (choiceNumber: number) => {
+  const choiceDepartment = (choice: string) => {
     setShowDepartmentModal();
     switch (selectedChoice) {
       case 1:
-        if (choice2 === choiceNumber) {
-          setChoice2(0);
-        } else if (choice3 === choiceNumber) {
-          setChoice3(0);
+        if (choice2 === choice) {
+          setChoice2('');
+        } else if (choice3 === choice) {
+          setChoice3('');
         }
-        setChoice1(choiceNumber);
+        setChoice1(choice);
         break;
       case 2:
-        if (choice1 === choiceNumber) {
-          setChoice1(0);
-        } else if (choice3 === choiceNumber) {
-          setChoice3(0);
+        if (choice1 === choice) {
+          setChoice1('');
+        } else if (choice3 === choice) {
+          setChoice3('');
         }
-        setChoice2(choiceNumber);
+        setChoice2(choice);
         break;
       case 3:
-        if (choice1 === choiceNumber) {
-          setChoice1(0);
-        } else if (choice2 === choiceNumber) {
-          setChoice2(0);
+        if (choice1 === choice) {
+          setChoice1('');
+        } else if (choice2 === choice) {
+          setChoice2('');
         }
-        setChoice3(choiceNumber);
+        setChoice3(choice);
         break;
     }
   };
@@ -58,13 +58,15 @@ const DepartmentModal: React.FC = () => {
       <S.FAQModalBox>
         <S.Title>원하시는 과를 골라주세요</S.Title>
         <S.SelectBox>
-          <S.SelectContent onClick={() => choiceDepartment(1)}>
+          <S.SelectContent onClick={() => choiceDepartment('인공지능 개발과')}>
             인공지능 개발과
           </S.SelectContent>
-          <S.SelectContent onClick={() => choiceDepartment(2)}>
+          <S.SelectContent
+            onClick={() => choiceDepartment('소프트웨어 개발과')}
+          >
             소프트웨어 개발과
           </S.SelectContent>
-          <S.SelectContent onClick={() => choiceDepartment(3)}>
+          <S.SelectContent onClick={() => choiceDepartment('스마트 iot과')}>
             스마트 iot과
           </S.SelectContent>
         </S.SelectBox>
