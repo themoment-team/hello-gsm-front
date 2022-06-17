a702a1bdb87f6668c4cd517af66a5e20b429aaed
yoosion030 <rsy5487@naver.com>
2022-06-17 11:31:25 +0900

    Add useEffect

--- packages/hello-gsm/src/pages/about.tsx
+++ packages/hello-gsm/src/pages/about.tsx
@@ -2,6 +2,7 @@ import type { GetServerSideProps, NextPage } from 'next';
 import { AboutPage, SEOHelmet } from 'components';
 import auth from 'Api/auth';
 import useStore from 'Stores/StoreContainer';
+import { useEffect } from 'react';
 
 interface CheckType {
   data: boolean;
@@ -11,7 +12,9 @@ const About: NextPage<CheckType> = ({ data }) => {
   const seoTitle = '팀 소개';
   const desc = 'Hello, GSM 서비스를 제작한 themoment-team을 소개합니다.';
   const { setLogged } = useStore();
-  setLogged(data);
+  useEffect(() => {
+    setLogged(data);
+  }, []);
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
