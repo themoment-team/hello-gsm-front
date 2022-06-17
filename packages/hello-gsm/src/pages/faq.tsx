14282f51135e2c59ed6eee9663ae6ab068121bde
yoosion030 <rsy5487@naver.com>
2022-06-17 13:58:41 +0900

    Add staticSSR

--- packages/hello-gsm/src/pages/faq.tsx
+++ packages/hello-gsm/src/pages/faq.tsx
@@ -1,10 +1,11 @@
-import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { FAQPage, SEOHelmet } from 'components';
 import axios from 'axios';
 import auth from 'Api/auth';
 import { useEffect } from 'react';
 import useStore from 'Stores/StoreContainer';
 import { CheckType } from 'type/check';
+import { HeaderType } from 'type/header';
 
 interface FaqType extends CheckType {
   data: {
@@ -30,16 +31,47 @@ const Faq: NextPage<FaqType> = ({ data, check }) => {
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
+  const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
   try {
     const { data } = await axios.get('https://hellogsm.kr/data/faq.json');
-    await auth.check(accessToken);
-    return {
-      props: {
-        data,
-        check: true,
-      },
-    };
+    if (ctx.req.cookies.refreshToken) {
+      if (ctx.req.cookies.accessToken) {
+        // 로그인 O
+        await auth.check(accessToken);
+        return {
+          props: {
+            data,
+            check: true,
+          },
+        };
+      } else {
+        // accessToken 만료 시
+        // 요청 헤더를 가저온다
+        const { headers }: HeaderType = await auth.refresh(refreshToken);
+        // headers의 set-cookie의 첫번째 요소 (accessToken)을 가져와 저장한다.
+        const accessToken = headers['set-cookie'][0].split(';')[0];
+        // 브라우저에 쿠키들을 저장한다
+        ctx.res.setHeader('set-cookie', headers['set-cookie']);
+        // headers에서 가져온 accessToken을 담아 요청을 보낸다
+        await auth.check(accessToken);
+        return {
+          props: {
+            data,
+            check: true,
+          },
+        };
+      }
+    } else {
+      // 로그인 X
+      return {
+        props: {
+          data,
+          check: false,
+        },
+      };
+    }
   } catch (e) {
+    // faq 요청 오류 시
     return {
       props: {},
     };
--- packages/hello-gsm/src/pages/school.tsx
+++ packages/hello-gsm/src/pages/school.tsx
@@ -4,6 +4,7 @@ import auth from 'Api/auth';
 import useStore from 'Stores/StoreContainer';
 import { useEffect } from 'react';
 import { CheckType } from 'type/check';
+import { HeaderType } from 'type/header';
 
 const School: NextPage<CheckType> = ({ check }) => {
   const seoTitle = '학교 소개';
@@ -21,15 +22,35 @@ const School: NextPage<CheckType> = ({ check }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
+  const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
-  try {
-    await auth.check(accessToken);
-    return {
-      props: {
-        check: true,
-      },
-    };
-  } catch (error: any) {
+  if (ctx.req.cookies.refreshToken) {
+    if (ctx.req.cookies.accessToken) {
+      // 로그인 O
+      await auth.check(accessToken);
+      return {
+        props: {
+          check: true,
+        },
+      };
+    } else {
+      // accessToken 만료 시
+      // 요청 헤더를 가저온다
+      const { headers }: HeaderType = await auth.refresh(refreshToken);
+      // headers의 set-cookie의 첫번째 요소 (accessToken)을 가져와 저장한다.
+      const accessToken = headers['set-cookie'][0].split(';')[0];
+      // 브라우저에 쿠키들을 저장한다
+      ctx.res.setHeader('set-cookie', headers['set-cookie']);
+      // headers에서 가져온 accessToken을 담아 요청을 보낸다
+      await auth.check(accessToken);
+      return {
+        props: {
+          check: true,
+        },
+      };
+    }
+  } else {
+    // 로그인 X
     return {
       props: {
         check: false,
