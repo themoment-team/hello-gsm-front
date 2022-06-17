6f5f9eab8f66be6bae2775f86214b03bdece0ae9
yoosion030 <rsy5487@naver.com>
2022-06-17 11:19:25 +0900

    Add type

--- packages/hello-gsm/src/pages/about.tsx
+++ packages/hello-gsm/src/pages/about.tsx
@@ -2,9 +2,8 @@ import type { GetServerSideProps, NextPage } from 'next';
 import { AboutPage, SEOHelmet } from 'components';
 import auth from 'Api/auth';
 import useStore from 'Stores/StoreContainer';
-import { useEffect } from 'react';
 
-const About: NextPage = ({ data }) => {
+const About: NextPage<CheckType> = ({ data }) => {
   const seoTitle = '팀 소개';
   const desc = 'Hello, GSM 서비스를 제작한 themoment-team을 소개합니다.';
   console.log(data);
@@ -16,10 +15,14 @@ const About: NextPage = ({ data }) => {
   );
 };
 
+interface CheckType {
+  data: string;
+}
+
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
   try {
-    const { data } = await auth.check(accessToken);
+    const { data }: CheckType = await auth.check(accessToken);
     return {
       props: { data },
     };
