1c50efe20bfcf03371ec916c13617d25cb039692
yoosion030 <rsy5487@naver.com>
2022-06-17 11:02:05 +0900

    Add console data

--- packages/hello-gsm/src/pages/about.tsx
+++ packages/hello-gsm/src/pages/about.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { AboutPage, SEOHelmet } from 'components';
+import auth from 'Api/auth';
+import useStore from 'Stores/StoreContainer';
+import { useEffect } from 'react';
 
-const About: NextPage = () => {
+const About: NextPage = ({ data }) => {
   const seoTitle = '팀 소개';
   const desc = 'Hello, GSM 서비스를 제작한 themoment-team을 소개합니다.';
+  console.log(data);
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
@@ -12,4 +16,17 @@ const About: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
+  try {
+    const { data } = await auth.check(accessToken);
+    return {
+      props: { data },
+    };
+  } catch (error: any) {
+    return {
+      props: {},
+    };
+  }
+};
 export default About;
