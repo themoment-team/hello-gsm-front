557ef1520a03366a1032796cf3e9b25e52626fb4
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-19 21:47:32 +0900

    Remove redirect

--- packages/hello-gsm/src/components/ApplyPage/index.tsx
+++ packages/hello-gsm/src/components/ApplyPage/index.tsx
@@ -165,7 +165,6 @@ const ApplyPage: NextPage<GetApplicationType> = ({ data }) => {
     onClick();
     if (isMajorSelected && isAddressExist && isSchoolNameExist && isIdPhoto) {
       await apply(data);
-      push('/calculator');
     } else {
       return;
     }
