b935d4374cc0bea35738041c0b867d385cef5ab9
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-22 20:07:24 +0900

    Update login status

--- packages/hello-gsm/src/pages/information.tsx
+++ packages/hello-gsm/src/pages/information.tsx
@@ -14,38 +14,24 @@ const Information: NextPage = () => {
   );
 };
 
-const check = async (accessToken: string) => {
-  try {
-    await auth.check(accessToken);
-    return {
-      props: {},
-    };
-  } catch (error) {
-    return {
-      props: {},
-      redirect: {
-        destination: '/auth/signin',
-      },
-    };
-  }
-};
-
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
   const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
 
   if (ctx.req.cookies.refreshToken) {
-    if (ctx.req.cookies.accessToken) {
-      return check(accessToken);
-    } else {
+    try {
+      await auth.check(accessToken);
+      return {
+        props: {},
+      };
+    } catch (error) {
       try {
         const { headers }: HeaderType = await auth.refresh(refreshToken);
-        // headers의 set-cookie의 첫번째 요소 (accessToken)을 가져와 저장한다.
-        const accessToken = headers['set-cookie'][0].split(';')[0];
         // 브라우저에 쿠키들을 저장한다
         ctx.res.setHeader('set-cookie', headers['set-cookie']);
-        // headers에서 가져온 accessToken을 담아 요청을 보낸다
-        return check(accessToken);
+        return {
+          props: {},
+        };
       } catch (errer) {
         return {
           props: {},
