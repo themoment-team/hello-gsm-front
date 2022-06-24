9ef5098967a1f530710f969457de09e9066c183b
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-24 15:05:48 +0900

    Remove BarBox

--- packages/hello-gsm/src/PageContainer/ApplyPage/index.tsx
+++ packages/hello-gsm/src/PageContainer/ApplyPage/index.tsx
@@ -9,6 +9,7 @@ import {
   DepartmentModal,
   FindAddressModal,
   FindSchoolModal,
+  ApplyBarBox,
 } from 'components';
 import { useForm } from 'react-hook-form';
 import application from 'Api/application';
@@ -187,24 +188,7 @@ const ApplyPage: NextPage<GetApplicationType> = ({ data }) => {
       {showDepartmentModal && <DepartmentModal />}
       <Header />
       <S.ApplyPage>
-        <S.BarBox>
-          <S.BarElement>증명사진</S.BarElement>
-          <S.BarElement>이름</S.BarElement>
-          <S.BarElement>성별</S.BarElement>
-          <S.BarElement>생년월일</S.BarElement>
-          <S.BarElement>주소지</S.BarElement>
-          <S.BarElement>집 전화</S.BarElement>
-          <S.BarElement>휴대폰 번호</S.BarElement>
-          <S.BarElement>전형</S.BarElement>
-          <S.BarElement>출신 중학교</S.BarElement>
-          <S.BarElement>졸업일</S.BarElement>
-          <S.BarElement>지원학과</S.BarElement>
-          <S.BarElement>이름</S.BarElement>
-          <S.BarElement>관계</S.BarElement>
-          <S.BarElement>핸드폰 번호</S.BarElement>
-          <S.BarElement>이름</S.BarElement>
-          <S.BarElement>연락처</S.BarElement>
-        </S.BarBox>
+        <ApplyBarBox />
         <S.ApplyPageContent onSubmit={handleSubmit(onSubmit)}>
           <S.Title>지원자 인적사항</S.Title>
           <S.ImgInputBox htmlFor="img-input">
