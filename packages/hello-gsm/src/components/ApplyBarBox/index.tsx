741074104e72faa643e026293f672ff757480bf6
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-29 20:22:29 +0900

    Update divide elements to Array

--- packages/hello-gsm/src/components/ApplyBarBox/index.tsx
+++ packages/hello-gsm/src/components/ApplyBarBox/index.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import * as S from './style';
 
 const ApplyBarBox: React.FC = () => {
+  const BarElements = [
+    '증명사진',
+    '이름',
+    '성별',
+    '생년월일',
+    '주소지',
+    '집 전화',
+    '휴대폰 번호',
+    '전형',
+    '출신 중학교',
+    '졸업일',
+    '지원학과',
+    '이름',
+    '관계',
+    '핸드폰 번호',
+    '이름',
+    '연락처',
+  ];
+
   return (
     <S.BarBox>
-      <S.BarElement>증명사진</S.BarElement>
-      <S.BarElement>이름</S.BarElement>
-      <S.BarElement>성별</S.BarElement>
-      <S.BarElement>생년월일</S.BarElement>
-      <S.BarElement>주소지</S.BarElement>
-      <S.BarElement>집 전화</S.BarElement>
-      <S.BarElement>휴대폰 번호</S.BarElement>
-      <S.BarElement>전형</S.BarElement>
-      <S.BarElement>출신 중학교</S.BarElement>
-      <S.BarElement>졸업일</S.BarElement>
-      <S.BarElement>지원학과</S.BarElement>
-      <S.BarElement>이름</S.BarElement>
-      <S.BarElement>관계</S.BarElement>
-      <S.BarElement>핸드폰 번호</S.BarElement>
-      <S.BarElement>이름</S.BarElement>
-      <S.BarElement>연락처</S.BarElement>
+      {BarElements.map((element: string, index: number) => (
+        <S.BarElement key={index}>{element}</S.BarElement>
+      ))}
     </S.BarBox>
   );
 };
