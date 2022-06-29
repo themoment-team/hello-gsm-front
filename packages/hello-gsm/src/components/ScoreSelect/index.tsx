0c41bda158ea56784cf5c8a6242ea8ed42f9f103
yoosion030 <rsy5487@naver.com>
2022-06-29 14:14:57 +0900

    Add 코드 설명

--- packages/hello-gsm/src/components/ScoreSelect/index.tsx
+++ packages/hello-gsm/src/components/ScoreSelect/index.tsx
@@ -6,9 +6,14 @@ interface ScoreSelectProps {
   register: UseFormRegisterReturn;
   scoreArray?: number[] | undefined;
   index: number;
-  isSubmission?: string | null;
 }
-
+/**
+ *
+ * @param register - register 값
+ * @param scoreArray - scoreArray값이 있고(이전에 점수를 입력했던 이력이 있다는 뜻), scoreArray[index]값이 N 이랑 같으면 selected되게 함
+ * @param index - 배열의 인덱스
+ * @returns - select 태그
+ */
 const ScoreSelect: NextPage<ScoreSelectProps> = ({
   register,
   scoreArray,
