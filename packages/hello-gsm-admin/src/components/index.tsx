98466dd67dfed0bf38417aa4526ef567c216537c
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-30 22:52:49 +0900

    Add PageContainer

--- packages/hello-gsm-admin/src/components/index.tsx
+++ packages/hello-gsm-admin/src/components/index.tsx
@@ -1,5 +1,3 @@
 export { default as Input } from './Input';
-export { default as MainPage } from './MainPage';
 export { default as PassModal } from './PassModal';
-export { default as SignInPage } from './SignInPage';
 export { default as SEOHelmet } from './SEOHelmet';
