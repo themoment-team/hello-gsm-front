48da692cbe96667727ad7c9676d33b5b4d717b75
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-30 11:50:15 +0900

    Add InspectionIcon

--- packages/hello-gsm/src/Assets/svg/InspectionIcon.tsx
+++ packages/hello-gsm/src/Assets/svg/InspectionIcon.tsx
@@ -1,27 +1,33 @@
 const InspectionIcon = () => (
   <svg
-    width="191"
-    height="172"
-    viewBox="0 0 191 172"
+    width="217"
+    height="217"
+    viewBox="0 0 217 217"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g filter="url(#filter0_d_3616_658)">
+    <g filter="url(#filter0_d_4005_899)">
       <path
-        d="M84.2417 12.5C89.2454 3.83331 101.755 3.83334 106.758 12.5L178.205 136.25C183.209 144.917 176.954 155.75 166.947 155.75H24.0529C14.0455 155.75 7.79088 144.917 12.7946 136.25L84.2417 12.5Z"
+        d="M97.2417 19.5C102.245 10.8333 114.755 10.8333 119.758 19.5L191.205 143.25C196.209 151.917 189.954 162.75 179.947 162.75H37.0529C27.0455 162.75 20.7909 151.917 25.7946 143.25L97.2417 19.5Z"
         fill="black"
       />
       <path
-        d="M103.294 14.5L174.741 138.25C178.205 144.25 173.875 151.75 166.947 151.75H24.0529C17.1247 151.75 12.7946 144.25 16.2587 138.25L87.7058 14.5C91.1699 8.49999 99.8301 8.49999 103.294 14.5Z"
+        d="M116.294 21.5L187.741 145.25C191.205 151.25 186.875 158.75 179.947 158.75H37.0529C30.1247 158.75 25.7946 151.25 29.2587 145.25L100.706 21.5C104.17 15.5 112.83 15.5 116.294 21.5Z"
         stroke="#F9CE37"
         strokeWidth="8"
       />
     </g>
+    <g filter="url(#filter1_d_4005_899)">
+      <path
+        d="M131.129 123.958L111.033 103.862C113.021 98.7832 111.917 92.8207 107.721 88.6249C103.304 84.2082 96.6792 83.3249 91.3792 85.7541L100.875 95.2499L94.25 101.875L84.5333 92.3791C81.8833 97.6791 82.9875 104.304 87.4042 108.721C91.6 112.917 97.5625 114.021 102.642 112.033L122.738 132.129C123.621 133.012 124.946 133.012 125.829 132.129L130.908 127.05C132.013 126.167 132.013 124.621 131.129 123.958Z"
+        fill="#F9CE37"
+      />
+    </g>
     <defs>
       <filter
-        id="filter0_d_3616_658"
-        x="0.0332031"
-        y="0"
+        id="filter0_d_4005_899"
+        x="13.0332"
+        y="7"
         width="190.934"
         height="171.75"
         filterUnits="userSpaceOnUse"
@@ -44,12 +50,46 @@ const InspectionIcon = () => (
         <feBlend
           mode="normal"
           in2="BackgroundImageFix"
-          result="effect1_dropShadow_3616_658"
+          result="effect1_dropShadow_4005_899"
         />
         <feBlend
           mode="normal"
           in="SourceGraphic"
-          in2="effect1_dropShadow_3616_658"
+          in2="effect1_dropShadow_4005_899"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter1_d_4005_899"
+        x="77"
+        y="82"
+        width="61"
+        height="61"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.976471 0 0 0 0 0.807843 0 0 0 0 0.215686 0 0 0 0.54 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_4005_899"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_4005_899"
           result="shape"
         />
       </filter>
