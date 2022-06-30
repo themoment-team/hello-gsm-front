bf2f46ddfbed3497d2e1b7a21991754b75a79a0e
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-01 08:49:49 +0900

    Add requestApi

---
+++ packages/hello-gsm-admin/src/Utils/Libs/requestApi.ts
@@ -0,0 +1,30 @@
+import { BASE_URL } from 'shared/config';
+import axios, { AxiosRequestConfig } from 'axios';
+
+const RequestApi = (p: AxiosRequestConfig, token?: string) => {
+  try {
+    const res = token
+      ? axios({
+          method: p.method,
+          baseURL: BASE_URL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+          headers: {
+            cookie: token,
+          },
+        })
+      : axios({
+          method: p.method,
+          baseURL: BASE_URL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+        });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default RequestApi;
