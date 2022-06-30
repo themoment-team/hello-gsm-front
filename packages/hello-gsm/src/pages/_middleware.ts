b47bd22f7e6e7c4447f6a0bd422a58ea9263851f
hyeongrok7874 <khr5k0228@gmail.com>
2022-06-30 17:32:35 +0900

    Add inspection redirect

---
+++ packages/hello-gsm/src/pages/_middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const { origin, pathname } = req.nextUrl;
+
+  if (
+    process.env.OPERATIONAL_STATUS === 'inspection' &&
+    pathname !== '/inspection'
+  ) {
+    return NextResponse.redirect(`${origin}/inspection`);
+  } else {
+    return NextResponse.next();
+  }
+}
