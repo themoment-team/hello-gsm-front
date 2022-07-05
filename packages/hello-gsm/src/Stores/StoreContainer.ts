64da1127a5f26844ee8aaa5d956a225117ba5740
yoosion030 <rsy5487@naver.com>
2022-07-05 19:09:02 +0900

    Add showScoreResult

--- packages/hello-gsm/src/Stores/StoreContainer.ts
+++ packages/hello-gsm/src/Stores/StoreContainer.ts
@@ -20,6 +20,7 @@ interface storeType {
   applicantAddress: string;
   showSideBar: boolean | null;
   showMypageSuccessModal: boolean;
+  showScoreResult: boolean;
 
   setLogged: (isLogged: boolean) => void;
   setShowFAQModal: () => void;
@@ -46,6 +47,7 @@ interface storeType {
   setApplicantAddress: (residence: string) => void;
   setShowSideBar: (value: boolean | null) => void;
   setShowMypageSuccessModal: () => void;
+  setShowScoreResult: () => void;
 }
 
 const useStore = create<storeType>(set => ({
@@ -68,6 +70,7 @@ const useStore = create<storeType>(set => ({
   applicantAddress: '',
   showSideBar: null,
   showMypageSuccessModal: false,
+  showScoreResult: false,
 
   setLogged: isLogged => set({ logged: isLogged }),
   setShowFAQModal: () => set(state => ({ showFAQModal: !state.showFAQModal })),
@@ -93,6 +96,8 @@ const useStore = create<storeType>(set => ({
   setShowSideBar: value => set({ showSideBar: value }),
   setShowMypageSuccessModal: () =>
     set(state => ({ showMypageSuccessModal: !state.showMypageSuccessModal })),
+  setShowScoreResult: () =>
+    set(state => ({ showScoreResult: !state.showScoreResult })),
 }));
 
 export default useStore;
