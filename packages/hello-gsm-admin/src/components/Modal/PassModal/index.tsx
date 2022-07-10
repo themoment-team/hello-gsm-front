364fe7eb8f184147c72113da8aa0a846bb7dd764
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-10 21:53:09 +0900

    Update ModalDescription

--- packages/hello-gsm-admin/src/components/Modal/PassModal/index.tsx
+++ packages/hello-gsm-admin/src/components/Modal/PassModal/index.tsx
@@ -2,15 +2,10 @@ import { css, Global } from '@emotion/react';
 import React, { useCallback, MouseEvent } from 'react';
 import * as S from './style';
 import useStore from 'Stores/StoreContainer';
+import { ModalDescription } from 'components';
 
 const PassModal: React.FC = () => {
-  const {
-    showPassModal,
-    setShowPassModal,
-    passModalRegistrationNumber,
-    passModalName,
-    passModalPeriod,
-  } = useStore();
+  const { showPassModal, setShowPassModal } = useStore();
 
   const removeClick = useCallback((e: MouseEvent<HTMLDivElement>): void => {
     e.stopPropagation();
@@ -27,14 +22,7 @@ const PassModal: React.FC = () => {
       />
       <S.PassModalBox onClick={removeClick}>
         <S.PassModalContent>
-          <S.DescriptionBox>
-            <S.RegistrationNumber>
-              접수번호 {passModalRegistrationNumber}
-            </S.RegistrationNumber>
-            <S.Description>
-              {passModalName}님의 {passModalPeriod}차 합격 여부를 선택해주세요.
-            </S.Description>
-          </S.DescriptionBox>
+          <ModalDescription />
           <S.ButtonWrap>
             <S.Fail onClick={setShowPassModal}>불합격</S.Fail>
             <S.Pass onClick={setShowPassModal}>합격</S.Pass>
