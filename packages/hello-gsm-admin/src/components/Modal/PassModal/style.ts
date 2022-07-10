097ac5a763925be6d7d4ed570adaf0bd2aead7bd
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-10 21:54:08 +0900

    Update ModalDescription

--- packages/hello-gsm-admin/src/components/Modal/PassModal/style.ts
+++ packages/hello-gsm-admin/src/components/Modal/PassModal/style.ts
@@ -30,30 +30,6 @@ export const PassModalContent = styled.div`
   justify-content: space-between;
 `;
 
-export const DescriptionBox = styled.div`
-  width: 550px;
-  height: 125px;
-  background: #ffffff;
-  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.17);
-  border-radius: 20px;
-  padding-top: 15px;
-`;
-
-export const RegistrationNumber = styled.p`
-  font-weight: 700;
-  font-size: 27px;
-  color: #0f0921;
-  text-align: center;
-`;
-
-export const Description = styled.p`
-  font-weight: 500;
-  font-size: 24px;
-  color: rgba(15, 9, 33, 0.85);
-  text-align: center;
-  margin-top: 10px;
-`;
-
 export const ButtonWrap = styled.div`
   width: 550px;
   height: 50px;
