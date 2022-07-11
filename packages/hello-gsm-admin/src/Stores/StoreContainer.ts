8b04bf2b6b90c701cf0ad20dc437d5ab5dad33b6
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-11 18:18:19 +0900

    Update store

--- packages/hello-gsm-admin/src/Stores/StoreContainer.ts
+++ packages/hello-gsm-admin/src/Stores/StoreContainer.ts
@@ -3,32 +3,32 @@ import create from 'zustand';
 interface storeType {
   showPassModal: boolean;
   showScoreModal: boolean;
-  passModalRegistrationNumber: number;
-  passModalName: string;
-  passModalPeriod: number;
+  modalRegistrationNumber: number;
+  modalName: string;
+  modalPeriod: 1 | 2;
 
   setShowPassModal: () => void;
   setShowScoreModal: () => void;
-  setPassModalRegistrationNumber: (registrationNumber: number) => void;
-  setPassModalName: (name: string) => void;
-  setPassModalPeriod: (period: number) => void;
+  setModalRegistrationNumber: (registrationNumber: number) => void;
+  setModalName: (name: string) => void;
+  setModalPeriod: (period: 1 | 2) => void;
 }
 
 const useStore = create<storeType>(set => ({
   showPassModal: false,
   showScoreModal: false,
-  passModalRegistrationNumber: 0,
-  passModalName: '',
-  passModalPeriod: 1,
+  modalRegistrationNumber: 0,
+  modalName: '',
+  modalPeriod: 1,
 
   setShowPassModal: () =>
     set(state => ({ showPassModal: !state.showPassModal })),
   setShowScoreModal: () =>
     set(state => ({ showScoreModal: !state.showScoreModal })),
-  setPassModalRegistrationNumber: registrationNumber =>
-    set({ passModalRegistrationNumber: registrationNumber }),
-  setPassModalName: name => set({ passModalName: name }),
-  setPassModalPeriod: period => set({ passModalPeriod: period }),
+  setModalRegistrationNumber: registrationNumber =>
+    set({ modalRegistrationNumber: registrationNumber }),
+  setModalName: name => set({ modalName: name }),
+  setModalPeriod: period => set({ modalPeriod: period }),
 }));
 
 export default useStore;
