23ae8806bd943445068b7f31079796460f0ec80f
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-11 18:30:30 +0900

    Add ScoreModal

--- packages/hello-gsm-admin/src/components/index.tsx
+++ packages/hello-gsm-admin/src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Input } from './Input';
 export { default as PassModal } from './Modal/PassModal';
+export { default as ScoreModal } from './Modal/ScoreModal';
 export { default as SEOHelmet } from './SEOHelmet';
 export { default as ModalDescription } from './ModalDescription';
