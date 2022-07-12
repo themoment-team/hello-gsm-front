22b64cd237972034eddd9bf49c7df8ed23ac7e1b
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-12 21:15:20 +0900

    Update application api

--- packages/hello-gsm-admin/src/Api/application.ts
+++ packages/hello-gsm-admin/src/Api/application.ts
@@ -1,14 +1,37 @@
-import { ReceptionType } from './../../Types/application';
+import { DocumentType, ScoreType } from 'Types/application';
 import RequestApi from 'Utils/Libs/requestApi';
 import { ApplicationController } from 'Utils/Libs/requestUrls';
 
 class Application {
-  reception(data: ReceptionType) {
+  document(data: DocumentType) {
     try {
       return RequestApi({
-        method: 'POST',
-        url: ApplicationController.reception(),
+        method: 'PATCH',
+        url: ApplicationController.document(),
         data: data,
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  score(data: ScoreType) {
+    try {
+      return RequestApi({
+        method: 'PATCH',
+        url: ApplicationController.score(),
+        data: data,
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  ticket() {
+    try {
+      return RequestApi({
+        method: 'GET',
+        url: ApplicationController.ticket(),
       });
     } catch (error) {
       return error;
