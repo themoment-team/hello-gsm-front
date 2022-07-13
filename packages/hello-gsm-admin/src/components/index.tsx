3837016a5594d9d16edabefefc8598037fc71dea
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-13 20:06:08 +0900

    Add ContentBox

--- packages/hello-gsm-admin/src/components/index.tsx
+++ packages/hello-gsm-admin/src/components/index.tsx
@@ -4,3 +4,4 @@ export { default as PassModal } from './Modal/PassModal';
 export { default as ScoreModal } from './Modal/ScoreModal';
 export { default as SEOHelmet } from './SEOHelmet';
 export { default as ModalDescription } from './ModalDescription';
+export { default as ContentBox } from './ContentBox';
