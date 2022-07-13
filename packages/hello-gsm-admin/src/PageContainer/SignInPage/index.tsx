3cbd82c38cfc5efc77d2ebae3be189dc161c4d6f
yoosion030 <rsy5487@naver.com>
2022-07-13 14:01:14 +0900

    Add api 연동, required

--- packages/hello-gsm-admin/src/PageContainer/SignInPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/SignInPage/index.tsx
@@ -1,45 +1,61 @@
 import type { NextPage } from 'next';
 import * as S from './style';
-import Link from 'next/link';
-import { Input } from 'components';
 import { useForm } from 'react-hook-form';
-
-interface UserForm {
-  id: string;
-  password: string;
-}
+import auth from 'Api/auth';
+import { LoginType } from 'Types/user';
+import { css } from '@emotion/react';
 
 const SignInPage: NextPage = () => {
-  const { register, handleSubmit } = useForm<UserForm>();
-  const onValid = (validForm: UserForm) => {
-    console.log(validForm);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginType>();
+
+  // 로그인 시도
+  const onValid = async (validForm: LoginType) => {
+    alert('asd');
+    const { id, password } = validForm;
+    try {
+      await auth.signin({ id, password });
+    } catch (err: any) {
+      console.error(err);
+      alert('비밀번호가 틀렸습니다.');
+    }
   };
+
+  const SelectError = () =>
+    css({
+      animation: `${S.shake} 0.3s`,
+    });
   return (
     <S.SignInPage>
       <S.BigBall />
       <S.SmallBall />
       <S.SignInForm onSubmit={handleSubmit(onValid)}>
         <S.Title>로그인</S.Title>
-        <Input
+        <S.Input
           placeholder="이메일을 입력해주세요."
           type="text"
-          register={register('id', {
-            required: true,
+          {...register('id', {
+            required: '* 아이디를 입력해주세요',
             minLength: 6,
             maxLength: 12,
           })}
-          bigWidth={false}
         />
-        <Input
+        <S.Error css={errors.id && SelectError}>{errors.id?.message}</S.Error>
+        <S.Input
           placeholder="비밀번호를 입력해주세요."
           type="password"
-          register={register('password', {
-            required: true,
+          {...register('password', {
+            required: '* 비밀번호를 입력해주세요',
             minLength: 8,
             maxLength: 16,
           })}
-          bigWidth={false}
         />
+        <S.Error css={errors.password && SelectError}>
+          {errors.password?.message}
+        </S.Error>
         <S.Submit>로그인</S.Submit>
       </S.SignInForm>
     </S.SignInPage>
