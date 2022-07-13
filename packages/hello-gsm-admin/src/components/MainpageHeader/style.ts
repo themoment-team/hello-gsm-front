049056252ad49c8323725ee0de70160451ae650e
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-13 16:29:53 +0900

    Add MainpageHeader style

---
+++ packages/hello-gsm-admin/src/components/MainpageHeader/style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.div`
+  width: 100%;
+  height: 60px;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  margin-top: 10px;
+`;
+
+export const HeaderElement = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  color: #f8f8f8;
+  text-align: center;
+`;
