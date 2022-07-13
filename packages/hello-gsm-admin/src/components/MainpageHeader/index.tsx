d21588c17e8fd2a6cd221b55cbf7ba5e88abde89
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-13 16:57:16 +0900

    Add headerElement array

--- packages/hello-gsm-admin/src/components/MainpageHeader/index.tsx
+++ packages/hello-gsm-admin/src/components/MainpageHeader/index.tsx
@@ -1,38 +1,26 @@
-import { css } from '@emotion/react';
 import React from 'react';
 import * as S from './style';
 
 const MainpageHeader: React.FC = () => {
+  const headerElement = [
+    '지원자',
+    '성명',
+    '전형',
+    '출신중',
+    '서류제출',
+    '지원자연락처',
+    '부모님연락처',
+    '담임선생님연락처',
+    '1차',
+    '2차',
+  ];
+
   return (
     <S.Header>
-      <S.HeaderElement>지원자 번호</S.HeaderElement>
-      <S.HeaderElement>성명</S.HeaderElement>
-      <S.HeaderElement>전형</S.HeaderElement>
-      <S.HeaderElement
-        css={css`
-          width: 140px;
-        `}
-      >
-        출신중
-      </S.HeaderElement>
-      <S.HeaderElement>서류제출</S.HeaderElement>
-      <S.HeaderElement>지원자연락처</S.HeaderElement>
-      <S.HeaderElement>부모님연락처</S.HeaderElement>
-      <S.HeaderElement>담임선생님연락처</S.HeaderElement>
-      <S.HeaderElement
-        css={css`
-          width: 40px;
-        `}
-      >
-        1차
-      </S.HeaderElement>
-      <S.HeaderElement
-        css={css`
-          width: 40px;
-        `}
-      >
-        2차
-      </S.HeaderElement>
+      {headerElement.map((element: string, index: number) => (
+        <S.HeaderElement key={index}>{element}</S.HeaderElement>
+      ))}
+      ;
     </S.Header>
   );
 };
