84756ea0ef27c1121e0cb250538365120b1a3ab3
hyeongrok7874 <khr5k0228@gmail.com>
2022-07-13 20:04:17 +0900

    Update application type

--- packages/hello-gsm-admin/src/Types/application.ts
+++ packages/hello-gsm-admin/src/Types/application.ts
@@ -6,3 +6,18 @@ export interface ScoreType {
   registrationNumber: number;
   personalityEvaluationScore: number;
 }
+
+export interface applicantType {
+  registrationNumber: number;
+  name: string;
+  screening: string;
+  schoolName: string;
+  isDocumentReception: boolean;
+  phoneNumber: string;
+  guardianNumber: string;
+  teacherNumber: string;
+}
+
+export interface applicantsType {
+  data: applicantType[];
+}
