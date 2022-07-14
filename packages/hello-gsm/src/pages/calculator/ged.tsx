ed93ae17d161a368a4a0104356d012c5ff9753d0
yoosion030 <rsy5487@naver.com>
2022-07-14 11:11:53 +0900

    Add GED SSR

--- packages/hello-gsm/src/pages/calculator/ged.tsx
+++ packages/hello-gsm/src/pages/calculator/ged.tsx
@@ -1,8 +1,66 @@
-import type { NextPage } from 'next';
+import auth from 'Api/auth';
+import { SEOHelmet } from 'components';
+import type { GetServerSideProps, NextPage } from 'next';
+import { useEffect } from 'react';
+import useStore from 'Stores/StoreContainer';
+import { HeaderType } from 'type/header';
 import { GEDPage } from 'PageContainer';
+import { CheckType } from 'type/check';
 
-const GED: NextPage = () => {
-  return <GEDPage />;
+const Calculator: NextPage<CheckType> = () => {
+  const seoTitle = '검정고시생 성적 입력';
+  const desc = '검정고시생읜 성적을 기재합니다.';
+  const { setLogged } = useStore();
+  useEffect(() => {
+    setLogged(true);
+  }, []);
+  return (
+    <>
+      <SEOHelmet seoTitle={seoTitle} desc={desc} />
+      <GEDPage />
+    </>
+  );
 };
 
-export default GED;
+/**
+ *
+ * @returns - 로그인 여부 확인 후 요청 성공 시 페이지 접근 가능
+ * accessToken이 없어 에러가 난다면 refresh 요청 후 접근 가능
+ * refreshToken도 없을 경우 로그인페이지로 이동
+ */
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const accessToken = `accessToken=${ctx.req.cookies.accessToken}`;
+  const refreshToken = `refreshToken=${ctx.req.cookies.refreshToken}`;
+
+  if (ctx.req.cookies.refreshToken) {
+    try {
+      // 로그인 O
+      await auth.check(accessToken);
+      return {
+        props: {},
+      };
+    } catch (err) {
+      try {
+        // accessToken 만료시
+        const { headers }: HeaderType = await auth.refresh(refreshToken);
+        // 브라우저에 쿠키들을 저장한다
+        ctx.res.setHeader('set-cookie', headers['set-cookie']);
+        return {
+          props: {},
+        };
+      } catch (err) {
+        // 로그인 실패
+        return {
+          props: {},
+        };
+      }
+    }
+  } else {
+    // 로그인 X
+    return {
+      props: {},
+    };
+  }
+};
+
+export default Calculator;
