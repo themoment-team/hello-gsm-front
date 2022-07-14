e3014839a157ba23040be49dd4923b4dbd092e2f
yoosion030 <rsy5487@naver.com>
2022-07-14 15:55:16 +0900

    Add TicketDataType

---
+++ packages/hello-gsm-admin/src/Types/ticket.ts
@@ -0,0 +1,14 @@
+export interface TicketDataType {
+  data: {
+    name: string;
+    birth: string;
+    application: {
+      schoolName: string;
+      screening: string;
+      registrationNumber: number;
+    };
+    application_image: {
+      idPhotoUrl: string;
+    };
+  }[];
+}
