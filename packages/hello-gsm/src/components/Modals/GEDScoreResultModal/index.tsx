62611046f3c262e196e5f625a347598fc9df37e5
yoosion030 <rsy5487@naver.com>
2022-07-14 20:42:59 +0900

    Add GEDScoreResultModal

---
+++ packages/hello-gsm/src/components/Modals/GEDScoreResultModal/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import useStore from 'Stores/StoreContainer';
+import * as S from './style';
+
+interface ResultType {
+  result?: number;
+}
+
+const GEDScoreResultModal: React.FC<ResultType> = ({ result }) => {
+  const { setShowScoreResult } = useStore();
+  const router = useRouter();
+
+  return (
+    <S.Background>
+      <S.ResultSection>
+        <S.SubjectSection>
+          <S.ResultTotal>석차백분율</S.ResultTotal>
+        </S.SubjectSection>
+        <S.SubjectSection>
+          <S.Score>{result}</S.Score>
+        </S.SubjectSection>
+        <hr />
+        <S.MainDesc>원서를 저장했습니다.</S.MainDesc>
+        <S.SubDesc>
+          ( 원서나 성적은 내정보 페이지에서 최종 제출 전에 수정할 수 있습니다. )
+        </S.SubDesc>
+        <S.ConfirmSection>
+          <S.Confirm onClick={() => router.push('/')}>홈으로</S.Confirm>
+          <S.Confirm onClick={() => setShowScoreResult()}>확인</S.Confirm>
+        </S.ConfirmSection>
+      </S.ResultSection>
+    </S.Background>
+  );
+};
+
+export default GEDScoreResultModal;
---
+++ packages/hello-gsm/src/components/Modals/GEDScoreResultModal/style.ts
@@ -0,0 +1,81 @@
+import styled from '@emotion/styled';
+import { Semester, Subject } from 'PageContainer/CalculatorPage/style';
+
+export const Background = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  background: rgba(0, 0, 0, 0.66);
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  hr {
+    width: 46px;
+    border-radius: 2px;
+    border: 2px solid #bebebe;
+  }
+`;
+
+export const ResultSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const SubjectSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const ResultSubject = styled(Semester)``;
+
+export const ResultTotal = styled(Subject)`
+  width: 127px;
+  height: 100%;
+  margin: 0;
+  line-height: 55px;
+`;
+
+export const Score = styled.div`
+  width: 127px;
+  height: 36px;
+  background-color: #484453;
+  border-radius: 6px;
+  line-height: 36px;
+  color: rgba(255, 255, 255, 0.61);
+  text-align: center;
+`;
+
+export const MainDesc = styled.h1`
+  font-size: 30px;
+  color: #ffffff;
+  font-weight: 700;
+  line-height: 50px;
+`;
+
+export const SubDesc = styled.p`
+  color: rgba(255, 255, 255, 0.69);
+`;
+
+export const Confirm = styled.a`
+  width: 150px;
+  height: 40px;
+  background-color: #ffffff;
+  color: #000000;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 10px;
+  margin-top: 50px;
+  cursor: pointer;
+`;
+
+export const ConfirmSection = styled.div`
+  width: 320px;
+  display: flex;
+  justify-content: space-between;
+`;
