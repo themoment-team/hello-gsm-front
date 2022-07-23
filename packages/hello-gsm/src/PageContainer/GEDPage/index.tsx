829ac14314ecee81468d2b20328183005211fb1c
yoosion030 <rsy5487@naver.com>
2022-07-23 22:29:42 +0900

    Add gedSubmissionAPI, LocalStorage

--- packages/hello-gsm/src/PageContainer/GEDPage/index.tsx
+++ packages/hello-gsm/src/PageContainer/GEDPage/index.tsx
@@ -1,3 +1,4 @@
+import application from 'Api/application';
 import { Header } from 'components';
 import GEDScoreResultModal from 'components/Modals/GEDScoreResultModal';
 import type { NextPage } from 'next';
@@ -8,24 +9,51 @@ import { GEDCalculate } from 'Utils/Calculate';
 import * as S from './style';
 
 interface ScoreType {
-  score: number;
-  perfectScore: number;
+  curriculumScoreSubtotal: number; // 전과목 득점
+  nonCurriculumScoreSubtotal: number; // 전과목 만점
 }
 
 const GEDPage: NextPage = () => {
-  const { register, handleSubmit } = useForm<ScoreType>();
+  const { register, handleSubmit, setValue, watch } = useForm<ScoreType>();
+
   const { showScoreResult, setShowScoreResult } = useStore();
   const [resultNumber, setResultNumber] = useState<number>(); //결과 화면 컴포넌트에 보일 점수
+  const [isSubmission, setIsSubmission] = useState(false);
 
-  const onValid = (validForm: ScoreType) => {
-    const scoreTotal = GEDCalculate(validForm.score, validForm.perfectScore);
-    setResultNumber(scoreTotal);
+  const onValid = async ({
+    curriculumScoreSubtotal,
+    nonCurriculumScoreSubtotal,
+  }: ScoreType) => {
+    const rankPercentage = GEDCalculate(
+      curriculumScoreSubtotal,
+      nonCurriculumScoreSubtotal,
+    );
+    window.localStorage.setItem(
+      'curriculumScoreSubtotal',
+      curriculumScoreSubtotal.toString(),
+    );
+    window.localStorage.setItem(
+      'nonCurriculumScoreSubtotal',
+      nonCurriculumScoreSubtotal.toString(),
+    );
+
+    setResultNumber(rankPercentage);
     setShowScoreResult();
+    try {
+      await application.postGedSubmission({
+        curriculumScoreSubtotal,
+        nonCurriculumScoreSubtotal,
+        rankPercentage,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const inValid = (Errors: FieldErrors) => {
     console.log(Errors);
   };
+
   return (
     <>
       <Header />
@@ -39,8 +67,12 @@ const GEDPage: NextPage = () => {
               <S.PerfectScoreSubject>전과목 만점 합계</S.PerfectScoreSubject>
             </S.Row>
             <S.Row>
-              <S.ScoreInput {...register('score', { required: true })} />
-              <S.ScoreInput {...register('perfectScore', { required: true })} />
+              <S.ScoreInput
+                {...register('curriculumScoreSubtotal', { required: true })}
+              />
+              <S.ScoreInput
+                {...register('nonCurriculumScoreSubtotal', { required: true })}
+              />
             </S.Row>
           </div>
           <S.Submit>저장</S.Submit>
