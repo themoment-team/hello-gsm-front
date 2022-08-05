01c8655f2e7b0d77af7362a23b7a6a24b016559c
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-06 03:09:42 +0900

    Add progress Box

--- packages/hello-gsm/src/PageContainer/InformationPage/style.ts
+++ packages/hello-gsm/src/PageContainer/InformationPage/style.ts
@@ -1,132 +1,120 @@
 import styled from '@emotion/styled';
 
 export const InformationPage = styled.div`
+  height: 100vh;
   position: relative;
   overflow-x: hidden;
 `;
 
 export const InformationContent = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export const InformationBox = styled.div`
   width: 850px;
-  height: 420px;
-  margin-top: 200px;
+  height: 630px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
-export const Number = styled.p`
-  font-weight: 700;
-  font-size: 40px;
-  color: #ffffff;
+export const InformationHeader = styled.div`
+  width: 100%;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Title = styled.div`
-  width: 170px;
-  height: 55px;
+  font-weight: 700;
+  font-size: 26px;
+  color: #ffffff;
+`;
+
+export const ProgressBox = styled.div`
+  width: 100%;
+  height: 41px;
+  position: relative;
+`;
+
+export const ProgressContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Progress = styled.div`
+  width: 90px;
+  height: 32px;
+  background: rgba(161, 161, 161, 0.42);
+  border-radius: 12px;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #f3f3f3;
-  color: #0f0921;
-  font-weight: 700;
-  font-size: 20px;
-  border-radius: 17px;
+  position: relative;
+`;
+
+export const ProgressText = styled.p`
+  font-weight: 500;
+  font-size: 14px;
+  color: #ebebeb;
+`;
+
+export const ProgressBar = styled.div`
+  width: 100%;
+  border: 2px solid rgba(255, 255, 255, 0.42);
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: -3;
 `;
 
 export const Point = styled.div`
-  width: 15px;
-  height: 15px;
-  background: #19baff;
+  width: 7px;
+  height: 7px;
+  background: #c8c8c8;
   border-radius: 100%;
-  position: relative;
-  bottom: 17px;
+  position: absolute;
+  top: 28px;
   z-index: 3;
 `;
 
-export const Content = styled.div`
-  width: 100%;
-  height: 280px;
+export const ControllerBox = styled.div`
+  width: 360px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  line-height: 35px;
-  background: #f3f3f3;
-  font-weight: 700;
-  font-size: 24px;
-  color: #0f0921;
-  text-align: center;
-  border-radius: 43px;
+  justify-content: space-between;
 `;
 
-export const Confirm = styled.a`
-  width: 230px;
-  height: 65px;
-  background: #19baff;
-  border-radius: 10px;
+export const Controller = styled.button`
+  width: 175px;
+  height: 50px;
   font-weight: 700;
-  font-size: 28px;
-  color: #ffffff;
-  padding: 18px 87px;
-  margin: 200px 0;
+  font-size: 20px;
+  color: #101010;
+  border-radius: 10px;
+  margin: 0;
+  padding: 0;
+  border: none;
   cursor: pointer;
+`;
+
+export const PrevController = styled(Controller)`
+  background: #19baff;
+  box-shadow: 0px 15px 30px -15px #19baff;
+`;
+
+export const NextController = styled(Controller)`
+  background: #dbe44e;
+  box-shadow: 0px 15px 30px -15px #dbe44e;
 `;
 
 const Ball = styled.div`
   border-radius: 100%;
   position: absolute;
   z-index: -1;
-`;
-
-export const GreenBall = styled(Ball)`
-  width: 154px;
-  height: 154px;
-  background: linear-gradient(
-    207.52deg,
-    rgba(252, 255, 173, 0.9) 13.95%,
-    rgba(222, 228, 73, 0.9) 27.08%,
-    rgba(48, 95, 2, 0.513) 91.31%
-  );
-`;
-
-export const BigBlueBall = styled(Ball)`
-  width: 459px;
-  height: 459px;
-  background: linear-gradient(
-    207.52deg,
-    rgba(93, 196, 255, 0.9) 13.95%,
-    rgba(41, 120, 214, 0.9) 37.16%,
-    rgba(10, 36, 74, 0.9) 91.31%
-  );
-`;
-
-export const SmallBlueBall = styled(Ball)`
-  width: 75px;
-  height: 75px;
-  background: linear-gradient(
-    207.52deg,
-    rgba(93, 196, 255, 0.9) 13.95%,
-    rgba(41, 120, 214, 0.9) 37.16%,
-    rgba(10, 36, 74, 0.9) 91.31%
-  );
-`;
-
-export const LightBlueBall = styled(Ball)`
-  width: 230px;
-  height: 230px;
-  top: 2075px;
-  left: 80%;
-  background: linear-gradient(
-    220deg,
-    #9cddff 10.2%,
-    #48afe4 32.84%,
-    rgba(0, 51, 77, 0.51) 88.36%
-  );
 `;
