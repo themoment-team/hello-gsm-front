fec86c6ca3ade1bb56510aa05931a50b7722f592
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-06 03:37:57 +0900

    Add InformationDescription

--- packages/hello-gsm/src/PageContainer/InformationPage/index.tsx
+++ packages/hello-gsm/src/PageContainer/InformationPage/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next';
-import { Header } from 'components';
+import { Header, InformationDescription } from 'components';
 import Link from 'next/link';
 import * as S from './style';
 import { css } from '@emotion/react';
 import useStore from 'Stores/StoreContainer';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const InformationPage: NextPage = () => {
   const { setLogged } = useStore();
   const [progressIndex, setProgressIndex] = useState<number>(1);
+  const { push } = useRouter();
 
   const progressArray = [
     '원서 작성',
@@ -18,6 +20,15 @@ const InformationPage: NextPage = () => {
     '2차 전형',
     '결과 발표',
   ];
+
+  const previusIndex = () => {
+    setProgressIndex(index => (index > 1 ? --index : index));
+  };
+
+  const nextIndex = () => {
+    progressIndex === 6 && push('/apply');
+    setProgressIndex(index => (index < 6 ? ++index : index));
+  };
 
   useEffect(() => {
     setLogged(true);
@@ -41,12 +52,13 @@ const InformationPage: NextPage = () => {
             <S.ProgressBar />
           </S.ProgressBox>
         </S.InformationHeader>
+        <InformationDescription index={progressIndex} />
         {progressIndex === (1 || 6) ? (
-          <S.NextController>다음</S.NextController>
+          <S.NextController onClick={nextIndex}>다음</S.NextController>
         ) : (
           <S.ControllerBox>
-            <S.PrevController>이전</S.PrevController>
-            <S.NextController>다음</S.NextController>
+            <S.PrevController onClick={previusIndex}>이전</S.PrevController>
+            <S.NextController onClick={nextIndex}>다음</S.NextController>
           </S.ControllerBox>
         )}
       </S.InformationContent>
