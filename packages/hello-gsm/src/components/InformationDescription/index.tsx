6acfc762ac17c6da5827d0264889c63b6b5ed871
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-07 03:58:27 +0900

    Update index case

--- packages/hello-gsm/src/components/InformationDescription/index.tsx
+++ packages/hello-gsm/src/components/InformationDescription/index.tsx
@@ -7,7 +7,17 @@ interface IndexType {
 
 const InformationDescription: React.FC<IndexType> = ({ index }) => {
   switch (index) {
+    case 0:
+      return <S.Description>{index}</S.Description>;
     case 1:
+      return <S.Description>{index}</S.Description>;
+    case 2:
+      return <S.Description>{index}</S.Description>;
+    case 3:
+      return <S.Description>{index}</S.Description>;
+    case 4:
+      return <S.Description>{index}</S.Description>;
+    case 5:
       return <S.Description>{index}</S.Description>;
     default:
       return <></>;
