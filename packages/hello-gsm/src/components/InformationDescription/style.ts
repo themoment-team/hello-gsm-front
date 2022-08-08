56e4d7ca1d12a356075d25a5bd977c0efe1e45c8
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-08 10:52:40 +0900

    Add InformationDescription style

--- packages/hello-gsm/src/components/InformationDescription/style.ts
+++ packages/hello-gsm/src/components/InformationDescription/style.ts
@@ -5,4 +5,42 @@ export const Description = styled.div`
   height: 280px;
   background: #f3f3f3;
   border-radius: 45px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const DescriptionTextSmall = styled.p`
+  width: 740px;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+  color: #0f0921;
+`;
+
+export const HighlightSmall = styled.span`
+  font-weight: 700;
+  font-size: 15px;
+`;
+
+export const DescriptionText = styled.p`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 35px;
+  color: #0f0921;
+`;
+
+export const Highlight = styled.span`
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+export const PostScript = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  color: rgba(15, 9, 33, 0.44);
+  position: absolute;
+  bottom: 45px;
 `;
