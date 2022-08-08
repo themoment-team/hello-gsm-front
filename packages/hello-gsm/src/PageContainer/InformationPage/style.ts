5ae53c4de73884812af553a750959d3a2c067358
hyeongrok7874 <khr5k0228@gmail.com>
2022-08-08 12:28:48 +0900

    Update information position

--- packages/hello-gsm/src/PageContainer/InformationPage/style.ts
+++ packages/hello-gsm/src/PageContainer/InformationPage/style.ts
@@ -17,6 +17,11 @@ export const InformationContent = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  @media (max-height: 850px) {
+    position: static;
+    transform: none;
+    margin: 50px auto;
+  }
 `;
 
 export const InformationHeader = styled.div`
