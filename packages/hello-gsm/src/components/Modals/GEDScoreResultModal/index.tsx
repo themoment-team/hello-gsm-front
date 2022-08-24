0bab377a9e0c2b1e98ac51fc0036794dca2b3338
yoosion030 <rsy5487@naver.com>
2022-08-24 20:24:15 +0900

    Update type error

--- packages/hello-gsm/src/components/Modals/GEDScoreResultModal/index.tsx
+++ packages/hello-gsm/src/components/Modals/GEDScoreResultModal/index.tsx
@@ -3,7 +3,7 @@ import useStore from 'Stores/StoreContainer';
 import * as S from './style';
 
 interface ResultType {
-  result: number[];
+  result?: number[];
 }
 
 const GEDScoreResultModal: React.FC<ResultType> = ({ result }) => {
@@ -18,8 +18,8 @@ const GEDScoreResultModal: React.FC<ResultType> = ({ result }) => {
           <S.ResultTotal>환산총점</S.ResultTotal>
         </S.Wrapper>
         <S.Wrapper>
-          <S.Score>{result[0]}</S.Score>
-          <S.Score>{result[1]}</S.Score>
+          <S.Score>{result && result[0]}</S.Score>
+          <S.Score>{result && result[1]}</S.Score>
         </S.Wrapper>
 
         <hr />
