05df3f5a2c1520c500d671022a8c4b7ebe03e3ae
yoosion030 <rsy5487@naver.com>
2022-08-29 14:25:51 +0900

    Add seoTitle, desc

--- packages/hello-gsm-calculate/src/pages/index.tsx
+++ packages/hello-gsm-calculate/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
-import { CalculatorPage } from 'components';
+import { CalculatorPage, SEOHelmet } from 'components';
 
 const Home: NextPage = () => {
-  return <CalculatorPage></CalculatorPage>;
+  const seoTitle = '모의성적 입력';
+  const desc = '모의성적 입력 페이지입니다.';
+  return (
+    <>
+      <SEOHelmet seoTitle={seoTitle} desc={desc} />
+      <CalculatorPage></CalculatorPage>
+    </>
+  );
 };
 
 export default Home;
