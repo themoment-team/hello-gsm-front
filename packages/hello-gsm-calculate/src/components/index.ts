298b2812b6d29604d75b209c42a67825030908e3
yoosion030 <rsy5487@naver.com>
2022-09-01 20:31:29 +0900

    Add PageContainer

--- packages/hello-gsm-calculate/src/components/index.ts
+++ packages/hello-gsm-calculate/src/components/index.ts
@@ -1,7 +1,7 @@
-export { default as CalculatorPage } from './CalculatorPage';
-export { default as GEDCalculatorPage } from './GEDCalculatorPage';
+export { default as CalculatorPage } from '../PageContainer/CalculatorPage';
+export { default as GEDCalculatorPage } from '../PageContainer/GEDCalculatorPage';
 export { default as ScoreSelect } from './ScoreSelect';
 export { default as GEDScoreResultModal } from './GEDScoreResultModal';
 export { default as ScoreResultModal } from './ScoreResultModal';
-export { default as NotFoundPage } from './NotFoundPage';
+export { default as NotFoundPage } from '../PageContainer/NotFoundPage';
 export { default as SEOHelmet } from './SEOHelmet';
