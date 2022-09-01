5543088dd69d8741dfd09e94a97eed657f245e40
yoosion030 <rsy5487@naver.com>
2022-09-01 22:36:04 +0900

    Add calculator/test/

--- packages/hello-gsm/src/pages/calculator/test/index.tsx
+++ packages/hello-gsm/src/pages/calculator/test/index.tsx
@@ -9,7 +9,7 @@ import { TestCalculatorPage } from 'PageContainer';
 
 const TestCalculator: NextPage<CheckType> = ({ check }) => {
   const seoTitle = '성적 입력 테스트';
-  const desc = '지원자의 성적 점수를 확인할 수 있습니다.';
+  const desc = '검정고시 지원자의 성적 점수를 확인할 수 있습니다.';
   const { setLogged } = useStore();
   useEffect(() => {
     setLogged(check);
