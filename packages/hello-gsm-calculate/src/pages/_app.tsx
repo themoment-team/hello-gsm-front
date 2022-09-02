b934c09553d73d756ea909e5d0ba69231a50932c
yoosion030 <rsy5487@naver.com>
2022-09-02 14:03:52 +0900

    Add ToastContainer

--- packages/hello-gsm-calculate/src/pages/_app.tsx
+++ packages/hello-gsm-calculate/src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from 'shared/GlobalStyle';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <ToastContainer />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
