1b29c41a53c8a87305998cc4733f5ecfb0f48f01
yoosion030 <rsy5487@naver.com>
2022-09-03 23:19:05 +0900

    Update GED SeoTitle

--- packages/hello-gsm-calculate/src/pages/calculator/ged.tsx
+++ packages/hello-gsm-calculate/src/pages/calculator/ged.tsx
@@ -3,7 +3,7 @@ import { SEOHelmet } from 'components';
 import { GEDCalculatorPage } from 'PageContainer';
 
 const GEDCalcaulator: NextPage = () => {
-  const seoTitle = '성적 입력 테스트';
+  const seoTitle = '검정고시생 성적 입력 테스트';
   const desc = '검정고시 지원자의 성적 점수를 확인할 수 있습니다.';
   return (
     <>
