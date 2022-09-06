15f948e6186d78bcce24453c3b4c989b793fc94f
yoosion030 <rsy5487@naver.com>
2022-09-06 19:58:50 +0900

    Add middleware

--- packages/hello-gsm/src/middleware.ts
+++ packages/hello-gsm/src/middleware.ts
@@ -3,6 +3,11 @@ import { NextRequest, NextResponse, userAgent } from 'next/server';
 export function middleware(req: NextRequest) {
   const { origin, pathname } = req.nextUrl;
   const { device, browser } = userAgent(req);
+
+  if (browser.name === 'IE') {
+    return NextResponse.redirect('https://hellogsm.kr/404');
+  }
+
   if (
     process.env.OPERATIONAL_STATUS === 'inspection' &&
     pathname !== '/inspection'
