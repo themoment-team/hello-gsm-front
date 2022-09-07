7eddcd71af5d9aabeb29a7323d9cba0fcd723d8b
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-07 20:20:00 +0900

    Add ImagePostLoadingModal

--- packages/hello-gsm/src/components/Modals/ImagePostLoadingModal/index.tsx
+++ packages/hello-gsm/src/components/Modals/ImagePostLoadingModal/index.tsx
@@ -1,13 +1,30 @@
-import { css } from '@emotion/react';
+import { css, Global } from '@emotion/react';
 import Image from 'next/image';
 import React from 'react';
+import useStore from 'Stores/StoreContainer';
 import * as S from './style';
 
 const ImagePostLodingModal: React.FC = () => {
+  const { showImagePostLoadingModal } = useStore();
+
   return (
     <S.ImagePostLoadingModal>
-      <Image src="/images/loading.gif" width={195} height={195} alt="loading" />
-      <S.LoadingText>이미지를 저장 중 입니다..</S.LoadingText>
+      <Global
+        styles={css`
+          body {
+            overflow: ${showImagePostLoadingModal ? 'hidden' : 'visible'};
+          }
+        `}
+      />
+      <S.ContentWrap>
+        <Image
+          src="/images/loading.gif"
+          width={195}
+          height={195}
+          alt="loading"
+        />
+        <S.LoadingText>이미지를 저장 중 입니다..</S.LoadingText>
+      </S.ContentWrap>
     </S.ImagePostLoadingModal>
   );
 };
