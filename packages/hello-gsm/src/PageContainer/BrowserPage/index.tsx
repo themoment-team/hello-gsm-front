9e07d6a52568a9fc871fc71b81a63397e0641a10
yoosion030 <rsy5487@naver.com>
2022-09-07 14:36:56 +0900

    Add browserPage

---
+++ packages/hello-gsm/src/PageContainer/BrowserPage/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import * as S from './style';
+import * as I from 'Assets/svg';
+import { Header } from 'components';
+
+const BrowserPage: NextPage = () => {
+  return (
+    <>
+      <S.BrowserPage>
+        <Header />
+        <I.ChromeLogo />
+        <p>
+          구글 크롬 브라우저를 <br /> 이용해서 접속해 주시기 바랍니다.
+        </p>
+      </S.BrowserPage>
+    </>
+  );
+};
+
+export default BrowserPage;
---
+++ packages/hello-gsm/src/PageContainer/BrowserPage/style.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const BrowserPage = styled.div`
+  width: 100vw;
+  height: 100vh
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin-top: 20px;
+    color: white;
+    text-align: center;
+    font-size: 20px;
+    line-height: 24px;
+  }
+`;
