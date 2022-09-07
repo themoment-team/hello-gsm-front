9130d16e1a451fccf858650c06c03949bc65259a
yoosion030 <rsy5487@naver.com>
2022-09-07 15:37:22 +0900

    Add Balls

--- packages/hello-gsm/src/PageContainer/BrowserPage/index.tsx
+++ packages/hello-gsm/src/PageContainer/BrowserPage/index.tsx
@@ -8,6 +8,10 @@ const BrowserPage: NextPage = () => {
     <>
       <Header />
       <S.BrowserPage>
+        <S.SkyBlueBall />
+        <S.GreenBall />
+        <S.BlueBall />
+        <S.BlueBall style={{ right: '20vw', top: '33vh' }} />
         <I.ChromeLogo />
         <p>
           구글 크롬 브라우저를 <br /> 이용해서 접속해 주시기 바랍니다.
--- packages/hello-gsm/src/PageContainer/BrowserPage/style.ts
+++ packages/hello-gsm/src/PageContainer/BrowserPage/style.ts
@@ -15,3 +15,49 @@ export const BrowserPage = styled.div`
     line-height: 24px;
   }
 `;
+
+const Ball = styled.div`
+  position: absolute;
+  z-index: -1;
+  border-radius: 50%;
+`;
+
+export const SkyBlueBall = styled(Ball)`
+  width: 6vh;
+  height: 6vh;
+  right: 53vw;
+  top: 30vh;
+  background: linear-gradient(
+    220deg,
+    #9cddff 10.2%,
+    #48afe4 32.84%,
+    rgba(0, 51, 77, 0.51) 88.36%
+  );
+`;
+
+export const GreenBall = styled(Ball)`
+  width: 13vh;
+  height: 13vh;
+  right: 30vw;
+  top: 50vh;
+  background: linear-gradient(
+    207.52deg,
+    #fcffad 13.95%,
+    #dee449 27.08%,
+    rgba(48, 95, 2, 0.57) 91.31%
+  );
+`;
+
+export const BlueBall = styled(Ball)`
+  width: 6vh;
+  height: 6vh;
+  right: 70vw;
+  top: 51vh;
+
+  background: linear-gradient(
+    207.52deg,
+    #5dc4ff 13.95%,
+    #2978d6 37.16%,
+    #0a244a 91.31%
+  );
+`;
