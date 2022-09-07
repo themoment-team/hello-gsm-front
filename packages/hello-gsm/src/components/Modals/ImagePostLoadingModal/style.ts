36318749b253b24a48bcb4a33f2a74b64afd47fc
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-07 20:20:06 +0900

    Add ImagePostLoadingModal style

--- packages/hello-gsm/src/components/Modals/ImagePostLoadingModal/style.ts
+++ packages/hello-gsm/src/components/Modals/ImagePostLoadingModal/style.ts
@@ -1,15 +1,26 @@
 import styled from '@emotion/styled';
 
 export const ImagePostLoadingModal = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background: rgba(0, 0, 0, 0.66);
   display: flex;
-  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 5;
+`;
+
+export const ContentWrap = styled.div`
+  height: 230px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
 `;
 
 export const LoadingText = styled.p`
   color: rgba(255, 255, 255, 0.79);
+  font-weight: 400;
+  font-size: 20px;
 `;
