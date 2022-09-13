d8a01fd1cf66a845b001c6bf8ed107b1f89d9984
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-13 14:03:05 +0900

    Add Logout Component

--- packages/hello-gsm-admin/src/components/index.tsx
+++ packages/hello-gsm-admin/src/components/index.tsx
@@ -5,3 +5,4 @@ export { default as ScoreModal } from './Modal/ScoreModal';
 export { default as SEOHelmet } from './SEOHelmet';
 export { default as ModalDescription } from './ModalDescription';
 export { default as ContentBox } from './ContentBox';
+export { default as Logout } from './Logout';
