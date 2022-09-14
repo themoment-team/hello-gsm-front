4d7e15c21d8af4efde7ed94d8c60e41381998c7c
유시온 <rsy5487@naver.com>
2022-09-14 20:31:44 +0900

    Update packages/hello-gsm-calculate/src/middleware.ts
    Co-authored-by: 김형록 <khr5k0228@gmail.com>

--- packages/hello-gsm-calculate/src/middleware.ts
+++ packages/hello-gsm-calculate/src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(req: NextRequest) {
   const { origin, pathname } = req.nextUrl;
   const { browser } = userAgent(req);
 
-  if (browser.name === 'IE' && pathname === '/') {
+  if (browser.name === 'IE' && pathname !== '/browser') {
     return NextResponse.redirect(`${origin}/browser`);
   }
 }
