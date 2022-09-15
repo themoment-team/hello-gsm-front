c17f72a5f71c5b5b7fbe485c2c02760307bc86ba
yoosion030 <rsy5487@naver.com>
2022-09-15 11:56:47 +0900

    Add data

--- packages/hello-gsm-admin/src/PageContainer/TicketPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/TicketPage/index.tsx
@@ -114,6 +114,15 @@ const TicketPage: NextPage<TicketDataType> = ({ data }) => {
                   수 험 표
                 </td>
               </tr>
+              <tr>
+                <td style={{ width: '15%' }}>2차 전형 (직무적성소양평가)</td>
+                <S.Left>2022.10.28.(금) 14:30 ~ 16:30</S.Left>
+                <td rowSpan={6} style={{ width: '30%', height: '100%' }}>
+                  <img src={user.application_image.idPhotoUrl} alt="없음" />
+                </td>
+                <S.Subject>접수번호</S.Subject>
+                <td>{user.application.registrationNumber && '없음'}</td>
+              </tr>
             </tbody>
           </S.Table>
           <S.Dash />
