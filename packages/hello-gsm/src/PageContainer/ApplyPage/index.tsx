a0f2dc50b5d06a98226d23af54c2f20fc48fa49d
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-15 19:36:29 +0900

    Update console.log

--- packages/hello-gsm/src/PageContainer/ApplyPage/index.tsx
+++ packages/hello-gsm/src/PageContainer/ApplyPage/index.tsx
@@ -235,7 +235,7 @@ const ApplyPage: NextPage<GetApplicationType> = ({ data }) => {
         ? push('/calculator')
         : push('/calculator/ged');
     } catch (error: any) {
-      console.log('apply error');
+      console.log('apply error!');
       // accessToken 없을 시에 accessToken 발급 후 logout 요청
       if (error.response.status === 401) {
         try {
