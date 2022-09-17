0bc6bd0024ed27a4edd2abb9c357862754418383
yoosion030 <rsy5487@naver.com>
2022-09-17 20:38:15 +0900

    Delete next/image

--- packages/hello-gsm-admin/src/PageContainer/TicketPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/TicketPage/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
 import { useEffect } from 'react';
 import { TicketDataType } from 'Types/ticket';
 import * as S from './style';
