f8f288ff00edc49970f655033706b413dd6a9c4b
yoosion030 <rsy5487@naver.com>
2022-09-17 20:39:22 +0900

    Add printIcon

--- packages/hello-gsm-admin/src/PageContainer/index.ts
+++ packages/hello-gsm-admin/src/PageContainer/index.ts
@@ -2,4 +2,3 @@ export { default as MainPage } from './MainPage';
 export { default as SignInPage } from './SignInPage';
 export { default as TicketPage } from './TicketPage';
 export { default as BrowserPage } from './BrowserPage';
-
--- packages/hello-gsm-admin/src/assets/svg/index.ts
+++ packages/hello-gsm-admin/src/assets/svg/index.ts
@@ -1,1 +1,2 @@
 export { default as ChromeLogo } from './ChromeLogo';
+export { default as PrintIcon } from './PrintIcon';
