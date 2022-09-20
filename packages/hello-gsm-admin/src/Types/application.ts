9861fbef55bdd20be9e7d48c9e0ecd8c447344c9
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-20 09:53:01 +0900

    Update ApplicantType

--- packages/hello-gsm-admin/src/Types/application.ts
+++ packages/hello-gsm-admin/src/Types/application.ts
@@ -16,6 +16,7 @@ export interface ApplicantType {
     firstResultScreening: null | string;
     guardianCellphoneNumber: string;
     isDocumentReception: boolean;
+    registrationNumber: number;
     schoolName: string | null;
     screening: '일반전형' | '사회통합전형' | '특별전형';
     teacherCellphoneNumber: string;
