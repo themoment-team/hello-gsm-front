027ad69aca1de6277b407576ddf52c202593c991
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-21 18:09:55 +0900

    Add sendScore

--- packages/hello-gsm-admin/src/components/Modal/ScoreModal/index.tsx
+++ packages/hello-gsm-admin/src/components/Modal/ScoreModal/index.tsx
@@ -1,14 +1,49 @@
+import application from 'Api/application';
+import auth from 'Api/auth';
 import ModalDescription from 'components/ModalDescription';
-import React, { useCallback, MouseEvent } from 'react';
+import React, { useCallback, MouseEvent, useRef } from 'react';
 import useStore from 'Stores/StoreContainer';
 import * as S from './style';
 
 const ScoreModal: React.FC = () => {
-  const { setShowScoreModal } = useStore();
+  const scoreRef = useRef<HTMLInputElement>(null);
+  const { setShowScoreModal, modalRegistrationNumber } = useStore();
 
   const removeClick = useCallback((e: MouseEvent<HTMLDivElement>): void => {
     e.stopPropagation();
   }, []);
+
+  const sendScore = async () => {
+    const score: number = scoreRef.current?.value
+      ? parseFloat(scoreRef.current?.value)
+      : 0;
+    const data = {
+      registrationNumber: modalRegistrationNumber,
+      personalityEvaluationScore: score,
+    };
+    if (score) return;
+    try {
+      await application.score(data);
+      setShowScoreModal();
+    } catch (error: any) {
+      // accessToken 없을 시에 accessToken 발급 후 검색 결과 요청
+      if (error.response.status === 401) {
+        try {
+          // accessToken 발급
+          await auth.refresh();
+          sendScore();
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  const validation = () => {
+    scoreRef.current?.value && sendScore();
+  };
 
   return (
     <S.ScoreModal onClick={setShowScoreModal}>
@@ -16,11 +51,14 @@ const ScoreModal: React.FC = () => {
         <S.ScoreModalContent>
           <S.ScoreWrap>
             <ModalDescription />
-            <S.ScoreInput placeholder="2차 점수를 입력해주세요." />
+            <S.ScoreInput
+              ref={scoreRef}
+              placeholder="2차 점수를 입력해주세요."
+            />
           </S.ScoreWrap>
           <S.ButtonWrap>
             <S.Cancel onClick={setShowScoreModal}>취소</S.Cancel>
-            <S.Confirm onClick={setShowScoreModal}>확인</S.Confirm>
+            <S.Confirm onClick={sendScore}>확인</S.Confirm>
           </S.ButtonWrap>
         </S.ScoreModalContent>
       </S.ScoreModalBox>
