e2a6fcdff09b54a334bb92b294424bb7f43c6171
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-21 10:26:28 +0900

    Add page state

--- packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
@@ -15,14 +15,12 @@ import application from 'Api/application';
 import auth from 'Api/auth';
 
 const MainPage: NextPage<ApplicantsType> = ({ data }) => {
-  let page = 2;
+  const [page, setPage] = useState<number>(2);
   const [applicationList, setApplicationList] = useState<ApplicantType[]>(data);
   const [isPageEnd, setIsPageEnd] = useState<boolean>(false);
   const searchRef = useRef<HTMLInputElement>(null);
   const { showPassModal, showScoreModal } = useStore();
   const loadMoreRef = useRef<HTMLDivElement>(null);
-
-  // let getApplicationList: ApplicantType[] = [];
 
   const handleObserver = useCallback(
     async (
@@ -33,11 +31,6 @@ const MainPage: NextPage<ApplicantsType> = ({ data }) => {
       if (entries[0].isIntersecting) {
         observer.unobserve(entries[0].target);
         await getList();
-        // getApplicationList &&
-        //   setApplicationList(list => [...list, ...getApplicationList]);
-        // getApplicationList === []
-        //   ? setIsPageEnd(getApplicationList === [] ? true : false)
-        //   : setIsPageEnd(false);
         observer.observe(entries[0].target);
       }
     },
@@ -66,7 +59,7 @@ const MainPage: NextPage<ApplicantsType> = ({ data }) => {
     try {
       const { data }: ApplicantsType = await application.getList(page);
       setApplicationList(list => [...list, ...data]);
-      page++;
+      setPage(prev => prev + 1);
       setIsPageEnd(data.length < 10 ? true : false);
     } catch (error: any) {
       // accessToken 없을 시에 accessToken 발급 후 가져오기 요청
