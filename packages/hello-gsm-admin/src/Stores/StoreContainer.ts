0f3530b4650a86220d1a78d6fc2b6d97442cbf1a
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-22 09:28:41 +0900

    Add scoreModalValue

--- packages/hello-gsm-admin/src/Stores/StoreContainer.ts
+++ packages/hello-gsm-admin/src/Stores/StoreContainer.ts
@@ -4,22 +4,26 @@ interface StoreType {
   showScoreModal: boolean;
   modalRegistrationNumber: number;
   modalName: string;
+  scoreModalValue: null | number;
 
   setShowScoreModal: () => void;
   setModalRegistrationNumber: (registrationNumber: number) => void;
   setModalName: (name: string) => void;
+  setScoreModalValue: (value: number) => void;
 }
 
 const useStore = create<StoreType>(set => ({
   showScoreModal: false,
   modalRegistrationNumber: 0,
   modalName: '',
+  scoreModalValue: null,
 
   setShowScoreModal: () =>
     set(state => ({ showScoreModal: !state.showScoreModal })),
   setModalRegistrationNumber: registrationNumber =>
     set({ modalRegistrationNumber: registrationNumber }),
   setModalName: name => set({ modalName: name }),
+  setScoreModalValue: value => set({ scoreModalValue: value }),
 }));
 
 export default useStore;
