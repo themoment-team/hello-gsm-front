225b5bc25e1b8b92bee8919640b0dcc078642bb5
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-22 13:20:32 +0900

    Add sendScore

--- packages/hello-gsm-admin/src/components/Modal/ScoreModal/index.tsx
+++ packages/hello-gsm-admin/src/components/Modal/ScoreModal/index.tsx
@@ -15,36 +15,37 @@ const ScoreModal: React.FC = () => {
     e.stopPropagation();
   }, []);
 
-  // const sendScore = async () => {
-  //   const score: number = scoreRef.current?.value
-  //     ? parseFloat(scoreRef.current?.value)
-  //     : 0;
-  //   const data = {
-  //     registrationNumber: modalRegistrationNumber,
-  //     personalityEvaluationScore: score,
-  //   };
-  //   if (!score) {
-  //     toast.error('점수를 입력해주세요.');
-  //     return;
-  //   }
-  //   try {
-  //     await application.score(data);
-  //     setShowScoreModal();
-  //   } catch (error: any) {
-  //     // accessToken 없을 시에 accessToken 발급 후 검색 결과 요청
-  //     if (error.response.status === 401) {
-  //       try {
-  //         // accessToken 발급
-  //         await auth.refresh();
-  //         sendScore();
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     } else {
-  //       console.log(error);
-  //     }
-  //   }
-  // };
+  const sendScore = async () => {
+    const score: number = scoreRef.current?.value
+      ? parseFloat(scoreRef.current?.value)
+      : 0;
+    const data = {
+      registrationNumber: modalRegistrationNumber,
+      personalityEvaluationScore: score,
+    };
+    if (!score) {
+      toast.error('점수를 입력해주세요.');
+      return;
+    }
+    try {
+      await application.score(data);
+      setScoreModalValue(score);
+      setShowScoreModal();
+    } catch (error: any) {
+      // accessToken 없을 시에 accessToken 발급 후 검색 결과 요청
+      if (error.response.status === 401) {
+        try {
+          // accessToken 발급
+          await auth.refresh();
+          sendScore();
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <S.ScoreModal onClick={setShowScoreModal}>
@@ -59,14 +60,7 @@ const ScoreModal: React.FC = () => {
           </S.ScoreWrap>
           <S.ButtonWrap>
             <S.Cancel onClick={setShowScoreModal}>취소</S.Cancel>
-            <S.Confirm
-              onClick={() =>
-                scoreRef.current?.value &&
-                setScoreModalValue(parseFloat(scoreRef.current?.value))
-              }
-            >
-              확인
-            </S.Confirm>
+            <S.Confirm onClick={sendScore}>확인</S.Confirm>
           </S.ButtonWrap>
         </S.ScoreModalContent>
       </S.ScoreModalBox>
