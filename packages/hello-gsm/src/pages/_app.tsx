d8c3488f77bf0ecc3a4775dd2b3c17231d279623
hyeongrok7874 <khr5k0228@gmail.com>
2022-09-26 22:56:46 +0900

    Update url type

--- packages/hello-gsm/src/pages/_app.tsx
+++ packages/hello-gsm/src/pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
+    const handleRouteChange = (url: URL) => {
       gtag.pageview(url);
     };
     router.events.on('routeChangeComplete', handleRouteChange);
