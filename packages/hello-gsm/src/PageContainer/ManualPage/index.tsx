e5a2ef82ba57be6d72d984301f50dbfe1189648e
yoosion030 <rsy5487@naver.com>
2022-10-03 00:41:29 +0900

    Add manualPage

---
+++ packages/hello-gsm/src/PageContainer/ManualPage/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from 'components';
+import type { NextPage } from 'next';
+import * as S from './style';
+
+const ManualPage: NextPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ManualPage;
