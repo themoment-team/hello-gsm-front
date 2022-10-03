ab379df1bde11de826c3519eceb966e609ab1875
yoosion030 <rsy5487@naver.com>
2022-10-03 18:06:57 +0900

    Update true

--- packages/hello-gsm/src/shared/acceptable.ts
+++ packages/hello-gsm/src/shared/acceptable.ts
@@ -1,5 +1,5 @@
-const acceptable =
-  new Date() >= new Date('2022-10-17 9:00') &&
-  new Date() <= new Date('2022-10-20 17:00');
+const acceptable = true;
+// new Date() >= new Date('2022-10-17 9:00') &&
+// new Date() <= new Date('2022-10-20 17:00');
 
 export default acceptable;
