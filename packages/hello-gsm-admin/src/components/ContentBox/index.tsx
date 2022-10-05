ac12bdc2d427b6074688bb1fbabeea963186957a
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-05 15:40:47 +0900

    Update isFinalResult for test

--- packages/hello-gsm-admin/src/components/ContentBox/index.tsx
+++ packages/hello-gsm-admin/src/components/ContentBox/index.tsx
@@ -33,7 +33,8 @@ const ContentBox: React.FC<ContentType> = ({
   //   new Date() >= new Date('2022-10-22 00:00:00') &&
   //   new Date() <= new Date('2022-11-02 00:00:00');
   const isFirstResult = true;
-  const isFinalResult: boolean = new Date() >= new Date('2022-11-02 00:00:00');
+  const isFinalResult = false;
+  // const isFinalResult: boolean = new Date() >= new Date('2022-11-02 00:00:00');
   const firstResult: '미정' | '합격' | '불합격' = !isFirstResult
     ? '미정'
     : firstResultScreening
