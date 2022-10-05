b74a0749efdc3e6e9666ad96d9b62c855244127d
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-05 10:53:05 +0900

    Update isFirstResult

--- packages/hello-gsm-admin/src/components/ContentBox/index.tsx
+++ packages/hello-gsm-admin/src/components/ContentBox/index.tsx
@@ -29,9 +29,10 @@ const ContentBox: React.FC<ContentType> = ({
     },
   },
 }) => {
-  const isFirstResult: boolean = true;
-  //    new Date() >= new Date('2022-10-22 00:00:00') &&
-  //    new Date() <= new Date('2022-11-02 00:00:00');
+  // const isFirstResult: boolean =
+  //   new Date() >= new Date('2022-10-22 00:00:00') &&
+  //   new Date() <= new Date('2022-11-02 00:00:00');
+  const isFirstResult = true;
   const isFinalResult: boolean = new Date() >= new Date('2022-11-02 00:00:00');
   const firstResult: '미정' | '합격' | '불합격' = !isFirstResult
     ? '미정'
