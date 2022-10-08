85c0b4220c6c20eeeb01f38b5649b432c1f5790c
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-09 05:13:26 +0900

    Update setLocalstorage type

--- packages/hello-gsm/src/hooks/setLocalstorage.ts
+++ packages/hello-gsm/src/hooks/setLocalstorage.ts
@@ -3,7 +3,7 @@
  * @param key localstorage key값
  * @param value localstorage에 저장할 배열값
  */
-const setLocalstorage = (key: string, value: Array<number | string>) => {
+const setLocalstorage = (key: string, value: object) => {
   window.localStorage.setItem(key, JSON.stringify(value));
 };
 
