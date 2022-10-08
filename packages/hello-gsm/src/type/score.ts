5d0d23264a5f734aa64d669da5e0029cdc2097cf
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-09 07:23:49 +0900

    Add LocalScoreType

--- packages/hello-gsm/src/type/score.ts
+++ packages/hello-gsm/src/type/score.ts
@@ -20,3 +20,18 @@ export interface GEDScoreType {
   rankPercentage: number; // 백분율
   scoreTotal: number; // 환산총점
 }
+
+export interface LocalScoreType {
+  score1_1?: number[];
+  score1_2?: number[];
+  score2_1?: number[];
+  score2_2?: number[];
+  score3_1?: number[];
+  artSportsScore: number[];
+  absentScore: number[];
+  attendanceScore: number[];
+  volunteerScore: number[];
+  freeSemester: string;
+  newSubjects: string[];
+  system: '자유학년제' | '자유학기제';
+}
