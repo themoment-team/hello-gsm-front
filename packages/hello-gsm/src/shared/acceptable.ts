051641de9e8436f98e254553dc7cd533ab9cab72
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-11 10:13:23 +0900

    Update acceptable

--- packages/hello-gsm/src/shared/acceptable.ts
+++ packages/hello-gsm/src/shared/acceptable.ts
@@ -1,5 +1,5 @@
 // 10월 17일 9시부터 10월 20일 17시 까지
-const acceptable = true;
+const acceptable = false;
 //   new Date() >= new Date('2022-10-17 9:00') &&
 //   new Date() <= new Date('2022-10-20 17:00');
 
