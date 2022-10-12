84c427df723b755570038239506928c64d01f95f
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-12 20:47:27 +0900

    Update middleware for test

--- packages/hello-gsm/src/middleware.ts
+++ packages/hello-gsm/src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
-import acceptable from 'shared/acceptable';
 
 export function middleware(req: NextRequest) {
+  // 10월 17일 9시부터 10월 20일 17시 까지
+  const acceptable =
+    new Date() >= new Date('2022-10-12 11:00') &&
+    new Date() <= new Date('2022-10-12 12:00');
   const { origin, pathname } = req.nextUrl;
   const { device, browser } = userAgent(req);
   const applicationFormURL = [
