47b0b4a677d57e33a3114e847ccea7df3bba1c30
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-12 20:56:32 +0900

    Update acceptable

--- packages/hello-gsm/src/shared/acceptable.ts
+++ packages/hello-gsm/src/shared/acceptable.ts
@@ -1,6 +1,6 @@
 // 10월 17일 9시부터 10월 20일 17시 까지
 const acceptable =
   new Date() >= new Date('2022-10-12 20:50') &&
-  new Date() <= new Date('2022-10-12 20:55');
+  new Date() <= new Date('2022-10-12 21:00');
 
 export default acceptable;
