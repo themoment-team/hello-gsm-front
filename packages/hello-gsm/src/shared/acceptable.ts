0de83918996cc08bdfe12b28e939bab4044abe55
yoosion030 <rsy5487@naver.com>
2022-10-12 20:50:07 +0900

    Update date

--- packages/hello-gsm/src/shared/acceptable.ts
+++ packages/hello-gsm/src/shared/acceptable.ts
@@ -1,6 +1,6 @@
 // 10월 17일 9시부터 10월 20일 17시 까지
 const acceptable =
-  new Date() >= new Date('2022-10-12 20:00') &&
-  new Date() <= new Date('2022-10-12 20:47');
+  new Date() >= new Date('2022-10-12 20:50') &&
+  new Date() <= new Date('2022-10-12 20:55');
 
 export default acceptable;
