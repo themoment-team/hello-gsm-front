966aae44e7ca0f16ffbe281840bc0db531e78387
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-23 05:38:22 +0900

    Add responsive design

--- packages/hello-gsm/src/components/Modals/MainNonLoginModal/style.ts
+++ packages/hello-gsm/src/components/Modals/MainNonLoginModal/style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import device from 'shared/config';
 
 export const MainResultModal = styled.div`
   width: 100%;
@@ -18,6 +19,12 @@ export const MainResultModalBox = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  @media (max-width: 710px) {
+    width: 90%;
+  }
+  @media ${device.mobile} {
+    height: 300px;
+  }
 `;
 
 export const MainResultModalContent = styled.div`
@@ -37,6 +44,9 @@ export const Text = styled.p`
   line-height: 29px;
   text-align: center;
   color: #0f0921;
+  @media ${device.mobile} {
+    font-size: 16px;
+  }
 `;
 
 export const InvisibleButtonWrap = styled.div`
@@ -57,12 +67,20 @@ export const InvisibleButton = styled.button`
   border: none;
   cursor: pointer;
   padding: 0;
+  @media ${device.mobile} {
+    width: 125px;
+    font-size: 13px;
+  }
   &::before {
     content: '';
     width: 15px;
     height: 15px;
     background: #c9c9c9;
     border-radius: 4px;
+    @media ${device.mobile} {
+      width: 12px;
+      height: 12px;
+    }
   }
 `;
 
@@ -74,4 +92,9 @@ export const ConfirmButton = styled.button`
   font-weight: 700;
   font-size: 20px;
   color: #ffffff;
+  @media ${device.mobile} {
+    width: 130px;
+    height: 37px;
+    font-size: 16px;
+  }
 `;
