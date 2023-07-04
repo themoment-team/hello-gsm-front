d03e2129eac597bf522086543433d4831d4069a2
yoosion030 <yoosioff@gmail.com>
2023-07-04 15:19:23 +0900

    Update Icon size

--- packages/hello-gsm/src/Assets/svg/DownloadIcon.tsx
+++ packages/hello-gsm/src/Assets/svg/DownloadIcon.tsx
@@ -1,7 +1,7 @@
 const DownloadIcon = () => (
   <svg
-    width="14"
-    height="18"
+    width="0.875rem"
+    height="1.125rem"
     viewBox="0 0 14 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
