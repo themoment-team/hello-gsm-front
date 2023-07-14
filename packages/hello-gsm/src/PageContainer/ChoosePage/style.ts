7351944b498d856ba859e9a4aefb696d6878b1c1
yoosion030 <yoosioff@gmail.com>
2023-07-14 19:36:38 +0900

    Update choosePage button

--- packages/hello-gsm/src/PageContainer/ChoosePage/style.ts
+++ packages/hello-gsm/src/PageContainer/ChoosePage/style.ts
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 
 export const ChoosePage = styled.div`
-  height: calc(100vh - 4.375rem);
+  height: calc(100vh - 70px);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 2.5rem;
+  gap: 40px;
 `;
 
 export const ChooseTitle = styled.h1`
@@ -16,19 +16,19 @@ export const ChooseTitle = styled.h1`
 
 export const ChooseSection = styled.div`
   display: flex;
-  width: 31.25rem;
-  height: 17.5rem;
+  width: 500px;
+  height: 280px;
   justify-content: space-between;
 `;
 
 export const ChooseButton = styled.button`
   width: 30%;
-  height: 3.8125rem;
+  height: 61px;
   background-color: #484453;
   color: rgba(255, 255, 255, 0.45);
-  border-radius: 0.375rem;
-  line-height: 3.8125rem;
+  border-radius: 6px;
+  line-height: 61px;
   border: none;
   cursor: pointer;
-  font-size: 1.25rem;
+  font-size: 20px;
 `;
