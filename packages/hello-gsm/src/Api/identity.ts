621816c9d92dceae1d4b9dcfcaba80c7c44177dd
Lee-Seungje <moondgod@gmail.com>
2023-07-14 21:03:11 +0900

    Add identity

---
+++ packages/hello-gsm/src/Api/identity.ts
@@ -0,0 +1,66 @@
+import BASE_URL from 'shared/baseURL';
+import { SignUpType } from 'type/signup';
+import RequestApi from 'Utils/Libs/requestApi';
+import { AuthController } from 'Utils/Libs/requestUrls';
+
+class Auth {
+  /**
+   * @returns 각 oauth 로그인 URL을 반환한다
+   */
+  kakaoSignin() {
+    return BASE_URL + AuthController.kakao.signin();
+  }
+
+  googleSignin() {
+    return BASE_URL + AuthController.google.signin();
+  }
+
+  guthubSignin() {
+    return BASE_URL + AuthController.github.signin();
+  }
+
+  /**
+   * 로그아웃을 위한 api
+   */
+  logout() {
+    return BASE_URL + AuthController.github.signin();
+  }
+
+  /**
+   * 토큰 재발급을 위한 api
+   */
+  refresh(refreshToken?: string) {
+    try {
+      return RequestApi(
+        {
+          method: 'POST',
+          url: AuthController.refresh(),
+        },
+        refreshToken,
+      );
+    } catch (error: any) {
+      return error;
+    }
+  }
+
+  /**
+   *
+   * @param accessToken - api 요청을 하기 위한 토큰
+   * @returns - 로그인 여부 확인
+   */
+  check(accessToken?: string) {
+    try {
+      return RequestApi(
+        {
+          method: 'GET',
+          url: AuthController.check(),
+        },
+        accessToken,
+      );
+    } catch (error: any) {
+      return error;
+    }
+  }
+}
+
+export default new Auth();
