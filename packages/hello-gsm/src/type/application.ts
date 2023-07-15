d13c591a311c40773cb54a2430dbfb2615c4e7c9
Lee-Seungje <moondgod@gmail.com>
2023-07-15 23:18:18 +0900

    Update application type

--- packages/hello-gsm/src/type/application.ts
+++ packages/hello-gsm/src/type/application.ts
@@ -1,99 +1,160 @@
-export interface ApplicationType {
-  application: {
-    teacherCellphoneNumber?: string;
-    schoolName?: string;
-    guardianCellphoneNumber: string;
-    screening: '일반전형' | '사회통합전형' | '특별전형';
-  };
-  applicationDetail: {
-    telephoneNumber?: string;
+type MajorType = 'SW' | 'IOT' | 'AI' | '';
+
+type GraduationStatusType = 'CANDIDATE' | 'GRADUATE' | 'GED';
+
+type ScreeningType =
+  | 'GENERAL'
+  | 'SOCIAL'
+  | 'SPECIAL_VETERANS'
+  | 'SPECIAL_ADMISSION';
+
+type EvaluationStatusType = 'NOT_YET' | 'PASS' | 'FALL';
+
+interface CommonApplicationResponseType {
+  id: number;
+  admissionInfo: {
+    applicantName: string;
+    applicantGender: 'MALE' | 'FEMALE';
+    applicantBirth: Date;
     address: string;
-    addressDetails?: string;
+    detailAddress: string;
+    graduation: GraduationStatusType;
+    telephone: string;
+    applicantPhoneNumber: string;
     guardianName: string;
-    guardianRelation: string;
-    educationStatus: '졸업예정' | '졸업' | '검정고시';
-    graduationYear: string;
-    graduationMonth: string;
-    firstWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과' | '';
-    secondWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과' | '';
-    thirdWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과' | '';
-    teacherName?: string;
-    schoolLocation?: string;
+    relationWithApplicant: string;
+    guardianPhoneNumber: string;
+    teacherName: string;
+    teacherPhoneNumber: string;
+    schoolName: string;
+    schoolLocation: string;
+    applicantImageUri: string;
+    desiredMajor: {
+      firstDesiredMajor: MajorType;
+      secondDesiredMajor: MajorType;
+      thirdDesiredMajor: MajorType;
+    };
+    screening: ScreeningType;
+  };
+  middleSchoolGrade: string;
+  admissionStatus: {
+    isFinalSubmitted: boolean;
+    isPrintsArrived: boolean;
+    firstEvaluation: EvaluationStatusType;
+    secondEvaluation: EvaluationStatusType;
+    screeningSubmittedAt: ScreeningType | null;
+    screeningFirstEvaluationAt: ScreeningType | null;
+    screeningSecondEvaluationAt: ScreeningType | null;
+    registrationNumber: number | null;
+    secondScore: number | null;
+    finalMajor: MajorType | null;
+  };
+}
+
+export interface GEDApplicationResponseType
+  extends CommonApplicationResponseType {
+  middleSchoolGrade: string;
+  admissionGrade: {
+    totalScore: number;
+    percentileRank: number;
+    gedTotalScore: number;
+    gedMaxScore: number;
+  };
+}
+
+export interface ApplicationResponseType extends CommonApplicationResponseType {
+  admissionGrade: {
+    totalScore: number;
+    percentileRank: number;
+    grade1Semester1Score: number;
+    grade1Semester2Score: number;
+    grade2Semester1Score: number;
+    grade2Semester2Score: number;
+    grade3Semester1Score: number;
+    artisticScore: number;
+    curricularSubtotalScore: number;
+    attendanceScore: number;
+    volunteerScore: number;
+    extracurricularSubtotalScore: number;
   };
 }
 
 export interface ApplyFormType {
-  addressDetails: string;
-  telephoneNumber: string;
-  screening: '일반전형' | '사회통합전형' | '특별전형';
-  graduationYear: string;
-  graduationMonth: string;
-  educationStatus: '졸업예정' | '졸업' | '검정고시';
+  applicantImageUri: string;
+  address: string;
+  detailAddress: string;
+  graduation: GraduationStatusType;
+  telephone: string;
   guardianName: string;
-  guardianRelation: string;
-  guardianCellphoneNumber: string;
-  teacherName: string | null;
-  teacherCellphoneNumber: string | null;
+  relationWithApplicant: string;
+  guardianPhoneNumber: string;
+  teacherName: string;
+  teacherPhoneNumber: string;
+  firstDesiredMajor: MajorType;
+  secondDesiredMajor: MajorType;
+  thirdDesiredMajor: MajorType;
+  middleSchoolGrade: string;
+  schoolName: string;
+  schoolLocation: string;
+  screening: ScreeningType;
 }
 
 export interface GetApplicationType {
-  data: {
+  user_idx: number;
+  userImg: string;
+  name: string;
+  birth: Date;
+  gender: 'MALE' | 'FEMALE';
+  cellphoneNumber: string;
+  application_image?: {
+    idPhotoUrl: string;
+  };
+  application?: {
+    applicationIdx: number;
+    registrationNumber?: number;
+    isFinalSubmission?: boolean;
+    isDocumentReception?: boolean;
+    firstResultScreening?: string;
+    finalResultScreening?: string;
+    guardianCellphoneNumber: string;
+    teacherCellphoneNumber?: string;
+    schoolName?: string;
+    screening: ScreeningType;
     user_idx: number;
-    userImg: string;
-    name: string;
-    birth: Date;
-    gender: '남자' | '여자';
-    cellphoneNumber: string;
-    application_image?: {
-      idPhotoUrl: string;
-    };
-    application?: {
+    application_score?: {
       applicationIdx: number;
-      registrationNumber?: number;
-      isFinalSubmission?: boolean;
-      isDocumentReception?: boolean;
-      firstResultScreening?: string;
-      finalResultScreening?: string;
-      guardianCellphoneNumber: string;
-      teacherCellphoneNumber?: string;
-      schoolName?: string;
-      screening: '일반전형' | '사회통합전형' | '특별전형';
-      user_idx: number;
-      application_score?: {
-        applicationIdx: number;
-        score1_1: number;
-        score1_2: number;
-        score2_1: number;
-        score2_2: number;
-        score3_1: number;
-        score3_2: number;
-        generalCurriculumScoreSubtotal: number;
-        artSportsScore: number;
-        curriculumScoreSubtotal: number;
-        attendanceScore: number;
-        volunteerScore: number;
-        nonCurriculumScoreSubtotal: number;
-        personalityEvaluationScore?: number;
-        scoreTotal: number;
-        rankPercentage: number;
-      };
-      application_details?: {
-        applicationIdx: number;
-        address: string;
-        addressDetails?: string;
-        telephoneNumber?: string;
-        guardianName: string;
-        guardianRelation: string;
-        teacherName?: string;
-        schoolLocation?: string;
-        educationStatus: '졸업예정' | '졸업' | '검정고시';
-        graduationYear: string;
-        graduationMonth: string;
-        firstWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과';
-        secondWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과';
-        thirdWantedMajor: '소프트웨어개발과' | '스마트IoT과' | '인공지능과';
-        majorResult?: string;
-      };
+      score1_1: number;
+      score1_2: number;
+      score2_1: number;
+      score2_2: number;
+      score3_1: number;
+      score3_2: number;
+      generalCurriculumScoreSubtotal: number;
+      artSportsScore: number;
+      curriculumScoreSubtotal: number;
+      attendanceScore: number;
+      volunteerScore: number;
+      nonCurriculumScoreSubtotal: number;
+      personalityEvaluationScore?: number;
+      scoreTotal: number;
+      rankPercentage: number;
+    };
+    application_details?: {
+      applicationIdx: number;
+      address: string;
+      addressDetails?: string;
+      telephoneNumber?: string;
+      guardianName: string;
+      guardianRelation: string;
+      teacherName?: string;
+      schoolLocation?: string;
+      educationStatus: EvaluationStatusType;
+      graduationYear: string;
+      graduationMonth: string;
+      firstWantedMajor: MajorType;
+      secondWantedMajor: MajorType;
+      thirdWantedMajor: MajorType;
+      majorResult?: string;
     };
   };
 }
