98546b8b6f8ea8a722b946f4b7b3f8d1f9a7a12b
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-07-26 13:50:21 +0900

    Update SignInPage

--- packages/hello-gsm-admin/src/PageContainer/SignInPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/SignInPage/index.tsx
@@ -1,69 +1,27 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import * as S from './style';
-import { useForm } from 'react-hook-form';
-import auth from 'Api/auth';
-import { LoginType } from 'Types/user';
-import { css } from '@emotion/react';
-import { useRouter } from 'next/router';
-import { toast } from 'react-toastify';
+import * as I from 'assets/svg';
+import GoogleLoginButton from 'components/GoogleLoginButton';
 
 const SignInPage: NextPage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginType>();
-
-  const { push } = useRouter();
-
-  // 로그인 시도
-  const onValid = async ({ id, password }: LoginType) => {
-    try {
-      await auth.signin({ id, password });
-      toast.success('로그인을 성공하였습니다.');
-      push('/');
-    } catch (err: any) {
-      console.error(err);
-      toast.error('아이디 혹은 비밀번호가 잘못되었습니다.');
-    }
-  };
-
-  const SelectError = () =>
-    css({
-      animation: `${S.shake} 0.3s`,
-    });
-
   return (
     <S.SignInPage>
-      <S.BigBall />
-      <S.SmallBall />
-      <S.SignInForm onSubmit={handleSubmit(onValid)}>
-        <S.Title>로그인</S.Title>
-        <S.Input
-          placeholder="아이디를 입력해주세요."
-          type="text"
-          {...register('id', {
-            required: '* 아이디를 입력해주세요.',
-            // minLength: 6,
-            // maxLength: 12,
-          })}
-        />
-        <S.Error css={errors.id && SelectError}>{errors.id?.message}</S.Error>
-        <S.Input
-          placeholder="비밀번호를 입력해주세요."
-          type="password"
-          autoComplete="on"
-          {...register('password', {
-            required: '* 비밀번호를 입력해주세요.',
-            // minLength: 8,
-            // maxLength: 16,
-          })}
-        />
-        <S.Error css={errors.password && SelectError}>
-          {errors.password?.message}
-        </S.Error>
-        <S.Submit>로그인</S.Submit>
-      </S.SignInForm>
+      <S.ContentWrap>
+        <S.LoginContent>
+          <S.LoginTitleWrap>
+            <Image
+              src="/favicon.png"
+              width={36}
+              height={36}
+              alt="hellogsm로고"
+            />
+            <S.AdminTitle>Hello, GSM Admin</S.AdminTitle>
+          </S.LoginTitleWrap>
+          <S.LoginTitle>로그인</S.LoginTitle>
+        </S.LoginContent>
+        <GoogleLoginButton>구글계정으로 로그인</GoogleLoginButton>
+      </S.ContentWrap>
     </S.SignInPage>
   );
 };
