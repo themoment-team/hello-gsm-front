7c17054e5dd72301d0897a42ea365aa775ca6df6
Lee-Seungje <moondgod@gmail.com>
2023-07-28 09:20:41 +0900

    Update testPage

--- packages/hello-gsm-admin/src/pages/test.tsx
+++ packages/hello-gsm-admin/src/pages/test.tsx
@@ -1,8 +1,23 @@
 import { NextPage } from 'next';
 import { ListHeader } from 'components';
 
+import styled from '@emotion/styled';
+
 const Test: NextPage = () => {
-  return <ListHeader />;
+  return (
+    <Container>
+      <ListHeader />
+    </Container>
+  );
 };
 
 export default Test;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+`;
