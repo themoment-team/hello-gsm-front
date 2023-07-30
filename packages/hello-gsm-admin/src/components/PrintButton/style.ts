632033b7a1f95b39aeac498f13ff75484ddab2b6
Lee-Seungje <moondgod@gmail.com>
2023-07-30 21:23:20 +0700

    Update button style

--- packages/hello-gsm-admin/src/components/PrintButton/style.ts
+++ packages/hello-gsm-admin/src/components/PrintButton/style.ts
@@ -10,6 +10,33 @@ export const PrintButton = styled.button`
   align-items: center;
   cursor: pointer;
   border: 0;
+
+  &:hover {
+    background: #d6e8ff;
+    span {
+      color: #2174d8;
+    }
+    path {
+      fill: #2174d8;
+    }
+    #x {
+      fill: #d6e8ff;
+    }
+    transition: ease-in-out 0.2s;
+  }
+
+  &:disabled {
+    background: #e0e0e0;
+    span {
+      color: #9e9e9e;
+    }
+    path {
+      fill: #9e9e9e;
+    }
+    #x {
+      fill: #e0e0e0;
+    }
+  }
 `;
 
 export const PrintText = styled.span`
