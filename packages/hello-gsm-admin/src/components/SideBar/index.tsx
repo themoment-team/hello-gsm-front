731ebf49045d4e3d6c933f2c2631a676edeb6849
예빈 <yebin060310@gmail.com>
2023-07-31 14:54:42 +0900

    Delete fragment

--- packages/hello-gsm-admin/src/components/SideBar/index.tsx
+++ packages/hello-gsm-admin/src/components/SideBar/index.tsx
@@ -3,21 +3,19 @@ import * as S from './style';
 import * as I from 'Assets/svg';
 const SlideBar: React.FC = () => {
   return (
-    <>
-      <S.SideBar>
-        <S.Title>Hello, GSM Admin</S.Title>
-        <S.ApplicantListBox>
-          <I.DocumentIcon />
-          <S.ApplicantList>지원자 리스트</S.ApplicantList>
-        </S.ApplicantListBox>
-        <S.LogoutBox>
-          <S.InnerLogoutBox>
-            <I.LogoutIcon />
-            <S.Logout>로그아웃</S.Logout>
-          </S.InnerLogoutBox>
-        </S.LogoutBox>
-      </S.SideBar>
-    </>
+    <S.SideBar>
+      <S.Title>Hello, GSM Admin</S.Title>
+      <S.ApplicantListBox>
+        <I.DocumentIcon />
+        <S.ApplicantList>지원자 리스트</S.ApplicantList>
+      </S.ApplicantListBox>
+      <S.LogoutBox>
+        <S.InnerLogoutBox>
+          <I.LogoutIcon />
+          <S.Logout>로그아웃</S.Logout>
+        </S.InnerLogoutBox>
+      </S.LogoutBox>
+    </S.SideBar>
   );
 };
 
