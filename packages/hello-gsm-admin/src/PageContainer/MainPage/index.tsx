b00ecfccef8df1d38ce89bb4c3c159024874609a
신희성 <vvwv3092@gmail.com>
2023-08-08 16:37:25 +0900

    Delete Dummy Data

--- packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
@@ -9,64 +9,8 @@ import application from 'Api/application';
 import auth from 'Api/auth';
 import { isStartFirstResult } from 'shared/acceptable';
 
-const ListDummyData: ApplicantType[] = [
-  {
-    cellphoneNumber: '01038157596',
-    name: '신희성',
-    application: {
-      applicationIdx: 1,
-      finalResultScreening: '합격',
-      firstResultScreening: '합격',
-      guardianCellphoneNumber: '01000000000',
-      isDocumentReception: true,
-      registrationNumber: 0,
-      schoolName: '금호중앙중학교',
-      screening: '일반전형',
-      teacherCellphoneNumber: '01000000000',
-      application_score: null,
-    },
-  },
-  {
-    cellphoneNumber: '01000000000',
-    name: '방가온',
-    application: {
-      applicationIdx: 2,
-      finalResultScreening: null,
-      firstResultScreening: null,
-      guardianCellphoneNumber: '01000000000',
-      isDocumentReception: false,
-      registrationNumber: 0,
-      schoolName: '수완중학교',
-      screening: '사회통합전형',
-      teacherCellphoneNumber: '01000000000',
-      application_score: {
-        personalityEvaluationScore: '99.99',
-      },
-    },
-  },
-  {
-    cellphoneNumber: '01000000000',
-    name: '박미리',
-    application: {
-      applicationIdx: 3,
-      finalResultScreening: '합격',
-      firstResultScreening: '합격',
-      guardianCellphoneNumber: '01000000000',
-      isDocumentReception: true,
-      registrationNumber: 0,
-      schoolName: '효광중학교',
-      screening: '일반전형',
-      teacherCellphoneNumber: '01000000000',
-      application_score: {
-        personalityEvaluationScore: '12.09',
-      },
-    },
-  },
-];
-
 const MainPage: NextPage<ApplicantsType> = ({ list, count }) => {
-  const [applicationList, setApplicationList] =
-    useState<ApplicantType[]>(ListDummyData);
+  const [applicationList, setApplicationList] = useState<ApplicantType[]>(list);
   const [isPageEnd, setIsPageEnd] = useState<boolean>(false);
   const loadMoreRef = useRef<HTMLDivElement>(null);
   const { showScoreModal } = useStore();
