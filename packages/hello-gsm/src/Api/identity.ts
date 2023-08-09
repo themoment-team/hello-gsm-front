c940785117128a8152a99bc886690564f55ebb83
yoosion030 <yoosioff@gmail.com>
2023-08-09 15:17:17 +0900

    Delete accessToken params

--- packages/hello-gsm/src/Api/identity.ts
+++ packages/hello-gsm/src/Api/identity.ts
@@ -8,123 +8,99 @@ interface newSignUpType extends SignUpType {
 
 class Identity {
   /**
-   * @param accessToken
    * @param phonNumber - 사용자의 전화번호
    * @returns 본인인증 코드를 발송합니다.
    * 테스트 환경에서만 사용 가능합니다
    */
-  postCode(phonNumber: string, accessToken?: string) {
+  postCode(phonNumber: string) {
     try {
-      return RequestApi(
-        {
-          method: 'POST',
-          url: IdentityController.sendCode(),
-          data: {
-            phonNumber,
-          },
+      return RequestApi({
+        method: 'POST',
+        url: IdentityController.sendCode(),
+        data: {
+          phonNumber,
         },
-        accessToken,
-      );
+      });
     } catch (error: any) {
       return error;
     }
   }
 
   /**
-   * @param accessToken
    * @param phonNumber - 사용자의 전화번호
    * @returns 현재 사용자의 본인인증 코드를 SMS로 발신하고, 본인인증 코드를 가져옵니다.
    */
-  postCodeTest(phonNumber: string, accessToken?: string) {
+  postCodeTest(phonNumber: string) {
     try {
-      return RequestApi(
-        {
-          method: 'POST',
-          url: IdentityController.sendCodeTest(),
-          data: {
-            phonNumber,
-          },
+      return RequestApi({
+        method: 'POST',
+        url: IdentityController.sendCodeTest(),
+        data: {
+          phonNumber,
         },
-        accessToken,
-      );
+      });
     } catch (error: any) {
       return error;
     }
   }
 
   /**
-   * @param accessToken
    * @param code - 인증 코드
    * @returns 휴대폰으로 발송된 인증 코드를 인증합니다.
    */
-  postAuthCode(code: string, accessToken?: string) {
+  postAuthCode(code: string) {
     try {
-      return RequestApi(
-        {
-          method: 'POST',
-          url: IdentityController.authCode(),
-          data: {
-            code,
-          },
+      return RequestApi({
+        method: 'POST',
+        url: IdentityController.authCode(),
+        data: {
+          code,
         },
-        accessToken,
-      );
+      });
     } catch (error: any) {
       return error;
     }
   }
 
   /**
-   * @param accessToken
    * @returns 현재 사용자의 본인인증 정보를 가져옵니다.
    */
-  getMyIdentity(accessToken?: string) {
+  getMyIdentity() {
     try {
-      return RequestApi(
-        {
-          method: 'GET',
-          url: IdentityController.myIdentity(),
-        },
-        accessToken,
-      );
+      return RequestApi({
+        method: 'GET',
+        url: IdentityController.myIdentity(),
+      });
     } catch (error: any) {
       return error;
     }
   }
 
   /**
-   * @param accessToken
    * @param data - 회원가입에 필요한 파라미터
    */
-  postMyIdentity(data: newSignUpType, accessToken?: string) {
+  postMyIdentity(data: newSignUpType) {
     try {
-      return RequestApi(
-        {
-          method: 'POST',
-          url: IdentityController.myIdentity(),
-          data,
-        },
-        accessToken,
-      );
+      return RequestApi({
+        method: 'POST',
+        url: IdentityController.myIdentity(),
+        data,
+      });
     } catch (error: any) {
       return error;
     }
   }
 
   /**
-   * @param accessToken
    * @param data - 회원가입에 필요한 파라미터
    */
-  putMyIdentity(data: newSignUpType, accessToken?: string) {
+  putMyIdentity(data: newSignUpType) {
     try {
-      return RequestApi(
-        {
-          method: 'PUT',
-          url: IdentityController.myIdentity(),
-          data,
-        },
-        accessToken,
-      );
+      return RequestApi({
+        method: 'PUT',
+        url: IdentityController.myIdentity(),
+        data,
+      });
     } catch (error: any) {
       return error;
     }
