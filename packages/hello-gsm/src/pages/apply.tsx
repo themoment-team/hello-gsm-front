4499e37dd5ce97cceaf2988ba92cd500ae123804
yoosion030 <yoosioff@gmail.com>
2023-08-22 11:05:21 +0900

    Add DummyData

--- packages/hello-gsm/src/pages/apply.tsx
+++ packages/hello-gsm/src/pages/apply.tsx
@@ -22,7 +22,7 @@ const Apply: NextPage<ApplyProps> = ({ applicationData, identityData }) => {
 
   const [step, setStep] = useState<'원서' | '성적'>('원서');
 
-  usePreventBackAndClose();
+  // usePreventBackAndClose();
 
   return (
     <>
@@ -60,13 +60,62 @@ export const getServerSideProps: GetServerSideProps = async () => {
     } else {
       return {
         props: {
-          data: { applicationData, identityData },
+          applicationData,
+          identityData,
         },
       };
     }
   } catch (error) {
     return {
-      props: {},
+      props: {
+        applicationData: {
+          id: 1,
+          admissionInfo: {
+            applicantName: 'human',
+            applicantGender: 'MALE',
+            applicantBirth: '2023-06-30',
+            address: '광주광역시 광산구 송정동 상무대로 312',
+            detailAddress: '이세상 어딘가',
+            graduation: 'GED',
+            telephone: '01012341234',
+            applicantPhoneNumber: '01012341234',
+            guardianName: '홍길동',
+            relationWithApplicant: '모',
+            guardianPhoneNumber: '01012341234',
+            teacherName: '홍길동',
+            teacherPhoneNumber: '01012341234',
+            schoolName: '광소마중',
+            schoolLocation: '광주 송정동 광소마중',
+            applicantImageUri: null,
+            desiredMajor: {
+              firstDesiredMajor: 'SW',
+              secondDesiredMajor: 'AI',
+              thirdDesiredMajor: 'IOT',
+            },
+            screening: 'SPECIAL_ADMISSION',
+          },
+          middleSchoolGrade:
+            '{"curriculumScoreSubtotal":100,"nonCurriculumScoreSubtotal":100,"rankPercentage":0,"scoreTotal":261}',
+          admissionGrade: {
+            totalScore: 261,
+            percentileRank: 0,
+            gedTotalScore: 100,
+            gedMaxScore: 100,
+          },
+          admissionStatus: {
+            isFinalSubmitted: false,
+            isPrintsArrived: false,
+            firstEvaluation: 'NOT_YET',
+            secondEvaluation: 'NOT_YET',
+            screeningSubmittedAt: null,
+            screeningFirstEvaluationAt: null,
+            screeningSecondEvaluationAt: null,
+            registrationNumber: null,
+            secondScore: null,
+            finalMajor: null,
+          },
+        },
+      },
     };
   }
 };
