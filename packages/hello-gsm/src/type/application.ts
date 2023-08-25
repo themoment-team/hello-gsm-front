a63127ca19b49aa02b50a84f63b10c1c03bebe63
yoosion030 <yoosioff@gmail.com>
2023-08-25 09:42:01 +0900

    Add MajorType, ApplyFormType, ApplicationFormType

--- packages/hello-gsm/src/type/application.ts
+++ packages/hello-gsm/src/type/application.ts
@@ -1,4 +1,4 @@
-type MajorType = 'SW' | 'IOT' | 'AI' | '';
+export type MajorType = 'SW' | 'IOT' | 'AI' | '';
 
 export type GraduationStatusType = 'CANDIDATE' | 'GRADUATE' | 'GED';
 
@@ -11,48 +11,6 @@ export type ScreeningType =
   | 'SPECIAL_ADMISSION';
 
 export type EvaluationStatusType = 'NOT_YET' | 'PASS' | 'FALL';
-
-export interface CommonApplicationResponseType {
-  id: number;
-  admissionInfo: {
-    applicantName: string;
-    applicantGender: GenderType;
-    applicantBirth: Date;
-    address: string;
-    detailAddress: string;
-    graduation: GraduationStatusType;
-    telephone?: string;
-    applicantPhoneNumber: string;
-    guardianName: string;
-    relationWithApplicant: string;
-    guardianPhoneNumber: string;
-    teacherName?: string;
-    teacherPhoneNumber?: string;
-    schoolName?: string;
-    schoolLocation?: string;
-    applicantImageUri: string;
-    desiredMajor: {
-      firstDesiredMajor: MajorType;
-      secondDesiredMajor: MajorType;
-      thirdDesiredMajor: MajorType;
-    };
-    screening: ScreeningType;
-  };
-  middleSchoolGrade: string;
-  admissionGrade: GEDScore | CommonScore;
-  admissionStatus: {
-    isFinalSubmitted: boolean;
-    isPrintsArrived: boolean;
-    firstEvaluation: EvaluationStatusType;
-    secondEvaluation: EvaluationStatusType;
-    screeningSubmittedAt: ScreeningType | null;
-    screeningFirstEvaluationAt: ScreeningType | null;
-    screeningSecondEvaluationAt: ScreeningType | null;
-    registrationNumber: number | null;
-    secondScore: number | null;
-    finalMajor: MajorType | null;
-  };
-}
 
 export interface GEDScore {
   totalScore: number;
@@ -76,42 +34,45 @@ export interface CommonScore {
   extracurricularSubtotalScore: number;
 }
 
-export interface ApplyFormType {
+export interface ApplicationFormType {
   applicantImageUri: string;
   address: string;
   detailAddress: string;
   graduation: GraduationStatusType;
-  telephone: string;
+  telephone: string | null;
   guardianName: string;
   relationWithApplicant: string;
   guardianPhoneNumber: string;
-  teacherName: string;
-  teacherPhoneNumber: string;
-  firstDesiredMajor: MajorType;
+  teacherName: string | null;
+  teacherPhoneNumber: string | null;
+  firstDesiredMajor: MajorType | null;
   secondDesiredMajor: MajorType;
   thirdDesiredMajor: MajorType;
-  schoolName: string;
-  schoolLocation: string;
+  schoolName: string | null;
+  schoolLocation: string | null;
   screening: ScreeningType | 'SPECIAL' | '';
+  middleSchoolGrade: string;
 }
+
+export type ApplyFormType = Omit<ApplicationFormType, 'middleSchoolGrade'>;
 
 export interface AdmissionInfoType {
   admissionInfo: {
     applicantName: string;
-    applicantGender: 'MALE' | 'FEMALE';
+    applicantGender: GenderType;
     applicantBirth: Date;
     address: string;
     detailAddress: string;
     graduation: GraduationStatusType;
-    telephone: string;
+    telephone: string | null;
     applicantPhoneNumber: string;
     guardianName: string;
     relationWithApplicant: string;
     guardianPhoneNumber: string;
-    teacherName: string;
-    teacherPhoneNumber: string;
-    schoolName: string;
-    schoolLocation: string;
+    teacherName: string | null;
+    teacherPhoneNumber: string | null;
+    schoolName: string | null;
+    schoolLocation: string | null;
     applicantImageUri: string;
     desiredMajor: {
       firstDesiredMajor: MajorType;
@@ -119,6 +80,48 @@ export interface AdmissionInfoType {
       thirdDesiredMajor: MajorType;
     };
     screening: ScreeningType;
+  };
+}
+
+export interface CommonApplicationResponseType {
+  id: number;
+  admissionInfo: {
+    applicantName: string;
+    applicantGender: GenderType;
+    applicantBirth: Date;
+    address: string;
+    detailAddress: string;
+    graduation: GraduationStatusType;
+    telephone: string | null;
+    applicantPhoneNumber: string;
+    guardianName: string;
+    relationWithApplicant: string;
+    guardianPhoneNumber: string;
+    teacherName: string | null;
+    teacherPhoneNumber: string | null;
+    schoolName: string | null;
+    schoolLocation: string | null;
+    applicantImageUri: string;
+    desiredMajor: {
+      firstDesiredMajor: MajorType;
+      secondDesiredMajor: MajorType;
+      thirdDesiredMajor: MajorType;
+    };
+    screening: ScreeningType;
+  };
+  middleSchoolGrade: string;
+  admissionGrade: GEDScore | CommonScore;
+  admissionStatus: {
+    isFinalSubmitted: boolean;
+    isPrintsArrived: boolean;
+    firstEvaluation: EvaluationStatusType;
+    secondEvaluation: EvaluationStatusType;
+    screeningSubmittedAt: ScreeningType | null;
+    screeningFirstEvaluationAt: ScreeningType | null;
+    screeningSecondEvaluationAt: ScreeningType | null;
+    registrationNumber: number | null;
+    secondScore: number | null;
+    finalMajor: MajorType | null;
   };
 }
 
