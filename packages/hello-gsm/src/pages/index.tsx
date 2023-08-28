900b69873e3e9b11f3f780f112339977d82e7af3
yoosion030 <yoosioff@gmail.com>
2023-08-28 14:49:01 +0900

    Add login query string redirect

--- packages/hello-gsm/src/pages/index.tsx
+++ packages/hello-gsm/src/pages/index.tsx
@@ -3,10 +3,33 @@ import { SEOHelmet } from 'components';
 import { MainPage } from 'PageContainer';
 import application from 'Api/application';
 import { ApplicationDataType } from 'type/application';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 const Home: NextPage<ApplicationDataType> = ({ data }) => {
   const seoTitle = '홈';
   const desc = '광주소프트웨어마이스터고등학교 입학 지원 서비스입니다.';
+
+  const { query, push } = useRouter();
+
+  useEffect(() => {
+    const isVerification = query.verification === 'true';
+    const isNotVerification = query.verfication === 'false';
+    const isLoginFailure = query.login === 'failure';
+
+    if (isVerification) {
+      toast.success('로그인 되었습니다.');
+    }
+    if (isNotVerification) {
+      push('/auth/signup');
+      toast.success('본인인증을 위해 확인을 해주세요.');
+    }
+
+    if (isLoginFailure) {
+      toast.error('로그인에 실패했습니다. 다시 시도해주세요.');
+    }
+  }, []);
 
   return (
     <>
