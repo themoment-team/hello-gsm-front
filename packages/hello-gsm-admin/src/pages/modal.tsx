f0ce4b373c40fe8d6188c82f093e7e87ff17ad0e
예빈 <yebin060310@gmail.com>
2023-08-28 21:00:59 +0900

    Delete Modal

--- packages/hello-gsm-admin/src/pages/modal.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next';
-import { NewModal } from 'components';
-const Modal: NextPage = () => {
-  return <NewModal name="다낭승제" studentCode="123" />;
-};
-
-export default Modal;
