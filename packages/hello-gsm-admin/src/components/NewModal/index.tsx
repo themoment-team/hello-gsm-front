5aca18b56bdbb13c197597dfaef49a4890ecce71
예빈 <yebin060310@gmail.com>
2023-08-28 10:11:18 +0900

    Update string -> number

--- packages/hello-gsm-admin/src/components/NewModal/index.tsx
+++ packages/hello-gsm-admin/src/components/NewModal/index.tsx
@@ -14,21 +14,21 @@ interface ModalProps {
 const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
   const [isClose, setIsClose] = useState<boolean>(true);
   const [isButtonClicked, setIsButtonClicked] = useState<boolean>(false);
-  const [selectedButtonId, setSelectedButtonId] = useState<string>('0');
+  const [selectedButtonId, setSelectedButtonId] = useState<number>(0);
   const [showModalResult, setShowModalResult] = useState<boolean>(false);
-  const [showModal, setShowModal] = useState<string>('0');
+  const [showModal, setShowModal] = useState<number>(0);
 
   const handleIsClose = () => setIsClose(prev => !prev);
   const handleOptionSelect = () => {
     setIsClose(true);
   };
 
-  const handleButtonClick = (id: string) => {
+  const handleButtonClick = (id: number) => {
     setIsButtonClicked(true);
     setSelectedButtonId(id);
   };
 
-  const handleModalButtonClick = (selectedButtonId: string) => {
+  const handleModalButtonClick = (selectedButtonId: number) => {
     setShowModal(selectedButtonId);
     setIsButtonClicked(false);
     setShowModalResult(true);
@@ -40,14 +40,14 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
       {isClose && (
         <S.Modal
           style={{
-            width: showModal !== '0' ? '28.5rem' : '55.5rem',
+            width: showModal !== 0 ? '28.5rem' : '55.5rem',
           }}
         >
           <S.ModalContent>
             <S.XIcon
               onClick={handleIsClose}
               style={{
-                width: showModal !== '0' ? '28.5rem' : '55.5rem',
+                width: showModal !== 0 ? '28.5rem' : '55.5rem',
               }}
             >
               <I.ModalCloseIcon />
@@ -60,7 +60,7 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
                 <S.Desc>{name}님의 어떤 상태를 수정하실건가요?</S.Desc>
               </S.TitleBox>
             )}
-            {showModal === '1' && (
+            {showModal === 1 && (
               <S.ContentBox>
                 <S.TitleBox style={{ width: '416px' }}>
                   <S.Title>수험번호 {studentCode}</S.Title>
@@ -74,7 +74,7 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
                 />
               </S.ContentBox>
             )}
-            {showModal === '2' && (
+            {showModal === 2 && (
               <S.ContentBox>
                 <S.TitleBox style={{ width: '416px' }}>
                   <S.Title>수험번호 {studentCode}</S.Title>
@@ -90,7 +90,7 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
                 />
               </S.ContentBox>
             )}
-            {showModal === '3' && (
+            {showModal === 3 && (
               <S.ContentBox>
                 <S.TitleBox style={{ width: '26rem' }}>
                   <S.Title>수험번호 {studentCode}</S.Title>
@@ -106,7 +106,7 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
                 />
               </S.ContentBox>
             )}
-            {showModal === '4' && (
+            {showModal === 4 && (
               <S.ContentBox>
                 <S.TitleBox style={{ width: '26rem' }}>
                   <S.Title>수험번호 {studentCode}</S.Title>
@@ -125,21 +125,19 @@ const NewModal: React.FC<ModalProps> = ({ name, studentCode }) => {
             ) : (
               <S.ContentBox>
                 <S.ButtonBox>
-                  <S.ModalOption onClick={() => handleButtonClick('1')}>
+                  <S.ModalOption onClick={() => handleButtonClick(1)}>
                     <I.DocumentsSubmissionStatus
-                      isActive={selectedButtonId === '1'}
+                      isActive={selectedButtonId === 1}
                     />
                   </S.ModalOption>
-                  <S.ModalOption onClick={() => handleButtonClick('2')}>
-                    <I.FirstPassStatus isActive={selectedButtonId === '2'} />
+                  <S.ModalOption onClick={() => handleButtonClick(2)}>
+                    <I.FirstPassStatus isActive={selectedButtonId === 2} />
                   </S.ModalOption>
-                  <S.ModalOption onClick={() => handleButtonClick('3')}>
-                    <I.SecondScoringStatus
-                      isActive={selectedButtonId === '3'}
-                    />
+                  <S.ModalOption onClick={() => handleButtonClick(3)}>
+                    <I.SecondScoringStatus isActive={selectedButtonId === 3} />
                   </S.ModalOption>
-                  <S.ModalOption onClick={() => handleButtonClick('4')}>
-                    <I.SecondScoringEntry isActive={selectedButtonId === '4'} />
+                  <S.ModalOption onClick={() => handleButtonClick(4)}>
+                    <I.SecondScoringEntry isActive={selectedButtonId === 4} />
                   </S.ModalOption>
                 </S.ButtonBox>
                 <ModalButton
