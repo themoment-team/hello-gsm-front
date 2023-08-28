125965825d34f01d87b31174358a9694aa3c6fba
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-08-28 20:09:51 +0900

    Update edit ticket type

--- packages/hello-gsm-admin/src/Types/ticket.ts
+++ packages/hello-gsm-admin/src/Types/ticket.ts
@@ -1,10 +1,12 @@
+import { ScreeningType } from 'Types/application';
+
 export interface TicketDataType {
   data: {
     applicantName: string;
     applicantBirth: string;
     applicantImageUri: string;
     schoolName: string;
-    screening: 'GENERAL' | 'SOCIAL' | 'SPECIAL_VETERANS' | 'SPECIAL_ADMISSION';
+    screening: ScreeningType;
     registrationNumber: number;
   }[];
 }
