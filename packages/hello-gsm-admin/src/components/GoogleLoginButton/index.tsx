5d61bedd53b2bc01181cb4f33984e540ed567ed7
Lee-Seungje <moondgod@gmail.com>
2023-08-31 16:00:40 +0900

    Add href

--- packages/hello-gsm-admin/src/components/GoogleLoginButton/index.tsx
+++ packages/hello-gsm-admin/src/components/GoogleLoginButton/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import * as S from './style';
+import auth from 'Api/auth';
 
 interface GoogleLoginButtonProps {
   children: React.ReactNode;
 }
 
 const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ children }) => (
-  <S.GoogleLoginButton>
+  <S.GoogleLoginButton href={auth.googleSignin()}>
     <S.ImgWrap>
       <Image src="/googleIcon.png" alt="google아이콘" width={30} height={30} />
     </S.ImgWrap>
