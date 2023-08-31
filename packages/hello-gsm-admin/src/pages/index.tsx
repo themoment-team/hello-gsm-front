e67d06889399830cfa5a5b68aa78adfed995afc7
Lee-Seungje <moondgod@gmail.com>
2023-08-31 17:20:54 +0900

    Add after redirect

--- packages/hello-gsm-admin/src/pages/index.tsx
+++ packages/hello-gsm-admin/src/pages/index.tsx
@@ -5,10 +5,35 @@ import { ApplicantsType } from 'Types/application';
 import application from 'Api/application';
 import auth from 'Api/auth';
 import HeaderType from 'Types/header';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Home: NextPage<ApplicantsType> = ({ list, count }) => {
   const seoTitle = '홈';
   const desc = '지원자들의 정보를 확인합니다.';
+
+  const { query, push } = useRouter();
+
+  useEffect(() => {
+    const isVerification = query.verification === 'true';
+    const isNotVerification = query.verification === 'false';
+    const isLoginFailure = query.login === 'failure';
+
+    if (isVerification) {
+      toast.success('로그인 되었습니다.');
+    }
+
+    if (isNotVerification) {
+      push('/signin');
+      toast.success('본인인증을 위해 확인을 해주세요.');
+    }
+
+    if (isLoginFailure) {
+      toast.error('로그인에 실패했습니다. 다시 시도해주세요.');
+    }
+  });
+
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
