1384d734c0e3b43e2e491ddf1f5358529c419ada
Lee-Seungje <moondgod@gmail.com>
2023-08-31 20:52:30 +0900

    Delete useless code

--- packages/hello-gsm-admin/src/pages/index.tsx
+++ packages/hello-gsm-admin/src/pages/index.tsx
@@ -1,10 +1,7 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 import { SEOHelmet } from 'components';
 import { MainPage } from 'PageContainer';
 import { ApplicantsType } from 'Types/application';
-import application from 'Api/application';
-import auth from 'Api/auth';
-import HeaderType from 'Types/header';
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -13,24 +10,13 @@ const Home: NextPage<ApplicantsType> = ({ list, count }) => {
   const seoTitle = '홈';
   const desc = '지원자들의 정보를 확인합니다.';
 
-  const { query, push } = useRouter();
+  const { query } = useRouter();
 
   useEffect(() => {
     const isVerification = query.verification === 'true';
-    const isNotVerification = query.verification === 'false';
-    const isLoginFailure = query.login === 'failure';
 
     if (isVerification) {
       toast.success('로그인 되었습니다.');
-    }
-
-    if (isNotVerification) {
-      push('/signin');
-      toast.success('본인인증을 위해 확인을 해주세요.');
-    }
-
-    if (isLoginFailure) {
-      toast.error('로그인에 실패했습니다. 다시 시도해주세요.');
     }
   });
 
