40bc45937ffc59b0492eb9eda82075c9c3223404
yoosion030 <yoosioff@gmail.com>
2023-08-31 10:16:39 +0900

    Delete unused type

--- packages/hello-gsm/src/type/user.ts
+++ packages/hello-gsm/src/type/user.ts
@@ -1,46 +1,16 @@
 import { CommonApplicationResponseType } from './application';
 
-interface StatusDataType extends StatusApplication {
-  name: string;
-  userImg: string;
-}
-
-export interface StatusApplication {
-  application: null | {
-    screening: '일반전형' | '사회통합전형' | '특별전형';
-    firstResultScreening: '일반전형' | '사회통합전형' | '특별전형';
-    finalResultScreening: '일반전형' | '사회통합전형' | '특별전형';
-    isFinalSubmission: boolean;
-    isDocumentReception: boolean;
-    registrationNumber: null | number;
-    application_score?: {
-      scoreTotal: number;
-    };
-    application_details: {
-      firstWantedMajor: '인공지능과' | '스마트IoT과' | '소프트웨어개발과';
-      secondWantedMajor: '인공지능과' | '스마트IoT과' | '소프트웨어개발과';
-      thirdWantedMajor: '인공지능과' | '스마트IoT과' | '소프트웨어개발과';
-      educationStatus: '졸업예정' | '졸업' | '검정고시';
-      majorResult?: '인공지능과' | '스마트IoT과' | '소프트웨어개발과';
-    };
-  };
-}
-
-export interface StatusType {
-  data: StatusDataType;
-}
-
 export interface MainDescStatusType {
   selectedIndex: number;
   data?: CommonApplicationResponseType;
 }
 
-export interface InfoType {
-  data: {
-    user_idx: number;
-    birth: Date;
-    cellphoneNumber: string;
-    gender: string;
-    name: string;
-  };
+type ProviderType = 'google' | 'kakao';
+type RoleType = 'ROLE_USER' | 'ROLE_UNAUTHENTICATED' | 'ROLE_ADMIN';
+
+export interface UserInfoType {
+  id: number;
+  provider: ProviderType;
+  providerId: string;
+  role: RoleType;
 }
