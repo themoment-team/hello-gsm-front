ffcba25522fc7b53fb0fa13561dbd2540e0af678
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-09-05 19:52:32 +0900

    Update delet unused code

--- packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
+++ packages/hello-gsm-admin/src/PageContainer/MainPage/index.tsx
@@ -18,35 +18,6 @@ const MainPage: NextPage<ApplicantsType> = ({ list, count }) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [tmpValue, setTmpValue] = useState<string>('');
 
-  const handleObserver = useCallback(
-    async (
-      [entry]: IntersectionObserverEntry[],
-      observer: IntersectionObserver,
-    ) => {
-      if (entry.isIntersecting) {
-        observer.unobserve(entry.target);
-        observer.observe(entry.target);
-      }
-    },
-    [],
-  );
-
-  useEffect(() => {
-    if (!loadMoreRef.current) return;
-
-    const option = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0,
-    };
-
-    const observer = new IntersectionObserver(handleObserver, option);
-
-    loadMoreRef.current && observer.observe(loadMoreRef.current);
-
-    return () => observer && observer.disconnect();
-  }, [handleObserver, isPageEnd]);
-
   useEffect(() => {
     const debounce = setTimeout(() => {
       return setSearchValue(tmpValue);
@@ -85,7 +56,6 @@ const MainPage: NextPage<ApplicantsType> = ({ list, count }) => {
           {filteredApplicationList?.map((content, index: number) => (
             <ContentBox content={content} key={index} />
           ))}
-          {!isPageEnd && <S.Target ref={loadMoreRef} />}
         </S.ContentList>
       </S.MainPageContent>
     </S.MainPage>
