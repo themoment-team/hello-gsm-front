ad89467500681d567b010fdfca961377d922cfdf
yoosion030 <yoosioff@gmail.com>
2023-09-06 09:19:07 +0900

    Fix undefined build error

--- packages/hello-gsm/src/type/ged.ts
+++ packages/hello-gsm/src/type/ged.ts
@@ -3,5 +3,5 @@ import { CommonScore, GEDScore } from './application';
 export const isGED = (
   target: GEDScore | CommonScore | undefined,
 ): target is GEDScore => {
-  return (target as GEDScore).gedTotalScore !== undefined;
+  return (target as GEDScore)?.gedTotalScore !== undefined;
 };
