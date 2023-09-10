057760a510d1cd954ac170dc8af6c15d45f32ec4
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-09-11 00:35:38 +0900

    Update Icon disabled condition

--- packages/hello-gsm-admin/src/Assets/svg/PaginationIcon.tsx
+++ packages/hello-gsm-admin/src/Assets/svg/PaginationIcon.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 
 interface IconProps {
   turn: 'right' | 'left';
+  disabled: boolean;
 }
 
 const Turn = {
@@ -9,7 +10,7 @@ const Turn = {
   right: '180',
 } as const;
 
-const PaginationIcon: React.FC<IconProps> = ({ turn }) => (
+const PaginationIcon: React.FC<IconProps> = ({ turn, disabled }) => (
   <svg
     css={css`
       transform: rotate(${Turn[turn]}deg);
@@ -22,7 +23,7 @@ const PaginationIcon: React.FC<IconProps> = ({ turn }) => (
   >
     <path
       d="M18.1877 11.3891L9.61218 3.27408C9.17828 2.86301 8.48306 2.92187 8.12444 3.40003L7.83642 3.78405C7.52995 4.19268 7.58069 4.76659 7.9541 5.1151L15.454 12.1151L8.24212 18.7841C7.86871 19.1326 7.81797 19.7065 8.12444 20.1151L8.41245 20.4991C8.77107 20.9773 9.46629 21.0361 9.9002 20.6251L18.1877 12.841C18.6041 12.4466 18.6041 11.7835 18.1877 11.3891Z"
-      fill="black"
+      fill={disabled ? '#E0E0E0' : '#000000'}
     />
   </svg>
 );
