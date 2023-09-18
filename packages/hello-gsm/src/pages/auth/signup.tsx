199cc773192e3ce354c2bd24356ec3630f0b359a
yoosion030 <yoosioff@gmail.com>
2023-09-19 08:20:20 +0900

    Add signup 로그인 유저 유효성 검사

--- packages/hello-gsm/src/pages/auth/signup.tsx
+++ packages/hello-gsm/src/pages/auth/signup.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from 'next';
 import { SEOHelmet } from 'components';
 import { SignUpPage } from 'PageContainer';
+import { UserInfoType } from 'type/user';
+import user from 'Api/user';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const SignUp: NextPage = () => {
   const seoTitle = '회원가입';
   const desc = '회원가입 페이지입니다.';
+
+  const { push } = useRouter();
+
+  const getUser = async () => {
+    try {
+      const { data }: { data: UserInfoType } = await user.getMyInfo();
+      if (data.role === 'ROLE_USER') {
+        push('/');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <>
       <SEOHelmet seoTitle={seoTitle} desc={desc} />
