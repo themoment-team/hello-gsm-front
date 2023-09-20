61db7a3787789ceac6abad919f875bb0bd42d9a5
yoosion030 <yoosioff@gmail.com>
2023-09-20 20:44:41 +0900

    Delete accessToken params

--- packages/hello-gsm-admin/src/Api/user.ts
+++ packages/hello-gsm-admin/src/Api/user.ts
@@ -3,19 +3,15 @@ import { UserController } from 'Utils/Libs/requestUrls';
 
 class User {
   /**
-   * @param accessToken
    * @param userId - user의 Id
    * @returns 특정 사용자 정보를 반환합니다.
    */
-  getUserInfo(userId: string, accessToken?: string) {
+  getUserInfo(userId: string) {
     try {
-      return RequestApi(
-        {
-          method: 'GET',
-          url: UserController.userInfo(userId),
-        },
-        accessToken,
-      );
+      return RequestApi({
+        method: 'GET',
+        url: UserController.userInfo(userId),
+      });
     } catch (error: any) {
       return error;
     }
