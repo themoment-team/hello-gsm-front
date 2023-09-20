aa36a2b205a20848bf6655d26c6d1d6db87eb5fe
yoosion030 <yoosioff@gmail.com>
2023-09-20 20:31:22 +0900

    Add toast 메세지 후 push

--- packages/hello-gsm-admin/src/pages/index.tsx
+++ packages/hello-gsm-admin/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import { SEOHelmet } from 'components';
 import { MainPage } from 'PageContainer';
-import { ApplicantsType } from 'Types/application';
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -10,13 +9,14 @@ const Home: NextPage = () => {
   const seoTitle = '홈';
   const desc = '지원자들의 정보를 확인합니다.';
 
-  const { query } = useRouter();
+  const { query, push } = useRouter();
 
   useEffect(() => {
     const isVerification = query.verification === 'true';
 
     if (isVerification) {
       toast.success('로그인 되었습니다.');
+      push('/');
     }
   });
 
