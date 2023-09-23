2f36559843cb571dd832b4580a6a57a3b64f3d89
예빈 <yebin060310@gmail.com>
2023-09-23 19:21:21 +0900

    Delete accesstoken

--- packages/hello-gsm-admin/src/Api/status.ts
+++ packages/hello-gsm-admin/src/Api/status.ts
@@ -3,19 +3,14 @@ import { StatusController } from 'Utils/Libs/requestUrls';
 
 class Status {
   /**
-   * @param accessToken
    * @param userID
-   * @returns 특정 사용자의 본인인증 정보를 가져옵니다.
    */
-  putStatus(userID: number, accessToken?: string) {
+  putStatus(userID: number) {
     try {
-      return RequestApi(
-        {
-          method: 'PUT',
-          url: StatusController.putStatus(userID),
-        },
-        accessToken,
-      );
+      return RequestApi({
+        method: 'PUT',
+        url: StatusController.putStatus(userID),
+      });
     } catch (error: any) {
       return error;
     }
