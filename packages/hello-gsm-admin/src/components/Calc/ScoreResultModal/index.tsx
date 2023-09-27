c1a1b690212981196632ed99c8315cce8e3e1794
yoosion030 <yoosioff@gmail.com>
2023-09-27 20:06:02 +0900

    Add calc component

---
+++ packages/hello-gsm-admin/src/components/Calc/ScoreResultModal/index.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import useApplyStore from 'Stores/ApplyStoreContainer';
+import * as S from './style';
+
+interface ResultType {
+  result: number[];
+}
+
+const ScoreResultModal: React.FC<ResultType> = ({ result }) => {
+  const { setShowScoreResult } = useApplyStore();
+
+  return (
+    <S.Background>
+      <S.ResultSection>
+        <S.SubjectSection>
+          <S.ResultSubject>교과성적</S.ResultSubject>
+          <S.ResultSubject>예체능</S.ResultSubject>
+          <S.ResultSubject>비교과 성적</S.ResultSubject>
+          <S.ResultTotal>총합</S.ResultTotal>
+        </S.SubjectSection>
+        <S.SubjectSection>
+          {result?.map((res, i) => (
+            <S.Score key={i}>{res}</S.Score>
+          ))}
+        </S.SubjectSection>
+        <hr />
+        <S.MainDesc>원서를 저장했습니다.</S.MainDesc>
+        <S.SubDesc>
+          ( 원서나 성적은 내정보 페이지에서 최종 제출 전에 수정할 수 있습니다. )
+        </S.SubDesc>
+        <S.ConfirmSection>
+          <S.Confirm onClick={setShowScoreResult}>수정</S.Confirm>
+          <Link href="/mypage" passHref>
+            <S.Confirm onClick={setShowScoreResult}>확인</S.Confirm>
+          </Link>
+        </S.ConfirmSection>
+      </S.ResultSection>
+    </S.Background>
+  );
+};
+
+export default ScoreResultModal;
