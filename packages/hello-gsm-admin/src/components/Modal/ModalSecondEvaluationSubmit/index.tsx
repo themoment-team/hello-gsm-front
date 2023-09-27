9f76600acf6314fa0a9c6bd10aec097158ce4a28
예빈 <yebin060310@gmail.com>
2023-09-27 10:21:29 +0900

    Update useEffect

--- packages/hello-gsm-admin/src/components/Modal/ModalSecondEvaluationSubmit/index.tsx
+++ packages/hello-gsm-admin/src/components/Modal/ModalSecondEvaluationSubmit/index.tsx
@@ -11,7 +11,7 @@ const ModalSecondEvaluationSubmit = ({
 }) => {
   const { setSelectedOption, selectedOption } = useStore();
   useEffect(() => {
-    setSelectedOption(data.secondEvaluation ? 1 : 2);
+    data.secondEvaluation !== undefined ? (data.secondEvaluation ? 1 : 2) : 0;
     console.log('2차 옵션');
     console.log(selectedOption);
   }, [setSelectedOption, data.secondEvaluation, selectedOption]);
