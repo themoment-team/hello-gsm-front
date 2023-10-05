b254bffcd1b10c2df2ae41216726fe8947fdafc5
예빈 <yebin060310@gmail.com>
2023-10-05 10:21:26 +0900

    Update disabled

--- packages/hello-gsm-admin/src/components/Modal/index.tsx
+++ packages/hello-gsm-admin/src/components/Modal/index.tsx
@@ -192,6 +192,9 @@ const Modal = ({ data, onClose, getApplicationList }: ModalProps) => {
                 <C.ModalButton
                   buttonTitle="확인"
                   onClick={() => handleSubmit()}
+                  disabled={
+                    data.firstEvaluation === 'NOT_YET' && selectedOption === 0
+                  }
                 />
               </S.ContentBox>
             )}
