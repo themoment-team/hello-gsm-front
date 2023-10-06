3a720a1d00765f2e5d81fbacd40de70fb7be8c36
yoosion030 <yoosioff@gmail.com>
2023-10-06 16:15:13 +0900

    Update Redirect -> rewrite

--- packages/hello-gsm/src/middleware.ts
+++ packages/hello-gsm/src/middleware.ts
@@ -10,27 +10,44 @@ const getSiteState = async () => {
 
 export async function middleware(req: NextRequest) {
   const siteState = await getSiteState();
+  const acceptable =
+    new Date() >= new Date('2023/10/16 00:00') &&
+    new Date() <= new Date('2023/10/19 08:00');
 
   const { origin, pathname } = req.nextUrl;
   const { device, browser } = userAgent(req);
-  console.log(siteState, pathname);
+  const applicationFormURL = ['/information', '/apply'];
 
+  // IE 예외처리
+  if (browser.name === 'IE' && pathname !== '/browser') {
+    return NextResponse.rewrite(`${origin}/browser`);
+  }
+
+  // 점검창 처리
   if (siteState === 'INSPECTION' && pathname !== '/inspection') {
     return NextResponse.rewrite(`${origin}/inspection`);
+  }
+
+  // 원서 날짜 이후 처리
+  if (applicationFormURL.includes(pathname)) {
+    // 원서 접수 가능 기간이 아닐 시
+    if (!acceptable) {
+      return NextResponse.rewrite(origin);
+    }
+
+    if (browser.name === 'Safari') {
+      return NextResponse.rewrite(`${origin}/browser`);
+    }
+
+    if (device.type === ('mobile' || 'tablet')) {
+      // pc가 아닐 시
+      return NextResponse.rewrite(origin);
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
