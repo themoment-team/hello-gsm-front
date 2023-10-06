99c4b7397f02a456a5350aa31b3f5ae24aa9f121
yoosion030 <yoosioff@gmail.com>
2023-10-06 14:27:53 +0900

    Update test date

--- packages/hello-gsm/src/shared/Date/firstScreening.ts
+++ packages/hello-gsm/src/shared/Date/firstScreening.ts
@@ -1,15 +1,15 @@
-const today = new Date();
+const today = new Date('2023/10/16 9:00:00');
 
-// 9월 27일 0시부터 오후 6시까지
+// 10월 16일 9시부터 10월 19일 17시 까지
 // 원서 접수 및 증빙서류 제출 날짜
-export const startApply = new Date('2023/10/5 00:00:00');
-export const endApply = new Date('2023/10/5 18:30:00');
+export const startApply = new Date('2023/10/16 9:00:00');
+export const endApply = new Date('2023/10/19 17:00:00');
 export const applyAcceptable = today >= startApply && today <= endApply;
 
-// 9월 27일 6시부터 7시 반까지
+// 10월 24일 10시부터 11월 1일 9시 59분까지
 // 첫번쨰 합격 결과 보여주는 날짜
-export const startFirstResult = new Date('2023/10/5 20:00:00');
-export const endFirstResult = new Date('2023/10/5 20:30:00');
+export const startFirstResult = new Date('2023/10/23 10:00:00');
+export const endFirstResult = new Date('2023/11/01 10:00:00');
 export const isStartFirstResult = today >= startFirstResult;
 export const isFirstResult =
   today >= startFirstResult && today < endFirstResult;
--- packages/hello-gsm/src/shared/Date/secondScreening.ts
+++ packages/hello-gsm/src/shared/Date/secondScreening.ts
@@ -1,3 +1,3 @@
 // 9월 27일 6시 30분
 // 2차 시험보는 날
-export const startFinalTest = new Date('2023/10/5 20:00:00');
+export const startFinalTest = new Date('2023/10/27 13:00:00');
