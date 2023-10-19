ea03a9103477d4c238bd6c624c54d35c7273d3e4
예빈 <yebin060310@gmail.com>
2023-10-19 11:55:28 +0900

    Delete useEffect

--- packages/hello-gsm-admin/src/components/ContentBox/index.tsx
+++ packages/hello-gsm-admin/src/components/ContentBox/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/react';
 import * as S from './style';
 import * as I from 'Assets/svg';
