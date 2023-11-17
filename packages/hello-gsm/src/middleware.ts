d55a6f350a6573675f4267398f11393a944968f1
yoosion030 <yoosioff@gmail.com>
2023-11-17 21:33:53 +0900

    Update Middleware

--- packages/hello-gsm/src/middleware.ts
+++ packages/hello-gsm/src/middleware.ts
@@ -31,11 +31,12 @@ export async function middleware(req: NextRequest) {
     return NextResponse.rewrite(`${origin}/browser`);
   }
 
+  const today = new Date();
   const acceptable =
-    new Date() >= new Date('2023/10/16 00:00') &&
-    new Date() <= new Date('2023/10/19 08:00');
+    today >= new Date('2023/10/16 00:00') &&
+    today <= new Date('2023/10/19 08:00');
 
-  const applicationFormURL = ['/information', '/apply', '/auth/signin'];
+  const applicationFormURL = ['/information', '/apply'];
 
   // 원서 날짜 이후 처리 해당 페이지라면
   if (applicationFormURL.includes(pathname)) {
@@ -50,6 +51,16 @@ export async function middleware(req: NextRequest) {
 
     if (device.type === ('mobile' || 'tablet')) {
       // pc가 아닐 시
+      return NextResponse.rewrite(origin);
+    }
+  }
+
+  const isShowResult =
+    today >= new Date('2023/10/23 01:00:00') &&
+    new Date('2023/10/23 01:00:00') <= new Date('2023/11/14 15:00:00');
+
+  if (pathname === 'auth/signin') {
+    if (isShowResult) {
       return NextResponse.rewrite(origin);
     }
   }
