b675c3ba8cd99156ad86fc6285a21404a69392e8
Lee-Seungje <moondgod@gmail.com>
2024-01-31 13:46:45 +0900

    Add projectCard

---
+++ packages/hello-gsm/src/components/ProjectCard/index.tsx
@@ -0,0 +1,31 @@
+import * as S from './style';
+
+import Image from 'next/image';
+
+interface Props {
+  project: {
+    imageUrl: string;
+    title: string;
+    description: string;
+  };
+}
+
+const ProjectCard: React.FC<Props> = ({
+  project: { imageUrl, title, description },
+}) => {
+  return (
+    <S.CardWrapper>
+      <S.ImageWrapper>
+        <S.ImageWrapper>
+          <Image src={imageUrl} fill alt="project image" />
+        </S.ImageWrapper>
+      </S.ImageWrapper>
+      <S.TextWrapper>
+        <S.CardTitle>{title}</S.CardTitle>
+        <S.CardDescription>{description}</S.CardDescription>
+      </S.TextWrapper>
+    </S.CardWrapper>
+  );
+};
+
+export default ProjectCard;
---
+++ packages/hello-gsm/src/components/ProjectCard/style.ts
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+
+export const CardWrapper = styled.div`
+  width: 414px;
+  height: 450px;
+  border-radius: 16px;
+  border: 1px solid rgba(255, 255, 255, 0.8);
+  background: linear-gradient(
+    140deg,
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  backdrop-filter: blur(20px);
+  padding: 40px 44px;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 326px;
+  height: 200px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 20px 0px rgba(255, 255, 255, 0.4);
+  position: relative;
+
+  .iamge {
+    object-fit: cover;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const CardTitle = styled.span`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 400;
+  letter-spacing: -0.72px;
+`;
+
+export const CardDescription = styled.p`
+  color: #a7a7a7;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+  letter-spacing: -0.32px;
+`;
